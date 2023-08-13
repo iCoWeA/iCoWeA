fea31469209d4c9a6dfd1aaf76f53b47b4d4927e
Richard Wagner <richard.wagner@gmail.com>
2023-08-13 10:38:58 +0200

    theme

--- src/lib/simpleComponents/contexts/accordion.ts
+++ src/lib/simpleComponents/contexts/accordion.ts
@@ -3,12 +3,14 @@ import { createContext } from 'react';
 export interface AccordionContext {
   isOpen: boolean;
   isDisabled: boolean;
+  unmountOnExit: boolean;
   onToggle: () => void;
 }
 
 export const initialState: AccordionContext = {
   isOpen: false,
   isDisabled: false,
+  unmountOnExit: false,
   onToggle: () => {}
 };
 
