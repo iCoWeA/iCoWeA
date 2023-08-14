01c7cb6466e7afc16b791e918e0c960e96a78302
Richard Wagner <richard.wagner@gmail.com>
2023-08-15 01:04:29 +0200

    footer

--- src/lib/simpleComponents/components/UI/CardFooter.tsx
+++ src/lib/simpleComponents/components/UI/CardFooter.tsx
@@ -1,39 +1,26 @@
 import React, { type BaseHTMLAttributes, forwardRef, useContext } from 'react';
+import { type CardFooterDefaultProps } from '../../configs/cardFooterConfig';
 import themeContext from '../../contexts/theme';
-import { twMerge } from 'tailwind-merge';
-import { mergeClasses } from '../../utils/styleHelper';
+import { mergeClasses, setDefaultProps } from '../../utils/propsHelper';
 
-export interface CardFooterProps extends BaseHTMLAttributes<HTMLDivElement> {
-  columns?: boolean;
-  fullwidht?: boolean;
-}
+export interface CardFooterProps extends CardFooterDefaultProps, BaseHTMLAttributes<HTMLDivElement> {}
 
-const CardFooter = forwardRef<HTMLDivElement, CardFooterProps>(
-  ({ columns, fullwidht, className, ...restProps }, ref) => {
-    const { config } = useContext(themeContext);
-    const { defaultProps, styles } = config.cardFooter;
+const CardFooter = forwardRef<HTMLDivElement, CardFooterProps>((props, ref) => {
+  const { config } = useContext(themeContext);
+  const { defaultProps, styles } = config.cardFooter;
+  const { columns, fullwidht, className, ...restProps } = setDefaultProps(props, defaultProps);
 
-    columns = columns ?? defaultProps.columns;
-    fullwidht = fullwidht ?? defaultProps.fullwidht;
+  /* Set props */
+  const mergedClassName = mergeClasses(styles.base, columns && styles.columns, fullwidht && styles.fullwidth, className);
 
-    const mergedClassName = twMerge(
-      mergeClasses(
-        styles.base,
-        columns && styles.columns,
-        fullwidht && styles.fullwidth,
-        className
-      )
-    );
-
-    return (
-      <div
-        className={mergedClassName}
-        ref={ref}
-        {...restProps}
-      />
-    );
-  }
-);
+  return (
+    <div
+      className={mergedClassName}
+      ref={ref}
+      {...restProps}
+    />
+  );
+});
 
 CardFooter.displayName = 'CardFooter';
 
