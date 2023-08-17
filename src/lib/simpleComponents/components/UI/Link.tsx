2408612aca48f50e82c5622ed3d41fcc272afc37
Richard Wagner <richard.wagner@gmail.com>
2023-08-17 15:36:27 +0200

    fix

--- src/lib/simpleComponents/components/UI/Link.tsx
+++ src/lib/simpleComponents/components/UI/Link.tsx
@@ -1,10 +1,18 @@
 import React, { forwardRef, useContext } from 'react';
-import { type LinkProps } from '../../configs/linkConfig';
+import { type LinkColors, type LinkUnderlines } from '../../configs/linkConfig';
 import { type LinkProps as BaseLinkProps, Link as BaseLink } from 'react-router-dom';
 import themeContext from '../../contexts/theme';
 import { mergeClasses, setDefaultProps } from '../../utils/propsHelper';
 
-const Link = forwardRef<HTMLAnchorElement, LinkProps & BaseLinkProps>((props, ref) => {
+export interface LinkProps extends BaseLinkProps {
+  underline?: LinkUnderlines;
+  color?: LinkColors;
+  fullwidth?: boolean;
+  disabled?: boolean;
+  clasName?: string;
+}
+
+const Link = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => {
   const { theme, config } = useContext(themeContext);
   const { defaultProps, styles } = config.link;
   const { underline, color, fullwidth, disabled, className, ...restProps } = setDefaultProps(props, defaultProps);
