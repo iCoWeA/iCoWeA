836a95a625a8f19adfef8acb46a7f7f8d8792944
Richard Wagner <richard.wagner@gmail.com>
2023-08-23 13:22:43 +0200

    fix

--- src/lib/simpleComponents/components/UI/Tooltip.tsx
+++ src/lib/simpleComponents/components/UI/Tooltip.tsx
@@ -1,198 +1,336 @@
-/* import React, {
+import React, {
   type BaseHTMLAttributes,
   forwardRef,
   useContext,
-  type ReactNode,
-  useEffect,
+  type TransitionEvent,
+  useImperativeHandle,
+  useRef,
+  type AnimationEvent,
+  type AnimationEventHandler,
   type CSSProperties,
-  type TransitionEvent
+  type TransitionEventHandler,
+  useEffect,
+  useCallback,
+  type ReactElement,
+  cloneElement,
+  type MouseEvent,
+  type ReactNode,
+  useState
 } from 'react';
-import { createPortal } from 'react-dom';
-import { type TooltipColors } from '../../configs/tooltipConfig';
+import { type TooltipPositions, type TooltipColors } from '../../configs/tooltipConfig';
 import themeContext from '../../contexts/theme';
-import useMount, {
-  States,
-  type Config as TransitionConfig
-} from '../../hooks/useTransition';
-import { twMerge } from 'tailwind-merge';
-import { mergeClasses, mergeStyles } from '../../utils/styleHelper';
-import {
-  type OriginPosition,
-  type TransformPosition
-} from '../../utils/positiontHelper';
-
-export interface TooltipTransitionProps extends TransitionConfig {
-  unmountOnExit?: boolean;
-  enterTransition?: string;
-}
+import useTransition, { TransitionStates, type TransitionConfig } from '../../hooks/useTransition';
+import useScroll from '../../hooks/useScroll';
+import useResize from '../../hooks/useResize';
+import { setElementPosition } from '../../utils/positiontHelper';
+import { mergeClasses, mergeStyles, mergeProps } from '../../utils/propsHelper';
+import { createPortal } from 'react-dom';
 
 export interface TooltipProps extends BaseHTMLAttributes<HTMLDivElement> {
   open?: boolean;
-  anchorRef?: HTMLElement | null;
-  overlayRef?: Element | null;
-  anchorOrigin?: OriginPosition;
-  transformOrigin?: TransformPosition;
-  spacing?: number;
   color?: TooltipColors;
+  position?: TooltipPositions;
+  gap?: number;
+  responsive?: boolean;
+  followCursor?: boolean;
+  overlayRef?: Element | null;
+  unmountOnExit?: boolean;
+  transitionConfig?: TransitionConfig;
+  handler?: ReactElement;
   arrow?: boolean;
-  transitionProps?: TooltipTransitionProps;
-  componentsProps?: {
-    arrow?: BaseHTMLAttributes<HTMLDivElement>;
-  };
+  arrowProps?: BaseHTMLAttributes<HTMLDivElement>;
+  onTransitionEnd?: TransitionEventHandler;
+  onAnimationEnd?: AnimationEventHandler;
+  style?: CSSProperties;
+  className?: string;
+  children?: ReactElement;
 }
 
-const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
-  (
-    {
-      open,
-      anchorRef,
-      overlayRef,
-      anchorOrigin,
-      transformOrigin,
-      spacing,
-      color,
-      arrow,
-      transitionProps,
-      componentsProps,
-      onTransitionEnd: onRootTransitionEnd,
-      style: rootStyle,
-      className: rootClassName,
-      children: rootChildren,
-      ...restRootProps
-    },
-    rootRef
-  ) => {
-    const { theme, config } = useContext(themeContext);
-    const { defaultProps, styles } = config.tooltip;
-    const rootStyles = s
-    const {
-      unmountOnExit = defaultProps.transitionProps.unmountOnExit,
-      enterTransition = defaultProps.transitionProps.enterTransition,
-      enterDuration = defaultProps.transitionProps.enterDuration,
-      exitDuration = defaultProps.transitionProps.exitDuration
-    } = transitionProps ?? {};
+const setArrowPosition = (element: HTMLElement | null, position: TooltipPositions): void => {
+  if (element === null) {
+    return;
+  }
 
-    const { state, enterState, exitState, enter, exit } = useMount({
-      ...defaultProps.transitionProps,
-      ...transitionProps
-    });
+  const splitedPosition = position.split('-')[0];
 
-    if (exitState && open === true) {
+  if (splitedPosition === 'top') {
+    element.style.top = '100%';
+    element.style.left = '50%';
+    element.style.transform = 'rotate(180deg)';
+    element.style.translate = '0 0';
+  }
+
+  if (splitedPosition === 'bottom') {
+    element.style.top = '0';
+    element.style.left = '50%';
+    element.style.transform = 'rotate(0deg)';
+    element.style.translate = '0 -100%';
+  }
+
+  if (splitedPosition === 'left') {
+    element.style.top = '50%';
+    element.style.left = '100%';
+    element.style.transform = 'rotate(90deg)';
+    element.style.translate = '0 0';
+  }
+
+  if (splitedPosition === 'right') {
+    element.style.top = '50%';
+    element.style.left = '0';
+    element.style.transform = 'rotate(-90deg)';
+    element.style.translate = '-100% 0';
+  }
+};
+
+const Tooltip = forwardRef<HTMLDivElement, TooltipProps>((rootProps, rootRef) => {
+  const { theme, config } = useContext(themeContext);
+  const { defaultProps, styles } = config.tooltip;
+  const {
+    open,
+    color,
+    position,
+    gap,
+    responsive,
+    followCursor,
+    overlayRef,
+    unmountOnExit,
+    transitionConfig,
+    handler,
+    arrow,
+    arrowProps,
+    onTransitionEnd: onRootTransitionEnd,
+    onAnimationEnd: onRootAnimationEnd,
+    style: rootStyle,
+    className: rootClassName,
+    children: rootChildren,
+    ...restRootProps
+  } = mergeProps(defaultProps, rootProps);
+  const mergedTransitionConfig = mergeProps(defaultProps.transitionConfig, transitionConfig);
+  let handlerNode: ReactNode;
+  let arrowNode: ReactNode;
+
+  const componentsRef = useRef<{ root: HTMLDivElement | null; handler: HTMLElement | null; arrow: HTMLDivElement | null }>({
+    root: null,
+    handler: null,
+    arrow: null
+  });
+
+  useImperativeHandle<HTMLDivElement | null, HTMLDivElement | null>(rootRef, () => componentsRef.current.root, []);
+
+  const [cursorY, setCursorY] = useState(-1);
+  const [cursorX, setCursorX] = useState(-1);
+  const { state: transitionState, enterState, exitState, className: transitionClassName, enter, exit } = useTransition(mergedTransitionConfig);
+
+  const resize = useCallback((): void => {
+    if (componentsRef.current.root !== null && componentsRef.current.handler !== null) {
+      let newPosition: TooltipPositions;
+
+      if (!followCursor) {
+        newPosition = setElementPosition(
+          componentsRef.current.root,
+          position,
+          componentsRef.current.handler.offsetTop,
+          componentsRef.current.handler.offsetLeft,
+          componentsRef.current.handler.offsetHeight,
+          componentsRef.current.handler.offsetWidth,
+          gap,
+          responsive
+        );
+      } else {
+        newPosition = setElementPosition(
+          componentsRef.current.root,
+          position,
+          cursorY + document.documentElement.scrollTop,
+          cursorX + document.documentElement.scrollLeft,
+          0,
+          0,
+          gap,
+          responsive
+        );
+      }
+
+      if (arrow && componentsRef.current.arrow !== null) {
+        setArrowPosition(componentsRef.current.arrow, newPosition);
+      }
+    }
+  }, [followCursor, position, gap, responsive, cursorY, cursorX]);
+
+  useScroll(resize, enterState);
+
+  useResize(resize, enterState);
+
+  useEffect(() => {
+    if (open === true) {
       enter();
     }
 
-    if (enterState && open === false) {
+    if (open === false) {
       exit();
     }
+  }, [open]);
 
-    useEffect(() => {
-      if (anchorRef !== undefined && anchorRef !== null) {
-        const showHandler: () => void = () => {
-          if (open === undefined) {
-            enter();
-          }
-        };
+  resize();
 
-        const hideHandler: () => void = () => {
-          if (open === undefined) {
-            exit();
-          }
-        };
-
-        if (!anchorRef.matches(':hover') && open === undefined) {
-          exit();
-        }
-
-        anchorRef.addEventListener('mouseenter', showHandler);
-        anchorRef.addEventListener('mouseleave', hideHandler);
-
-        return () => {
-          anchorRef.removeEventListener('mouseenter', showHandler);
-          anchorRef.removeEventListener('mouseleave', hideHandler);
-        };
-      }
-    }, [anchorRef, open, enter, exit]);
-
-    if (anchorRef === null || (unmountOnExit && state === States.EXITED)) {
-      return <></>;
-    }
-
-    let arrowNode: ReactNode;
-    let mergedRootStyle: CSSProperties = {};
-
-    overlayRef = overlayRef ?? defaultProps.overlayRef;
-    position = position ?? defaultProps.position;
-    spacing = spacing ?? defaultProps.spacing;
-    color = color ?? defaultProps.color;
-    arrow = arrow ?? defaultProps.arrow;
-
-    /* Set arrow props
-    if (arrow) {
-      const { className: arrowClassName, ...restArrowProps } =
-        componentsProps?.arrow ?? defaultProps.componentsProps.arrow;
-
-      const mergedArrowClassName = twMerge(
-        mergeClasses(
-          arrowStyles.base,
-          arrowStyles.positions[position.split('-')[0]],
-          arrowStyles.colors[theme][color],
-          arrowClassName
-        )
-      );
-
-      arrowNode = (
-        <div
-          className={mergedArrowClassName}
-          {...restArrowProps}
-        ></div>
-      );
-    }
-
-    /* Set root props
-    const transitionEndRootHandler = (
-      event: TransitionEvent<HTMLDivElement>
-    ): void => {
-      if (enterState) {
-        enter(true);
-      } else {
-        exit(true);
+  /* Set handler props */
+  if (handler !== undefined) {
+    const onHandlerMouseEnter = (event: MouseEvent<HTMLElement>): void => {
+      if (followCursor) {
+        setCursorY(event.clientY);
+        setCursorX(event.clientX);
       }
 
-      if (onRootTransitionEnd !== undefined) {
-        onRootTransitionEnd(event);
+      if (open === undefined && exitState) {
+        enter();
+      }
+
+      if (handler.props.onMouseEnter !== undefined) {
+        handler.props.onMouseEnter(event);
       }
     };
 
-    const mergedRootClassName = twMerge(
-      mergeClasses(
-        rootStyles.base,
-        rootStyles.positions[position],
-        rootStyles.colors[theme][color],
-        (state === States.ENTERING || state === States.ENTERED) &&
-          rootStyles.open,
-        rootClassName,
-        (state === States.ENTERING || state === States.ENTERED) &&
-          enterTransition
-      )
-    );
+    const onHandlerMouseLeave = (event: MouseEvent<HTMLElement>): void => {
+      if (followCursor) {
+        setCursorY(event.clientY);
+        setCursorX(event.clientX);
+      }
 
-    const rootNode = (
+      if (open === undefined && enterState) {
+        exit();
+      }
+
+      if (handler.props.onMouseLeave !== undefined) {
+        handler.props.onMouseLeave(event);
+      }
+    };
+
+    const onHandlerMouseMove = (event: MouseEvent<HTMLElement>): void => {
+      if (followCursor) {
+        setCursorY(event.clientY);
+        setCursorX(event.clientX);
+      }
+
+      if (handler.props.onMouseMove !== undefined) {
+        handler.props.onMouseMove(event);
+      }
+    };
+
+    if ((handler as Record<string, any>)?.ref === undefined || (handler as Record<string, any>).ref === null) {
+      handlerNode = cloneElement(handler, {
+        onMouseEnter: onHandlerMouseEnter,
+        onMouseLeave: onHandlerMouseLeave,
+        onMouseMove: onHandlerMouseMove,
+        ref: (element: HTMLElement) => {
+          componentsRef.current.handler = element;
+        }
+      });
+    } else {
+      componentsRef.current.handler = (handler as Record<string, any>).ref.current;
+
+      handlerNode = cloneElement(handler, {
+        onMouseEnter: onHandlerMouseEnter,
+        onMouseLeave: onHandlerMouseLeave,
+        onMouseMove: onHandlerMouseMove
+      });
+    }
+  }
+
+  /* Set arrow props */
+  if (arrow) {
+    const arrowStyles = styles.arrow;
+    const { className: arrowClassName, ...restArrowProps } = arrowProps;
+
+    const setArrowRef = (element: HTMLDivElement): void => {
+      componentsRef.current.arrow = element;
+    };
+
+    const mergedArrowClassName = mergeClasses(arrowStyles.base, arrowStyles.colors[theme][color], arrowClassName);
+
+    arrowNode = (
       <div
-        onTransitionEnd={transitionEndRootHandler}
-        style={mergedRootStyle}
-        className={mergedRootClassName}
-        ref={rootRef}
-        {...restRootProps}
-      >
+        className={mergedArrowClassName}
+        ref={setArrowRef}
+        {...restArrowProps}
+      />
+    );
+  }
+
+  /* Set root props */
+  const rootStyles = styles.root;
+
+  const transitionEndRootHandler = (event: TransitionEvent<HTMLDivElement>): void => {
+    if (transitionState === TransitionStates.ENTERING) {
+      enter(true);
+    }
+
+    if (transitionState === TransitionStates.EXITING) {
+      exit(true);
+    }
+
+    if (onRootTransitionEnd !== undefined) {
+      onRootTransitionEnd(event);
+    }
+  };
+
+  const animationEndRootHandler = (event: AnimationEvent<HTMLDivElement>): void => {
+    if (transitionState === TransitionStates.ENTERING) {
+      enter(true);
+    }
+
+    if (transitionState === TransitionStates.EXITING) {
+      exit(true);
+    }
+
+    if (onRootAnimationEnd !== undefined) {
+      onRootAnimationEnd(event);
+    }
+  };
+
+  const setRootRef = (element: HTMLDivElement): void => {
+    componentsRef.current.root = element;
+  };
+
+  const mergedRootStyle = mergeStyles(
+    {
+      opacity: `${transitionState === TransitionStates.ENTERING || transitionState === TransitionStates.ENTERED ? 100 : 0}`,
+      transitionDuration: `${enterState ? transitionConfig.enterDuration : transitionConfig.exitDuration}ms`
+    },
+    rootStyle
+  );
+
+  const mergedRootClassName = mergeClasses(rootStyles.base, rootStyles.colors[theme][color], rootClassName, transitionClassName);
+
+  const rootChildrenNode = componentsRef.current.root !== null &&
+    componentsRef.current.handler !== null &&
+    (!unmountOnExit || (unmountOnExit && transitionState !== TransitionStates.EXITED)) && (
+      <>
         {arrowNode}
         {rootChildren}
-      </div>
-    );
+      </>
+  );
 
-    return overlayRef === null ? rootNode : createPortal(rootNode, overlayRef);
-  }
-);
+  let rootNode = (
+    <div
+      onTransitionEnd={transitionEndRootHandler}
+      onAnimationEnd={animationEndRootHandler}
+      style={mergedRootStyle}
+      className={mergedRootClassName}
+      ref={setRootRef}
+      {...restRootProps}
+    >
+      {rootChildrenNode}
+    </div>
+  );
+
+  rootNode = overlayRef === null ? rootNode : createPortal(rootNode, overlayRef);
+
+  return (
+    <>
+      {handlerNode}
+      {rootNode}
+    </>
+  );
+});
 
 Tooltip.displayName = 'Tooltip';
 
-export default Tooltip; */
+export default Tooltip;
--- src/lib/simpleComponents/configs/tooltipConfig.ts
+++ src/lib/simpleComponents/configs/tooltipConfig.ts
@@ -1,36 +1,38 @@
-import { type BaseHTMLAttributes } from 'react';
+import { type ReactElement, type TransitionEventHandler, type AnimationEventHandler, type CSSProperties, type ReactNode, type BaseHTMLAttributes } from 'react';
 
-export type TooltipPositions = 'top' | 'top-start' | 'top-end' | 'bottom' | 'bottom-start' | 'bottom-end' | 'left' | 'left-start' | 'left-end' | 'right' | 'right-start' | 'right-end';
+export type TooltipPositions = Positions;
 export type TooltipColors = 'default' | 'light' | string;
 
 export interface TooltipConfig {
   defaultProps: {
-    anchorRef: Element | null;
-    overlayRef: Element | null;
-    position: TooltipPositions;
-    spacing: number;
+    open?: boolean;
     color: TooltipColors;
+    position: TooltipPositions;
+    gap: number;
+    responsive: boolean;
+    followCursor: boolean;
+    overlayRef: Element | null;
+    unmountOnExit: boolean;
+    transitionConfig: {
+      enterDuration: number,
+      exitDuration: number
+    };
+    handler?: ReactElement;
     arrow: boolean;
-    transitionProps: {
-      unmountOnExit: boolean;
-      enterTransition: string;
-      enterDuration: number;
-      exitDuration: number;
-    };
-    componentsProps: {
-      arrow: BaseHTMLAttributes<HTMLDivElement>;
-    };
+    arrowProps: BaseHTMLAttributes<HTMLDivElement>
+    onTransitionEnd?: TransitionEventHandler;
+    onAnimationEnd?: AnimationEventHandler;
+    style: CSSProperties;
+    className: string;
+    children?: ReactNode;
   };
   styles: {
     root: {
       base: Record<string, string>;
-      open: Record<string, string>;
-      positions: Record<TooltipPositions, Record<string, string>>;
       colors: Record<string, Record<TooltipColors, Record<string, string>>>
     },
     arrow: {
       base: Record<string, string>;
-      positions: Record<string, Record<string, string>>;
       colors: Record<string, Record<TooltipColors, Record<string, string>>>
     }
   }
@@ -38,80 +40,42 @@ export interface TooltipConfig {
 
 const tooltipConfig: TooltipConfig = {
   defaultProps: {
-    anchorRef: null,
-    overlayRef: null,
-    position: 'bottom',
-    spacing: 1,
     color: 'default',
-    arrow: false,
-    transitionProps: {
-      unmountOnExit: true,
-      enterTransition: '',
+    position: 'bottom',
+    gap: 1,
+    responsive: true,
+    followCursor: false,
+    overlayRef: null,
+    unmountOnExit: true,
+    transitionConfig: {
       enterDuration: 500,
       exitDuration: 500
     },
-    componentsProps: {
-      arrow: {}
-    }
+    arrow: false,
+    arrowProps: {},
+    style: {},
+    className: ''
   },
   styles: {
     root: {
       base: {
         position: 'absolute',
-        display: 'flex',
+        display: 'block',
         gap: 'gap-2',
         alignItems: 'items-center',
         padding: 'py-1.5 px-3',
         borderRadius: 'rounded-xl',
         font: 'antialiased font-normal text-sm font-sans',
-        opacity: 'opacity-0',
         transition: 'transition-[opacity]',
         focus: 'focus:outline-0'
-      },
-      open: {
-        opacity: 'opacity-100'
-      },
-      positions: {
-        top: {
-          translate: '-translate-y-full -translate-x-2/4'
-        },
-        'top-start': {
-          translate: '-translate-y-full'
-        },
-        'top-end': {
-          translate: '-translate-y-full -translate-x-full'
-        },
-        bottom: {
-          translate: '-translate-x-2/4'
-        },
-        'bottom-start': {},
-        'bottom-end': {
-          translate: '-translate-x-full'
-        },
-        left: {
-          translate: '-translate-y-2/4 -translate-x-full'
-        },
-        'left-start': {
-          translate: '-translate-x-full'
-        },
-        'left-end': {
-          translate: '-translate-y-full -translate-x-full'
-        },
-        right: {
-          translate: '-translate-y-2/4'
-        },
-        'right-start': {},
-        'right-end': {
-          translate: '-translate-y-full'
-        }
       },
       colors: {
         default: {
           default: {
-            background: 'bg-default-bg-dark/20'
+            background: 'bg-default-bg-dark/70'
           },
           light: {
-            background: 'bg-default-bg/20'
+            background: 'bg-default-bg/70'
           }
         }
       }
@@ -124,38 +88,13 @@ const tooltipConfig: TooltipConfig = {
         border: 'border-l-[0.25rem] border-l-transparent border-r-[0.25rem] border-r-transparent border-b-[0.5rem]',
         focus: 'focus:outline-0'
       },
-      positions: {
-        top: {
-          bottom: 'bottom-0',
-          left: 'left-2/4',
-          translate: 'translate-y-full -translate-x-1',
-          transform: 'rotate-180'
-        },
-        bottom: {
-          top: 'top-0',
-          left: 'left-2/4',
-          translate: '-translate-y-full -translate-x-1'
-        },
-        left: {
-          top: 'top-2/4',
-          left: 'right-0',
-          translate: '-translate-y-1 translate-x-full',
-          transform: 'rotate-90'
-        },
-        right: {
-          top: 'top-2/4',
-          left: 'left-0',
-          translate: '-translate-y-1 -translate-x-full',
-          transform: '-rotate-90'
-        }
-      },
       colors: {
         default: {
           default: {
-            border: 'border-b-default-bg-dark/20'
+            border: 'border-b-default-bg-dark/70'
           },
           light: {
-            border: 'border-b-default-bg/20'
+            border: 'border-b-default-bg/70'
           }
         }
       }
