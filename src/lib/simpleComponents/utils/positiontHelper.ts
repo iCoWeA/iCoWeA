b775146837cf3c06bee4cd3a95b30726edc3bd65
Richard Wagner <richard.wagner@gmail.com>
2023-08-23 13:42:36 +0200

    fix

--- src/lib/simpleComponents/utils/positiontHelper.ts
+++ src/lib/simpleComponents/utils/positiontHelper.ts
@@ -3,84 +3,134 @@ export interface Position {
   horizontal: 'left' | 'center' | 'right';
 }
 
-export interface Cords {
-  top: number;
-  left: number;
-}
-
 export const getViewportCords = (): { top: number; bottom: number; left: number; right: number; } => ({ top: document.documentElement.scrollTop, left: document.documentElement.scrollLeft, bottom: document.documentElement.scrollTop + document.documentElement.clientHeight, right: document.documentElement.scrollLeft + document.documentElement.clientWidth });
 
-export const calculateResponsiveCords = (anchor?: HTMLElement | null, element?: HTMLElement | null, position: Positions = 'bottom', gap: number = 0, responsive: boolean = false): Cords => {
-  const cords: Cords = { top: 0, left: 0 };
-
-  if (anchor === undefined || anchor === null || element === undefined || element === null) {
-    return cords;
+export const setElementPosition = (element: HTMLElement | null, position: Positions, anchorTop: number = 0, anchorLeft: number = 0, anchorHeight: number = 0, anchorWidth: number = 0, gap: number = 0, responsive: boolean = false): Positions => {
+  if (element === null) {
+    return position;
   }
 
+  const cords = { top: anchorTop, left: anchorLeft };
   const positions = position.split('-');
   const viewportCords = getViewportCords();
 
   gap = gap * parseFloat(getComputedStyle(document.documentElement).fontSize);
 
   if (positions[0] === 'top') {
-    cords.top = anchor.offsetTop - gap - element.offsetHeight;
-    cords.top = responsive && cords.top < viewportCords.top ? anchor.offsetTop + gap + anchor.offsetHeight : cords.top;
+    cords.top = anchorTop - gap - element.offsetHeight;
+    position = 'top';
+
+    if (responsive && cords.top < viewportCords.top) {
+      cords.top = anchorTop + anchorHeight + gap;
+      position = 'bottom';
+    }
   }
 
   if (positions[0] === 'bottom') {
-    cords.top = anchor.offsetTop + gap + anchor.offsetHeight;
-    cords.top = responsive && cords.top + element.offsetHeight > viewportCords.bottom ? anchor.offsetTop - gap - element.offsetHeight : cords.top;
-  }
+    cords.top = anchorTop + anchorHeight + gap;
+    position = 'bottom';
 
-  if ((positions[0] === 'left' || positions[0] === 'right') && positions[1] === 'start') {
-    cords.top = anchor.offsetTop;
-    cords.top = responsive && cords.top + element.offsetHeight > viewportCords.bottom ? anchor.offsetTop + anchor.offsetHeight - element.offsetHeight : cords.top;
-  }
-
-  if ((positions[0] === 'left' || positions[0] === 'right') && positions[1] === 'end') {
-    cords.top = anchor.offsetTop + anchor.offsetHeight - element.offsetHeight;
-    cords.top = responsive && cords.top < viewportCords.top ? anchor.offsetTop : cords.top;
-  }
-
-  if ((positions[0] === 'left' || positions[0] === 'right') && positions[1] === undefined) {
-    cords.top = anchor.offsetTop + (anchor.offsetHeight / 2) - (element.offsetHeight / 2);
-    cords.top = responsive && cords.top < viewportCords.top && cords.top + element.offsetHeight < viewportCords.bottom ? viewportCords.top : cords.top;
-    cords.top = responsive && cords.top > anchor.offsetTop ? anchor.offsetTop : cords.top;
-    cords.top = responsive && cords.top > viewportCords.top && cords.top + element.offsetHeight > viewportCords.bottom ? viewportCords.bottom - element.offsetHeight : cords.top;
-    cords.top = responsive && cords.top + element.offsetHeight < anchor.offsetTop + anchor.offsetHeight ? anchor.offsetTop + anchor.offsetHeight - element.offsetHeight : cords.top;
-  }
-
-  if ((positions[0] === 'top' || positions[0] === 'bottom') && positions[1] === 'start') {
-    cords.left = anchor.offsetLeft;
-    cords.left = responsive && cords.left + element.offsetWidth > viewportCords.right ? anchor.offsetLeft + anchor.offsetWidth - element.offsetWidth : cords.left;
-  }
-
-  if ((positions[0] === 'top' || positions[0] === 'bottom') && positions[1] === 'end') {
-    cords.left = anchor.offsetLeft + anchor.offsetWidth - element.offsetWidth;
-    cords.left = responsive && cords.left < viewportCords.left ? anchor.offsetLeft : cords.left;
-  }
-
-  if ((positions[0] === 'top' || positions[0] === 'bottom') && positions[1] === undefined) {
-    cords.left = anchor.offsetLeft + (anchor.offsetWidth / 2) - (element.offsetWidth / 2);
-    cords.left = responsive && cords.left + element.offsetWidth > viewportCords.right && cords.left > viewportCords.left ? viewportCords.right - element.offsetWidth : cords.left;
-    cords.left = responsive && cords.left + element.offsetWidth < anchor.offsetLeft + anchor.offsetWidth ? anchor.offsetLeft + anchor.offsetWidth - element.offsetWidth : cords.left;
-    cords.left = responsive && cords.left < viewportCords.left && cords.left + element.offsetWidth < viewportCords.right ? viewportCords.left : cords.left;
-    cords.left = responsive && cords.left > anchor.offsetLeft ? anchor.offsetLeft : cords.left;
+    if (responsive && cords.top + element.offsetHeight > viewportCords.bottom) {
+      cords.top = anchorTop - gap - element.offsetHeight;
+      position = 'top';
+    }
   }
 
   if (positions[0] === 'left') {
-    cords.left = anchor.offsetLeft - gap - element.offsetWidth;
-    cords.left = responsive && cords.left < viewportCords.left ? anchor.offsetLeft + gap + anchor.offsetWidth : cords.left;
+    cords.left = anchorLeft - gap - element.offsetWidth;
+    position = 'left';
+
+    if (responsive && cords.left < viewportCords.left) {
+      cords.left = anchorLeft + anchorWidth + gap;
+      position = 'right';
+    }
   }
 
   if (positions[0] === 'right') {
-    cords.left = anchor.offsetLeft + gap + anchor.offsetWidth;
-    cords.left = responsive && cords.left > viewportCords.right ? anchor.offsetLeft - gap - element.offsetWidth : cords.left;
+    cords.left = anchorLeft + gap + anchorWidth;
+    position = 'right';
+
+    if (responsive && cords.left + element.offsetWidth > viewportCords.right) {
+      cords.left = anchorLeft - gap - element.offsetWidth;
+      position = 'left';
+    }
   }
 
-  return cords;
+  if ((positions[0] === 'top' || positions[0] === 'bottom') && positions[1] === 'start') {
+    cords.left = anchorLeft;
+    position += '-start';
+
+    if (responsive && cords.left + element.offsetWidth > viewportCords.right) {
+      cords.left = anchorLeft + anchorWidth - element.offsetWidth;
+      position = position === 'top' ? 'top-end' : 'bottom-end';
+    }
+  }
+
+  if ((positions[0] === 'top' || positions[0] === 'bottom') && positions[1] === 'end') {
+    cords.left = anchorLeft + anchorWidth - element.offsetWidth;
+    position += '-end';
+
+    if (responsive && cords.left < viewportCords.left) {
+      cords.left = anchorLeft;
+      position = position === 'top' ? 'top-start' : 'bottom-start';
+    }
+  }
+
+  if ((positions[0] === 'top' || positions[0] === 'bottom') && positions[1] === undefined) {
+    cords.left = anchorLeft + (anchorWidth / 2) - (element.offsetWidth / 2);
+
+    if (responsive && cords.left + element.offsetWidth > viewportCords.right && cords.left > viewportCords.left) {
+      cords.left = viewportCords.right - element.offsetWidth;
+      cords.left = cords.left + element.offsetWidth < anchorLeft + anchorWidth ? anchorLeft + anchorWidth - element.offsetWidth : cords.left;
+    }
+
+    if (responsive && cords.left < viewportCords.left && cords.left + element.offsetWidth < viewportCords.right) {
+      cords.left = viewportCords.left;
+      cords.left = cords.left > anchorLeft ? anchorLeft : cords.left;
+    }
+  }
+
+  if ((positions[0] === 'left' || positions[0] === 'right') && positions[1] === 'start') {
+    cords.top = anchorTop;
+    position += '-start';
+
+    if (responsive && cords.top + element.offsetHeight > viewportCords.bottom) {
+      cords.top = anchorTop + anchorHeight - element.offsetHeight;
+      position = position === 'left' ? 'left-end' : 'right-end';
+    }
+  }
+
+  if ((positions[0] === 'left' || positions[0] === 'right') && positions[1] === 'end') {
+    cords.top = anchorTop + anchorHeight - element.offsetHeight;
+    position += '-end';
+
+    if (responsive && cords.top < viewportCords.top) {
+      cords.top = anchorTop;
+      position = position === 'left' ? 'left-start' : 'right-start';
+    }
+  }
+
+  if ((positions[0] === 'left' || positions[0] === 'right') && positions[1] === undefined) {
+    cords.top = anchorTop + (anchorHeight / 2) - (element.offsetHeight / 2);
+
+    if (responsive && cords.top < viewportCords.top && cords.top + element.offsetHeight < viewportCords.bottom) {
+      cords.top = viewportCords.top;
+      cords.top = cords.top > anchorTop ? anchorTop : cords.top;
+    }
+
+    if (responsive && cords.top > viewportCords.top && cords.top + element.offsetHeight > viewportCords.bottom) {
+      cords.top = viewportCords.bottom - element.offsetHeight;
+      cords.top = cords.top + element.offsetHeight < anchorTop + anchorHeight ? anchorTop + anchorHeight - element.offsetHeight : cords.top;
+    }
+  }
+
+  element.style.top = `${cords.top}px`;
+  element.style.left = `${cords.left}px`;
+
+  return position;
 };
 
+/*
 export const calculateCords = (anchor?: HTMLElement | null, element?: HTMLElement | null, position?: Position, transformPosition?: Position): Cords => {
   const cords: Cords = { top: 0, left: 0 };
 
@@ -124,4 +174,4 @@ export const calculateCords = (anchor?: HTMLElement | null, element?: HTMLElemen
   }
 
   return cords;
-};
+}; */
