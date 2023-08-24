0b20386753b5dce3986e096ff0d1635e4c8526ce
Richard Wagner <richard.wagner@gmail.com>
2023-08-24 18:15:11 +0200

    fix

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,27 @@
+import React, { type FC } from 'react';
+import Main from '../../lib/simpleComponents/components/layouts/Main';
+import LoginForm from './LoginForm';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase';
+import { redirect } from 'react-router-dom';
+
+export const Component: FC = () => (
+  <Main
+    color="primary"
+    className="justify-center"
+  >
+    <LoginForm />
+  </Main>
+);
+
+Component.displayName = 'LoginRoute';
+
+export const action = async ({ request }: { request: Request }): Promise<unknown> => {
+  const formData = await request.formData();
+  const email = formData.get('email')?.toString() ?? '';
+  const password = formData.get('password')?.toString() ?? '';
+
+  await signInWithEmailAndPassword(auth, email, password);
+
+  return redirect('/admin');
+};
