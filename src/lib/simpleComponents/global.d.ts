62394f6488e29e32bf8c6663acf7e711c3b48b80
Richard Wagner <richard.wagner@gmail.com>
2023-08-24 21:31:17 +0200

    fix

--- src/lib/simpleComponents/global.d.ts
+++ src/lib/simpleComponents/global.d.ts
@@ -1,5 +1,8 @@
 export {};
 
 declare global {
+  export type Colors = 'default' | 'primary' | 'secondary' | 'success' | 'warning' | 'error' | 'light' | 'dark' | string;
+  export type Sizes = 'sm' | 'md' | 'lg';
+  export type Aligns = 'left' | 'center' | 'right' | 'justify' | 'start' | 'end';
   export type Positions = 'top' | 'top-start' | 'top-end' | 'bottom' | 'bottom-start' | 'bottom-end' | 'left' | 'left-start' | 'left-end' | 'right' | 'right-start' | 'right-end';
 };
