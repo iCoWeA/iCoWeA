2a43c305b935e284132b4bcf23a317acaffed2b4
Richard Wagner <richard.wagner@gmail.com>
2023-08-28 01:10:26 +0200

    fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,23 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from 'firebase/database';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCZW3K1C4wBCBlIHprAhS1Zh8u2kqEM95k',
-  authDomain: 'icowea-b35d1.firebaseapp.com',
-  databaseURL: 'https://icowea-b35d1-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'icowea-b35d1',
-  storageBucket: 'icowea-b35d1.appspot.com',
-  messagingSenderId: '536815551625',
-  appId: '1:536815551625:web:4b140732c096bfcffcf258',
-  measurementId: 'G-CWBL20TBRZ'
+  apiKey: 'AIzaSyC1CNW0Q-6I7aQqTxBn-JECIIXyUOIACsQ',
+  authDomain: 'icowea-ed3ee.firebaseapp.com',
+  databaseURL: 'https://icowea-ed3ee-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'icowea-ed3ee',
+  storageBucket: 'icowea-ed3ee.appspot.com',
+  messagingSenderId: '450764627942',
+  appId: '1:450764627942:web:59554908698e5298528027',
+  measurementId: 'G-PEZ0YBCT37'
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const database = getDatabase(app);
