773f3d608ab3f16891278c5801eb354fa6a4b848
Richard Wagner <richard.wagner@gmail.com>
2023-08-27 22:42:52 +0200

    fix

---
+++ src/pages/DashboardLayout/DashboardHeader.tsx
@@ -0,0 +1,32 @@
+import React, { type FC } from 'react';
+import useBreakpoint, { Breakpoints } from '../../lib/simpleComponents/hooks/useBreakpoint';
+import Header from '../../lib/simpleComponents/components/layouts/Header';
+import DashboardMenuButton from './DashboardMenuButton';
+import DashboardLogo from './DashboardLogo';
+import DashboardAvatar from './DashboardAvatar';
+
+const DashboardHeader: FC = () => {
+  const breakpoint = useBreakpoint();
+
+  if (breakpoint === Breakpoints.SM) {
+    return (
+      <Header flex>
+        <DashboardMenuButton />
+        <DashboardLogo />
+        <DashboardAvatar />
+      </Header>
+    );
+  }
+
+  return (
+    <Header
+      flex
+      className="md:pl-6"
+    >
+      <DashboardLogo />
+      <DashboardAvatar />
+    </Header>
+  );
+};
+
+export default DashboardHeader;
