b3d392ee5b0428ede89023cd6944be71558d9cdf
Richard Wagner <richard.wagner@gmail.com>
2023-08-27 15:31:32 +0200

    fix

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,18 +1,10 @@
 import React, { type FC } from 'react';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
-import Tooltip from '../../lib/simpleComponents/components/UI/Tooltip';
-import Button from '../../lib/simpleComponents/components/UI/Button';
 
 const Component: FC = () => (
   <>
     <ScrollRestoration />
     <Outlet />
-    <Tooltip arrow handler={<Button>B</Button>}color="default" >Text</Tooltip>
-    <Tooltip arrow handler={<Button>B</Button>}color="dark" >Text</Tooltip>
-    <Tooltip arrow handler={<Button>B</Button>}color="primary" >Text</Tooltip>
-    <div className="bg-default-primary">
-      <Tooltip arrow handler={<Button>B</Button>}color="light" >Text</Tooltip>
-    </div>
   </>
 );
 
