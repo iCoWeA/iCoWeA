3bd7530cfd7820e03f01e80b55525b23c9f8d047
Richard Wagner <richard.wagner@gmail.com>
2023-08-27 21:50:53 +0200

    logo

---
+++ src/pages/DashboardLayout/DashboardLogo.tsx
@@ -0,0 +1,16 @@
+import React, { type FC } from 'react';
+import Link from '../../lib/simpleComponents/components/UI/Link';
+
+const DashboardLogo: FC = () => (
+  <Link
+    to="/"
+    className="h-10"
+  >
+    <img
+      src={require('../../assets/images/logo.png')}
+      alt="logo"
+    />
+  </Link>
+);
+
+export default DashboardLogo;
