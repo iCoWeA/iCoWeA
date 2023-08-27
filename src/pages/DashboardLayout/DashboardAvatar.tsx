905c186caaebf00b172b49190af71649b045b27b
Richard Wagner <richard.wagner@gmail.com>
2023-08-27 22:46:29 +0200

    fix

--- src/pages/DashboardLayout/DashboardAvatar.tsx
+++ src/pages/DashboardLayout/DashboardAvatar.tsx
@@ -1,45 +1,47 @@
 import React, { type FC } from 'react';
 import Menu from '../../lib/simpleComponents/components/UI/Menu';
+import MenuBody from '../../lib/simpleComponents/components/UI/MenuBody';
 import List from '../../lib/simpleComponents/components/UI/List';
 import ListItemButton from '../../lib/simpleComponents/components/UI/ListItemButton';
 import Link from '../../lib/simpleComponents/components/UI/Link';
 import Avatar from '../../lib/simpleComponents/components/UI/Avatar';
 
-const DashboardAvatar: FC = () => (
-  <Menu
-    elevated
-    handler={
-      <Avatar
-        src={require('../../assets/images/photo.png')}
-        className="ml-auto w-10"
-      />
-    }
-  >
-    <List>
-      <ListItemButton
-        color="dark"
-        fullwidth
-      >
-        <Link
-          to="/admin/settings"
-          color="dark"
-        >
-          Settings
-        </Link>
-      </ListItemButton>
-      <ListItemButton
-        color="dark"
-        fullwidth
-      >
-        <Link
-          to="/logout"
-          color="dark"
-        >
-          Logout
-        </Link>
-      </ListItemButton>
-    </List>
-  </Menu>
-);
+const DashboardAvatar: FC = () => {
+  const overlay = document.getElementById('overlay');
+
+  return (
+    <Menu
+      elevated
+      overlayRef={overlay}
+      handler={
+        <Avatar
+          src={require('../../assets/images/photo.png')}
+          className="ml-auto w-10"
+        />
+      }
+    >
+      <MenuBody>
+        <List>
+          <ListItemButton color="dark">
+            <Link
+              to="/admin/settings"
+              color="dark"
+            >
+              Settings
+            </Link>
+          </ListItemButton>
+          <ListItemButton color="dark">
+            <Link
+              to="/logout"
+              color="dark"
+            >
+              Logout
+            </Link>
+          </ListItemButton>
+        </List>
+      </MenuBody>
+    </Menu>
+  );
+};
 
 export default DashboardAvatar;
