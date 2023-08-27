414469d4a7270e75bb061f18cd712e91f326ef25
Richard Wagner <richard.wagner@gmail.com>
2023-08-28 01:36:41 +0200

    fix

---
+++ src/pages/AdminMessages/index.tsx
@@ -0,0 +1,17 @@
+import React, { type FC } from 'react';
+import Main from '../../lib/simpleComponents/components/layouts/Main';
+import Messages from './Messages';
+import { type DeferredData } from '@remix-run/router/dist/utils';
+import { defer } from 'react-router-dom';
+import { child, get, ref } from 'firebase/database';
+import { database } from '../../firebase';
+
+export const Component: FC = () => (
+  <Main>
+    <Messages />
+  </Main>
+);
+
+Component.defaultProps = 'Component';
+
+export const loader = (): DeferredData => defer({ snapchot: get(child(ref(database), 'messages')) });
