c4b8854dc45af985322e8b5b2059fe09e2b9655d
Richard Wagner <richard.wagner@gmail.com>
2023-08-27 23:00:47 +0200

    fix

--- src/pages/DashboardLayout/DashboardMenuButton.tsx
+++ src/pages/DashboardLayout/DashboardMenuButton.tsx
@@ -12,6 +12,7 @@ const DashboardMenuButton: FC = () => {
       onClick={() => {
         dispatch(navMenu.actions.toggle());
       }}
+      className="shrink-0"
     >
       <Icon>
         <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
