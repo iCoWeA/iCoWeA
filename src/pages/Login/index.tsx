1fb981004d7227de41c38d35e611ed8f8a4069b7
Richard Wagner <richard.wagner@gmail.com>
2023-08-28 16:48:27 +0200

    fix

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, type FC } from 'react';
 import { redirect, useActionData, useNavigation } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import statusAlert from '../../store/Slices/StatusAlert';
+import { useDispatch, useSelector } from 'react-redux';
+import statusAlert, { selectState } from '../../store/Slices/StatusAlert';
 import Main from '../../lib/simpleComponents/components/layouts/Main';
 import LoginForm from './LoginForm';
 import { signInWithEmailAndPassword } from 'firebase/auth';
@@ -11,14 +11,17 @@ export const Component: FC = () => {
   const navigation = useNavigation();
   const error = useActionData();
   const dispatch = useDispatch();
+  const { open } = useSelector(selectState);
 
   useEffect(() => {
     if (navigation.state === 'idle' && error !== undefined) {
       dispatch(statusAlert.actions.show({ props: { color: 'error', children: typeof error === 'string' ? error : '' } }));
     } else {
-      dispatch(statusAlert.actions.hide());
+      if (open) {
+        dispatch(statusAlert.actions.hide());
+      }
     }
-  }, [navigation.state, error]);
+  }, [navigation.state, error, open]);
 
   return (
     <Main
