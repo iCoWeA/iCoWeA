0b32f5e78496289f8a15dd43148398804703256c
Richard Wagner <richard.wagner@gmail.com>
2023-08-28 16:16:18 +0200

    fix

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,5 +1,7 @@
-export const namePattern = '^[A-Za-z]{1,32}$';
+export const NAME_PATTERN = '^[A-Za-z]{1,32}$';
 
-export const emailPattern = '^[\\w\\.]+@[\\w\\.]+$';
+export const EMAIL_PATTERN = '^[\\w\\.]+@[\\w\\.]+$';
 
-export const expirationDuration = 30;
+export const EXPIRATION_DURATION = 30;
+
+export const ALERT_TIMER = 5000;
--- src/store/Slices/StatusAlert.ts
+++ src/store/Slices/StatusAlert.ts
@@ -1,20 +1,29 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import { ALERT_TIMER } from '../../data/constants';
 import { type AlertProps } from '../../lib/simpleComponents/components/UI/Alert';
 import { type StoreState } from '..';
 
 interface State {
   open: boolean;
+  closable: boolean;
+  timer: number;
   props: AlertProps;
 }
 
-const initialState: State = { open: false, props: {} };
+interface ShowAction {
+  closable?: boolean;
+  timer?: number;
+  props?: AlertProps
+}
+
+const initialState: State = { open: false, closable: true, timer: ALERT_TIMER, props: {} };
 
 const statusAlert = createSlice({
   name: 'statusAlert',
   initialState,
   reducers: {
-    show (_, action: PayloadAction<AlertProps>) { return { open: true, props: action.payload }; },
-    hide (prevState) { return { open: false, props: prevState.props }; }
+    show (_, { payload: { closable = true, timer = ALERT_TIMER, props = {} } }: PayloadAction<ShowAction>) { return { open: true, closable, timer, props }; },
+    hide (prevState) { return { open: false, closable: prevState.closable, timer: prevState.timer, props: prevState.props }; }
   }
 });
 
