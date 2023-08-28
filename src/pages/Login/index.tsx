cb68f9017417406edcfb054698e0e46cf59c138a
Richard Wagner <richard.wagner@gmail.com>
2023-08-28 12:32:20 +0200

    fix

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,20 +1,51 @@
-import React, { type FC } from 'react';
+import React, { useEffect, useRef, type FC } from 'react';
+import { redirect, useActionData, useNavigation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import statusAlert from '../../store/Slices/StatusAlert';
 import Main from '../../lib/simpleComponents/components/layouts/Main';
 import LoginForm from './LoginForm';
-import ErrorAlert from './ErrorAlert';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase';
-import { redirect } from 'react-router-dom';
 
-export const Component: FC = () => (
-  <Main
-    color="primary"
-    className="justify-center"
-  >
-    <LoginForm />
-    <ErrorAlert />
-  </Main>
-);
+const TIMER = 5000;
+
+export const Component: FC = () => {
+  const navigation = useNavigation();
+  const error = useActionData();
+  const dispatch = useDispatch();
+
+  const timerId = useRef(-1);
+
+  const closeHandler = (): void => {
+    clearTimeout(timerId.current);
+    dispatch(statusAlert.actions.hide());
+  };
+
+  useEffect(() => {
+    clearTimeout(timerId.current);
+  }, [navigation.state]);
+
+  useEffect(() => {
+    if (navigation.state === 'idle' && error !== undefined) {
+      dispatch(statusAlert.actions.show({ color: 'error', onClose: closeHandler }));
+
+      timerId.current = window.setTimeout(() => {
+        dispatch(statusAlert.actions.hide());
+      }, TIMER);
+    } else {
+      dispatch(statusAlert.actions.hide());
+    }
+  }, [navigation.state, error]);
+
+  return (
+    <Main
+      color="primary"
+      className="justify-center"
+    >
+      <LoginForm />
+    </Main>
+  );
+};
 
 Component.displayName = 'LoginRoute';
 
