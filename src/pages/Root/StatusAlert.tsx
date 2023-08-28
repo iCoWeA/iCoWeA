36f295691810d5d385b8bf33fc974fbdc9b44de3
Richard Wagner <richard.wagner@gmail.com>
2023-08-28 16:42:17 +0200

    fix

--- src/pages/Root/StatusAlert.tsx
+++ src/pages/Root/StatusAlert.tsx
@@ -1,23 +1,56 @@
-import React, { type FC } from 'react';
-import { useSelector } from 'react-redux';
-import { selectState } from '../../store/Slices/StatusAlert';
+import React, { useRef, type FC, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import statusAlert, { selectState } from '../../store/Slices/StatusAlert';
 import Popover from '../../lib/simpleComponents/components/UI/Popover';
 import Alert from '../../lib/simpleComponents/components/UI/Alert';
 
 const StatusAlert: FC = () => {
-  const { open, props: alertProps } = useSelector(selectState);
+  const { open, closable, timer, props: alertProps } = useSelector(selectState);
+  const dispatch = useDispatch();
 
-  const overlayRef = document.getElementById('overlay');
+  const timerId = useRef(-1);
+
+  useEffect(() => {
+    if (open && timer >= 0) {
+      timerId.current = window.setTimeout(() => {
+        dispatch(statusAlert.actions.hide());
+      }, timer);
+    }
+
+    if (!open) {
+      clearTimeout(timerId.current);
+    }
+  }, [open, timer]);
+
+  /* Set root props */
+  const rootOverlayRef = document.getElementById('overlay');
+
+  /* Set alert props */
+  const { onClose: onAlertClose, ...restAlertProps } = alertProps;
+  let closeAlertHandler = onAlertClose;
+
+  if (closable) {
+    closeAlertHandler = () => {
+      dispatch(statusAlert.actions.hide());
+
+      if (onAlertClose !== undefined) {
+        onAlertClose();
+      }
+    };
+  }
 
   return (
     <Popover
       open={open}
-      overlayRef={overlayRef}
+      overlayRef={rootOverlayRef}
       disableOutsideClick
       unmountOnExit
       className="fixed bottom-4 w-full px-[16px] md:px-[32px]"
     >
-      <Alert {...alertProps} />
+      <Alert
+        onClose={closeAlertHandler}
+        {...restAlertProps}
+      />
     </Popover>
   );
 };
