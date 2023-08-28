0c8770cacdedb3687cef1ea63250c1b17dc371cd
Richard Wagner <richard.wagner@gmail.com>
2023-08-28 08:53:33 +0200

    fix

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,12 +1,18 @@
-import React, { type FC } from 'react';
+import React, { useEffect, type FC } from 'react';
 import { ScrollRestoration, Outlet } from 'react-router-dom';
 
-const Component: FC = () => (
-  <>
-    <ScrollRestoration />
-    <Outlet />
-  </>
-);
+const Component: FC = () => {
+  useEffect(() => {
+    document.body.classList.add('bg-default-default');
+  }, []);
+
+  return (
+    <>
+      <ScrollRestoration />
+      <Outlet />
+    </>
+  );
+};
 
 export default Component;
 
