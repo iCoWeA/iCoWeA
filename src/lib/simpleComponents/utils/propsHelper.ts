efa17fa7b1d3337c8563f6e018e2804c18aad84d
Richard Wagner <richard.wagner@gmail.com>
2023-08-29 20:55:46 +0200

    fix

--- src/lib/simpleComponents/utils/propsHelper.ts
+++ src/lib/simpleComponents/utils/propsHelper.ts
@@ -13,14 +13,14 @@ export const mergeClasses = (...classNames: Array<Record<string, string> | undef
 
 export const isLast = (array: any[], index: number): boolean => (array.length - 1 === index);
 
-export const deepClone = (object: Record<string, any>): Record<string, any> => {
+export const deepClone = <T>(object: T): T => {
   const newObject: Record<string, any> = {};
 
   for (const key in object) {
     newObject[key] = object[key] === 'object' ? deepClone(object[key]) : object[key];
   }
 
-  return newObject;
+  return newObject as T;
 };
 
 export const mergeProps = <T extends object, V extends object>(defaultProps: T, props: V): T & V => ({ ...defaultProps, ...props });
