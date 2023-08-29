b27a73958eb35f12e569b96842ecbe86d5d69212
Richard Wagner <richard.wagner@gmail.com>
2023-08-29 15:44:07 +0200

    fix

--- src/pages/AdminMessages/Messages/Messages.tsx
+++ src/pages/AdminMessages/Messages/Messages.tsx
@@ -1,33 +1,22 @@
-import React, { type FC, useEffect } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import useDatabase from '../../../hooks/useDatabase';
-import { database } from '../../../firebase';
-import { child, get, ref, type DataSnapshot } from 'firebase/database';
-import statusAlert from '../../../store/Slices/statusAlert';
+import React, { type FC, Suspense, type ReactNode, useRef } from 'react';
+import { Await, useLoaderData } from 'react-router-dom';
+import { type DataSnapshot } from 'firebase/database';
 import Section from '../../../lib/simpleComponents/components/layouts/Section';
 import Container from '../../../lib/simpleComponents/components/UI/Container';
 import MessagesCard from './MessagesCard';
 
 const Messages: FC = () => {
   const deferedMessages = useLoaderData() as { snapchot: Promise<DataSnapshot> };
-  const dispatch = useDispatch();
 
-  const { state, resolve } = useDatabase();
-  const { error, data } = state;
+  const data = useRef<Record<string, Message> | null>(null);
 
-  useEffect(() => {
-    void resolve(deferedMessages.snapchot);
-  }, [deferedMessages]);
+  /* Set fallback props */
+  const fallbackNode = <MessagesCard>{data.current}</MessagesCard>;
 
-  useEffect(() => {
-    if (error !== null) {
-      dispatch(statusAlert.actions.show({ props: { color: 'error', children: 'Fetching messages error' } }));
-    }
-  }, [error]);
-
-  const refetchMessages = (): void => {
-    void resolve(get(child(ref(database), 'messages')));
+  /* Set children props */
+  const resolvedNode = (resolvedData: any): ReactNode => {
+    data.current = resolvedData;
+    return <MessagesCard>{resolvedData}</MessagesCard>;
   };
 
   return (
@@ -36,7 +25,9 @@ const Messages: FC = () => {
         variant="column"
         className="px-[16px] md:pl-6 md:pr-[32px]"
       >
-        <MessagesCard onRefetch={refetchMessages}>{data as Record<string, Message> | null}</MessagesCard>
+        <Suspense fallback={fallbackNode}>
+          <Await resolve={deferedMessages.snapchot}>{resolvedNode}</Await>
+        </Suspense>
       </Container>
     </Section>
   );
