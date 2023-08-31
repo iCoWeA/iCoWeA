60d9db8b2e9cb1fedc6da2af14525733981f9531
Richard Wagner <richard.wagner@gmail.com>
2023-08-31 17:17:15 +0200

    fix

--- src/lib/simpleComponents/components/UI/AccordionBody/AccordionBody.tsx
+++ src/lib/simpleComponents/components/UI/AccordionBody/AccordionBody.tsx
@@ -1,7 +1,7 @@
 import React, { type BaseHTMLAttributes, forwardRef, useContext } from 'react';
 import Collapse, { type CollapseProps } from '../Collapse';
 import accordionContext from '../../../contexts/accordion';
-import themeContext from '../../../contexts/theme';
+import accordionBodyConfig from '../../../configs/accordionBodyConfig';
 import { mergeProps } from '../../../utils/propsHelper';
 
 export interface AccordionBodyProps extends BaseHTMLAttributes<HTMLDivElement> {
@@ -12,8 +12,7 @@ export interface AccordionBodyProps extends BaseHTMLAttributes<HTMLDivElement> {
 const AccordionBody = forwardRef<HTMLDivElement, AccordionBodyProps>((props, ref) => {
   /* --- Set default props --- */
   const { open: isOpen, duration } = useContext(accordionContext);
-  const defaultProps = useContext(themeContext).config.accordionBody.defaultProps;
-  const { collapseProps, className: containerClassName, ...restContainerProps } = mergeProps(defaultProps, props);
+  const { collapseProps, className: containerClassName, ...restContainerProps } = mergeProps(accordionBodyConfig.defaultProps, props);
 
   /* --- Set props --- */
   const { open, transitionConfig, ...restProps } = mergeProps({ open: isOpen }, collapseProps);
