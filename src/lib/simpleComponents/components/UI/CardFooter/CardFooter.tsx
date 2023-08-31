4722a50f946a98f49a63f0e4d6f63f6954e7a967
Richard Wagner <richard.wagner@gmail.com>
2023-08-31 17:50:01 +0200

    fix

---
+++ src/lib/simpleComponents/components/UI/CardFooter/CardFooter.tsx
@@ -0,0 +1,30 @@
+import React, { type BaseHTMLAttributes, forwardRef } from 'react';
+import cardFooterConfig from '../../../configs/cardFooterConfig';
+import { mergeClasses, mergeProps } from '../../../utils/propsHelper';
+
+export interface CardFooterProps extends BaseHTMLAttributes<HTMLDivElement> {
+  columns?: boolean;
+  fullwidht?: boolean;
+  className?: string;
+}
+
+const CardFooter = forwardRef<HTMLDivElement, CardFooterProps>((props, ref) => {
+  /* --- Set default props --- */
+  const { defaultProps, styles } = cardFooterConfig;
+  const { columns, fullwidht, className, ...restProps } = mergeProps(defaultProps, props);
+
+  /* --- Set props --- */
+  const mergedClassName = mergeClasses(styles.base, columns && styles.columns, fullwidht && styles.fullwidth, className);
+
+  return (
+    <div
+      className={mergedClassName}
+      ref={ref}
+      {...restProps}
+    />
+  );
+});
+
+CardFooter.displayName = 'CardFooter';
+
+export default CardFooter;
