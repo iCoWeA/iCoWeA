f8b988b1c1398ef23b0a8993ed7c7bfc5c10dae7
Richard Wagner <richard.wagner@gmail.com>
2023-08-31 19:51:39 +0200

    fix

---
+++ src/lib/simpleComponents/components/UI/Accordion/AccordionBody.tsx
@@ -0,0 +1,41 @@
+import React, { type BaseHTMLAttributes, forwardRef, useContext } from 'react';
+import Collapse, { type CollapseProps } from '../Collapse/Collapse';
+import accordionContext from '../../../contexts/accordion';
+import accordionBodyConfig from '../../../configs/accordionBodyConfig';
+import { mergeProps } from '../../../utils/propsHelper';
+import AccordionBodyContainer from './AccordionBodyContainer';
+
+export interface AccordionBodyProps extends BaseHTMLAttributes<HTMLDivElement> {
+  collapseProps?: CollapseProps;
+  className?: string;
+}
+
+const AccordionBody = forwardRef<HTMLDivElement, AccordionBodyProps>((props, ref) => {
+  /* --- Set context props --- */
+  const { open: isOpen, duration } = useContext(accordionContext);
+
+  /* --- Set default props --- */
+  const { collapseProps, className: containerClassName, ...restContainerProps } = mergeProps(accordionBodyConfig.defaultProps, props);
+
+  /* --- Set props --- */
+  const { open, transitionConfig, ...restProps } = mergeProps({ open: isOpen }, collapseProps);
+  const mergedTransitionConfig = mergeProps({ enterDuration: duration, exitDuration: duration }, transitionConfig ?? {});
+
+  return (
+    <Collapse
+      open={open}
+      transitionConfig={mergedTransitionConfig}
+      ref={ref}
+      {...restProps}
+    >
+      <AccordionBodyContainer
+        className={containerClassName}
+        {...restContainerProps}
+      />
+    </Collapse>
+  );
+});
+
+AccordionBody.displayName = 'AccordionBody';
+
+export default AccordionBody;
