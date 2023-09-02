958db3b9d04c2590d44733deb998d39f60b494a0
Richard Wagner <richard.wagner@gmail.com>
2023-09-02 13:41:09 +0200

    fix

--- src/pages/DashboardLayout/DashboardAvatar.tsx
+++ src/pages/DashboardLayout/DashboardAvatar.tsx
@@ -1,53 +1,49 @@
 import React, { type FC } from 'react';
-import Menu from '../../lib/simpleComponents/components/UI/Menu';
-import Avatar from '../../lib/simpleComponents/components/UI/Avatar';
-import MenuBody from '../../lib/simpleComponents/components/UI/MenuBody';
-import List from '../../lib/simpleComponents/components/UI/List';
-import ListItemButton from '../../lib/simpleComponents/components/UI/ListItemButton';
-import Link from '../../lib/simpleComponents/components/UI/Link';
+import Menu from '../../lib/simpleComponents/components/UI/Menu/Menu';
+import Avatar from '../../lib/simpleComponents/components/UI/Avatar/Avatar';
+import MenuBody from '../../lib/simpleComponents/components/UI/Menu/MenuBody';
+import List from '../../lib/simpleComponents/components/UI/List/List';
+import ListItemButton from '../../lib/simpleComponents/components/UI/Listitembutton/ListItemButton';
+import Link from '../../lib/simpleComponents/components/UI/Link/Link';
 
-const DashboardAvatar: FC = () => {
-  const overlay = document.getElementById('overlay');
-
-  return (
-    <Menu
-      elevated
-      overlayRef={overlay}
-      handler={
-        <Avatar
-          src={require('../../assets/images/photo.png')}
-          className="w-10"
-        />
-      }
-    >
-      <MenuBody fullwidht>
-        <List>
-          <ListItemButton
+const DashboardAvatar: FC = () => (
+  <Menu
+    elevated
+    overlayRef={document.getElementById('overlay')}
+    handler={
+      <Avatar
+        src={require('../../assets/images/photo.png')}
+        className="w-10"
+      />
+    }
+  >
+    <MenuBody fullwidht>
+      <List>
+        <ListItemButton
+          color="dark"
+          fullwidth
+        >
+          <Link
+            to="/admin/settings"
             color="dark"
-            fullwidth
           >
-            <Link
-              to="/admin/settings"
-              color="dark"
-            >
-              Settings
-            </Link>
-          </ListItemButton>
-          <ListItemButton
+            Settings
+          </Link>
+        </ListItemButton>
+        <ListItemButton
+          color="dark"
+          fullwidth
+        >
+          <Link
+            to="/logout"
             color="dark"
-            fullwidth
           >
-            <Link
-              to="/logout"
-              color="dark"
-            >
-              Logout
-            </Link>
-          </ListItemButton>
-        </List>
-      </MenuBody>
-    </Menu>
-  );
-};
+            Logout
+          </Link>
+        </ListItemButton>
+      </List>
+    </MenuBody>
+  </Menu>
+);
 
 export default DashboardAvatar;
