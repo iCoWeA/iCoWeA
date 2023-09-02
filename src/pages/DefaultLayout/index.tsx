ab581fefe3f43deddcc3d215f2d0a3f14ad2a31d
Richard Wagner <richard.wagner@gmail.com>
2023-09-02 13:32:18 +0200

    fix

--- src/pages/DefaultLayout/index.tsx
+++ src/pages/DefaultLayout/index.tsx
@@ -1,5 +1,5 @@
 import React, { type FC } from 'react';
-import Container from '../../lib/simpleComponents/components/UI/Container';
+import Container from '../../lib/simpleComponents/components/UI/Container/Container';
 import { Outlet } from 'react-router-dom';
 
 export const Component: FC = () => (
