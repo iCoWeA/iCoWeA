e686336a679fcd9a3449077b256dcb9c5fce50fd
Richard Wagner <richard.wagner@gmail.com>
2023-09-04 19:57:48 +0200

    fix

--- src/lib/simpleComponents/components/UI/Dropdown/Dropdown.tsx
+++ src/lib/simpleComponents/components/UI/Dropdown/Dropdown.tsx
@@ -3,17 +3,18 @@ import themeContext from '../../../contexts/theme';
 import dropdownConfig from '../../../configs/dropdownConfig';
 import { mergeClasses } from '../../../utils/propsHelper';
 
-interface DropdownContainerProps extends BaseHTMLAttributes<HTMLDivElement> {
-  color: Colors;
-  elevated: boolean;
+interface DropdownProps extends BaseHTMLAttributes<HTMLDivElement> {
+  color?: Colors;
+  elevated?: boolean;
 }
 
-const DropdownContainer = forwardRef<HTMLDivElement, DropdownContainerProps>(({ color, elevated, className, ...restProps }, ref) => {
+const Dropdown = forwardRef<HTMLDivElement, DropdownProps>((props, ref) => {
   /* --- Set context props --- */
   const theme = useContext(themeContext).theme;
 
   /* --- Set default props --- */
   const styles = dropdownConfig.styles;
+  const { color, elevated, className, ...restProps } = { ...dropdownConfig.defaultProps, ...props };
 
   /* --- Set props --- */
   const mergedClassName = mergeClasses(styles.base, styles.colors[theme][color], elevated && styles.elevated[theme], className);
@@ -27,6 +28,6 @@ const DropdownContainer = forwardRef<HTMLDivElement, DropdownContainerProps>(({ 
   );
 });
 
-DropdownContainer.displayName = 'DropdownContainer';
+Dropdown.displayName = 'Dropdown';
 
-export default DropdownContainer;
+export default Dropdown;
