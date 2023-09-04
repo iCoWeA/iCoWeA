e394530664ef3fabe89f40341c907642b2e03009
Richard Wagner <richard.wagner@gmail.com>
2023-09-04 22:46:12 +0200

    fix

---
+++ src/components/PasswordInout.tsx/PasswordInputButton.tsx
@@ -0,0 +1,33 @@
+import React, { type MouseEvent, forwardRef } from 'react';
+import IconButton, { type IconButtonProps } from '../../lib/simpleComponents/components/UI/IconButton/IconButton';
+
+export interface PasswordInputButtonProps extends IconButtonProps {
+  onShow?: () => void;
+}
+
+const PasswordInputButton = forwardRef<HTMLButtonElement, PasswordInputButtonProps>(({ onShow, onClick, ...restProps }, ref) => {
+  /* --- Set props --- */
+  const clickHandler = (event: MouseEvent<HTMLButtonElement>): void => {
+    if (onShow !== undefined) {
+      event.stopPropagation();
+      onShow();
+    }
+
+    if (onClick !== undefined) {
+      onClick(event);
+    }
+  };
+
+  return (
+    <IconButton
+      variant="text"
+      onClick={clickHandler}
+      ref={ref}
+      {...restProps}
+    />
+  );
+});
+
+PasswordInputButton.displayName = 'PasswordInputButton';
+
+export default PasswordInputButton;
