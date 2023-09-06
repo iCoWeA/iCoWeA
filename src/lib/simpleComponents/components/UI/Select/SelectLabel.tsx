bc4d64bd6e63ea4a312be8ef95501089dd84f3f7
Richard Wagner <richard.wagner@gmail.com>
2023-09-06 20:18:23 +0200

    fix

--- src/lib/simpleComponents/components/UI/Select/SelectLabel.tsx
+++ src/lib/simpleComponents/components/UI/Select/SelectLabel.tsx
@@ -1,47 +1,40 @@
-import React, { forwardRef, useContext, type LabelHTMLAttributes } from 'react';
-import themeContext from '../../../contexts/theme';
+import React, { type LabelHTMLAttributes, forwardRef, useContext } from 'react';
 import selectConfig from '../../../configs/selectConfig';
+import themeContext from '../../../contexts/theme';
 import { mergeClasses } from '../../../utils/propsHelper';
 
 interface SelectLabelProps extends LabelHTMLAttributes<HTMLLabelElement> {
-  open: boolean;
   variant: InputVariants;
   color: Colors;
   valid: boolean;
   invalid: boolean;
-  transitionConfig: { enterDuration: number; exitDuration: number };
 }
 
-const SelectLabel = forwardRef<HTMLLabelElement, SelectLabelProps>(
-  ({ open, variant, color, valid, invalid, transitionConfig, style, className, ...restProps }, ref) => {
-    /* --- Set context props --- */
-    const theme = useContext(themeContext).theme;
+const SelectLabel = forwardRef<HTMLLabelElement, SelectLabelProps>(({ variant, color, valid, invalid, className, ...restProps }, ref) => {
+  /* --- Set context props --- */
+  const theme = useContext(themeContext).theme;
 
-    /* --- Set default props --- */
-    const styles = selectConfig.styles.label;
+  /* --- Set default props --- */
+  const styles = selectConfig.styles.label;
 
-    /* --- Set props --- */
-    const mergedClassName = mergeClasses(
-      styles.base,
-      styles.sizes[variant],
-      !valid && !invalid && styles.colors[theme][color],
-      valid && styles.valid[theme],
-      invalid && styles.invalid[theme],
-      className
-    );
+  /* --- Set props --- */
+  const mergedClassName = mergeClasses(
+    styles.base,
+    styles.sizes[variant],
+    !valid && !invalid && styles.colors[theme][color],
+    valid && styles.valid[theme],
+    invalid && styles.invalid[theme],
+    className
+  );
 
-    const mergedStyle = { transitionDuration: `${open ? transitionConfig.enterDuration : transitionConfig.exitDuration}ms`, ...style };
-
-    return (
-      <label
-        style={mergedStyle}
-        className={mergedClassName}
-        ref={ref}
-        {...restProps}
-      />
-    );
-  }
-);
+  return (
+    <label
+      className={mergedClassName}
+      ref={ref}
+      {...restProps}
+    />
+  );
+});
 
 SelectLabel.displayName = 'SelectLabel';
 
