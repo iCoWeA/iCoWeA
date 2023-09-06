dea5785c5649b4c1dd250bf8dbb8322095c43180
Richard Wagner <richard.wagner@gmail.com>
2023-09-06 20:23:25 +0200

    fix

--- src/lib/simpleComponents/components/UI/Select/SelectArrow.tsx
+++ src/lib/simpleComponents/components/UI/Select/SelectArrow.tsx
@@ -1,26 +1,21 @@
 import React, { forwardRef } from 'react';
-import Icon, { type IconProps } from '../Icon/Icon';
 import selectConfig from '../../../configs/selectConfig';
 import { mergeClasses } from '../../../utils/propsHelper';
+import Icon, { type IconProps } from '../Icon/Icon';
 
 interface SelectArrowProps extends IconProps {
   open: boolean;
-  transitionConfig: { enterDuration: number; exitDuration: number };
 }
 
-const SelectArrow = forwardRef<SVGSVGElement, SelectArrowProps>(({ open, transitionConfig, style, className, ...restProps }, ref) => {
+const SelectArrow = forwardRef<SVGSVGElement, SelectArrowProps>(({ open, className, ...restProps }, ref) => {
   /* --- Set default props --- */
   const styles = selectConfig.styles.arrow;
 
   /* --- Set props --- */
   const mergedClassName = mergeClasses(styles.base, open && styles.open, className);
 
-  const mergedStyle = { transitionDuration: `${open ? transitionConfig.enterDuration : transitionConfig.exitDuration}ms`, ...style };
-
   return (
     <Icon
-      color="default"
-      style={mergedStyle}
       className={mergedClassName}
       ref={ref}
       {...restProps}
