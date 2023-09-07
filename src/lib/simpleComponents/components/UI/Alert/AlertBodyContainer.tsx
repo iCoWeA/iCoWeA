fa66049884c0b136c9f6a7d820065e883bdd4b77
Richard Wagner <richard.wagner@gmail.com>
2023-09-07 18:50:32 +0200

    fix

--- src/lib/simpleComponents/components/UI/Alert/AlertBodyContainer.tsx
+++ src/lib/simpleComponents/components/UI/Alert/AlertBodyContainer.tsx
@@ -2,14 +2,17 @@ import React, { type BaseHTMLAttributes, forwardRef } from 'react';
 import alertConfig from '../../../configs/alertConfig';
 import { mergeClasses } from '../../../utils/propsHelper';
 
-interface AlertBodyContainerProps extends BaseHTMLAttributes<HTMLDivElement> {}
+interface AlertBodyContainerProps extends BaseHTMLAttributes<HTMLDivElement> {
+  icon: boolean;
+  action: boolean;
+}
 
-const AlertBodyContainer = forwardRef<HTMLDivElement, AlertBodyContainerProps>(({ className, ...restProps }, ref) => {
+const AlertBodyContainer = forwardRef<HTMLDivElement, AlertBodyContainerProps>(({ icon, action, className, ...restProps }, ref) => {
   /* --- Set default props --- */
   const styles = alertConfig.styles.bodyContainer;
 
   /* --- Set props --- */
-  const mergedClassName = mergeClasses(styles.base, className);
+  const mergedClassName = mergeClasses(styles.base, icon && styles.icon, action && styles.action, className);
 
   return (
     <div
