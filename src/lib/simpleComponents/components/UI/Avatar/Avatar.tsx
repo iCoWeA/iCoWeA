273f38995db85dc06bf6b109e6ff6eadaf7731c3
Richard Wagner <richard.wagner@gmail.com>
2023-09-12 00:26:08 +0200

    fix

--- src/lib/simpleComponents/components/UI/Avatar/Avatar.tsx
+++ src/lib/simpleComponents/components/UI/Avatar/Avatar.tsx
@@ -1,7 +1,7 @@
-import React, { type ImgHTMLAttributes, type BaseHTMLAttributes, forwardRef, useContext } from 'react';
+import React, { type BaseHTMLAttributes, forwardRef, useContext, type ImgHTMLAttributes } from 'react';
 import avatarConfig from '../../../configs/avatarConfig';
-import { mergeClasses } from '../../../utils/propsHelper';
 import themeContext from '../../../contexts/theme';
+import { mergeClasses } from '../../../utils/propsHelper';
 
 interface AvatarContainerProps extends BaseHTMLAttributes<HTMLDivElement> {
   variant: Borders;
