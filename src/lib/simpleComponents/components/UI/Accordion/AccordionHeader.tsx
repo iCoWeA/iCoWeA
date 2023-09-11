ecdb3e5596bc9b5693643a7bd5d38430067a9b88
Richard Wagner <richard.wagner@gmail.com>
2023-09-11 22:02:24 +0200

    fix

--- src/lib/simpleComponents/components/UI/Accordion/AccordionHeader.tsx
+++ src/lib/simpleComponents/components/UI/Accordion/AccordionHeader.tsx
@@ -1,43 +1,85 @@
-import React, { type ButtonHTMLAttributes, forwardRef, useContext } from 'react';
+import React, { type BaseHTMLAttributes, type FC, type ButtonHTMLAttributes, type ReactNode, forwardRef, useContext } from 'react';
 import accordionHeaderConfig from '../../../configs/accordionHeaderConfig';
 import accordionContext from '../../../contexts/accordion';
 import themeContext from '../../../contexts/theme';
 import { mergeClasses } from '../../../utils/propsHelper';
-import { type IconProps } from '../Icon/Icon';
-import AccordionIcon from './AccordionIcon';
+
+interface AccordionDecorationContainerProps extends BaseHTMLAttributes<HTMLDivElement> {
+  position: 'start' | 'end';
+}
+
+const AccordionDecorationContainer: FC<AccordionDecorationContainerProps> = ({ position, className, ...restProps }) => {
+  /* --- Set default props --- */
+  const styles = accordionHeaderConfig.styles.container;
+
+  const mergedClassName = mergeClasses(styles.base, styles.positions[position], className);
+
+  return (
+    <div
+      className={mergedClassName}
+      {...restProps}
+    />
+  );
+};
 
 export interface AccordionHeaderProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  iconProps?: IconProps;
+  color?: Colors;
+  divider?: boolean;
+  startDecoration?: ReactNode;
+  endDecoration?: ReactNode;
+  startDecorationContainerProps?: BaseHTMLAttributes<HTMLDivElement>;
+  endDecorationContainerProps?: BaseHTMLAttributes<HTMLDivElement>;
 }
 
 const AccordionHeader = forwardRef<HTMLButtonElement, AccordionHeaderProps>((props, ref) => {
   /* --- Set context props --- */
-  const {
-    onToggle: onAccordionToggle,
-    open: isAccordionOpen,
-    icon: accordionIcon,
-    id: accordionId,
-    disabled: isAccordionDisabled
-  } = useContext(accordionContext);
+  const { onToggle: onAccordionToggle, open: isAccordionOpen, id: accordionId, disabled: isAccordionDisabled } = useContext(accordionContext);
   const theme = useContext(themeContext).theme;
 
   /* --- Set default props --- */
-  const styles = accordionHeaderConfig.styles.button;
-  const { iconProps, disabled, className, children, ...restProps } = {
+  const styles = accordionHeaderConfig.styles;
+  const {
+    color,
+    divider,
+    startDecoration,
+    endDecoration,
+    startDecorationContainerProps,
+    endDecorationContainerProps,
+    disabled,
+    className,
+    children,
+    ...restProps
+  } = {
     ...accordionHeaderConfig.defaultProps,
     disabled: isAccordionDisabled,
     ...props
   };
 
-  /* --- Set icon props --- */
-  let iconNode = accordionIcon;
+  /* --- Set start decoration --- */
+  let startDecorationNode = startDecoration;
 
-  if (iconNode === undefined) {
-    iconNode = (
-      <AccordionIcon
-        open={isAccordionOpen}
-        {...iconProps}
-      />
+  if (startDecorationNode !== undefined) {
+    startDecorationNode = (
+      <AccordionDecorationContainer
+        position="start"
+        {...startDecorationContainerProps}
+      >
+        {startDecoration}
+      </AccordionDecorationContainer>
+    );
+  }
+
+  /* --- Set end decoration --- */
+  let endDecorationNode = endDecoration;
+
+  if (endDecorationNode !== undefined) {
+    endDecorationNode = (
+      <AccordionDecorationContainer
+        position="end"
+        {...endDecorationContainerProps}
+      >
+        {endDecoration}
+      </AccordionDecorationContainer>
     );
   }
 
@@ -45,7 +87,15 @@ const AccordionHeader = forwardRef<HTMLButtonElement, AccordionHeaderProps>((pro
   const ariaContarols = accordionId === undefined ? undefined : `acd-body-${accordionId}`;
   const id = accordionId === undefined ? undefined : `acd-header-${accordionId}`;
 
-  const mergedClassName = mergeClasses(styles.base, styles.divider[theme], styles.colors[theme], className);
+  const mergedClassName = mergeClasses(
+    styles.button.base,
+    styles.button.colors[theme],
+    isAccordionOpen && styles.button.open[theme][color],
+    styles.before.base,
+    styles.before.colors[theme],
+    isAccordionOpen && styles.before.open[theme][color],
+    className
+  );
 
   return (
     <button
@@ -59,8 +109,9 @@ const AccordionHeader = forwardRef<HTMLButtonElement, AccordionHeaderProps>((pro
       ref={ref}
       {...restProps}
     >
+      {startDecoration}
       {children}
-      {iconNode}
+      {endDecoration}
     </button>
   );
 });
