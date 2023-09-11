15d118424a7dd5114421aa4f237aa55eb20fa705
Richard Wagner <richard.wagner@gmail.com>
2023-09-11 23:25:22 +0200

    fix

--- src/lib/simpleComponents/configs/accordionHeaderConfig.ts
+++ src/lib/simpleComponents/configs/accordionHeaderConfig.ts
@@ -1,111 +1,88 @@
-import { type IconProps } from '../components/UI/Icon/Icon';
-
 export interface AccordionHeaderConfig {
   defaultProps: {
-    iconProps: IconProps;
+    color: Colors;
+    divider: boolean;
   };
   styles: {
     button: {
       base: Record<string, string>;
-      sizes: Record<Sizes, Record<string, string>>;
-      variants: Record<CheckVariants, Record<Themes, Record<string, string>>>;
-      open: Record<CheckVariants, Record<Themes, Record<Colors, Record<string, string>>>>;
+      colors: Record<Themes, Record<string, string>>;
+      open: Record<Themes, Record<Colors, Record<string, string>>>;
     },
     before: {
       base: Record<string, string>;
       colors: Record<Themes, Record<string, string>>;
+      open: Record<Themes, Record<Colors, Record<string, string>>>;
     },
     divider: {
       base: Record<string, string>;
       colors: Record<Themes, Record<string, string>>;
-    },
-    icon: {
-      base: Record<string, string>;
-      open: Record<string, string>;
     }
   }
 }
 
 const accordionHeaderConfig: AccordionHeaderConfig = {
   defaultProps: {
-    iconProps: {}
+    color: 'default',
+    divider: true
   },
   styles: {
     button: {
       base: {
         position: 'relative',
         display: 'flex',
+        gap: 'gap-4',
         alignItems: 'items-center',
+        height: 'h-12',
         width: 'w-full',
-        font: 'antialiased font-normal font-sans',
+        font: 'antialiased font-normal text-base font-sans',
         transition: 'transition',
         focus: 'focus:outline-0'
       },
-      sizes: {
-        sm: {
-          height: 'h-8',
-          padding: 'py-1.5',
-          text: 'text-sm'
-        },
-        md: {
-          height: 'h-10',
-          padding: 'py-2',
-          text: 'text-base'
-        },
-        lg: {
-          height: 'h-12',
-          padding: 'py-2.5',
-          text: 'text-lg'
-        }
-      },
-      variants: {
-        plain: {
-          light: {
-            fill: 'fill-light-on-surface/70',
-            color: 'text-light-on-surface/70',
-            hover: 'hover:fill-light-on-surface hover:text-light-on-surface'
-          }
-        },
-        text: {
-          light: {
-            fill: 'fill-light-on-surface/70',
-            color: 'text-light-on-surface/70',
-            hover: 'hover:fill-light-on-surface hover:text-light-on-surface'
-          }
-        },
-        filled: {
-          light: {
-            fill: 'fill-light-on-surface',
-            color: 'text-light-on-surface'
-          }
+      colors: {
+        light: {
+          fill: 'fill-light-on-surface-variant',
+          color: 'text-light-on-surface-variant',
+          hover: 'hover:fill-light-on-surface hover:text-light-on-surface'
         }
       },
       open: {
-        plain: {
-          light: {
-            primary: {
-              fill: 'fill-light-primary',
-              color: 'text-light-primary',
-              hover: 'hover:fill-light-primary hover:text-light-primary'
-            }
-          }
-        },
-        text: {
-          light: {
-            primary: {
-              fill: 'fill-light-primary',
-              color: 'text-light-primary',
-              hover: 'hover:fill-light-primary hover:text-light-primary'
-            }
-          }
-        },
-        filled: {
-          light: {
-            primary: {
-              fill: 'fill-light-on-primary-container',
-              color: 'text-light-on-primary-container',
-              background: 'bg-light-primary-container'
-            }
+        light: {
+          default: {
+            fill: 'fill-light-on-surface',
+            color: 'text-light-on-surface',
+            background: 'bg-light-surface-low',
+            hover: 'hover:fill-light-on-surface hover:text-light-on-surface'
+          },
+          primary: {
+            fill: 'fill-light-on-primary-container',
+            color: 'text-light-on-primary-container',
+            background: 'bg-light-primary-container',
+            hover: 'hover:fill-light-on-primary-container hover:text-light-on-primary-container'
+          },
+          secondary: {
+            fill: 'fill-light-on-secondary-container',
+            color: 'text-light-on-secondary-container',
+            background: 'bg-light-secondary-container',
+            hover: 'hover:fill-light-on-secondary-container hover:text-light-on-secondary-container'
+          },
+          success: {
+            fill: 'fill-light-on-success-container',
+            color: 'text-light-on-success-container',
+            background: 'bg-light-success-container',
+            hover: 'hover:fill-light-on-success-container hover:text-light-on-success-container'
+          },
+          warning: {
+            fill: 'fill-light-on-warning-container',
+            color: 'text-light-on-warning-container',
+            background: 'bg-light-warning-container',
+            hover: 'hover:fill-light-on-warning-container hover:text-light-on-warning-container'
+          },
+          error: {
+            fill: 'fill-light-on-error-container',
+            color: 'text-light-on-error-container',
+            background: 'bg-light-error-container',
+            hover: 'hover:fill-light-on-error-container hover:text-light-on-error-container'
           }
         }
       }
@@ -125,6 +102,34 @@ const accordionHeaderConfig: AccordionHeaderConfig = {
           hover: 'hover:before:bg-light-on-surface/[0.08]',
           active: 'active:before:bg-light-on-surface/[0.12]'
         }
+      },
+      open: {
+        light: {
+          default: {
+            hover: 'hover:before:bg-light-on-surface/[0.08]',
+            active: 'active:before:bg-light-on-surface/[0.12]'
+          },
+          primary: {
+            hover: 'hover:before:bg-light-on-primary-container/[0.08]',
+            active: 'active:before:bg-light-on-primary-container/[0.12]'
+          },
+          secondary: {
+            hover: 'hover:before:bg-light-on-secondary-container/[0.08]',
+            active: 'active:before:bg-light-on-secondary-container/[0.12]'
+          },
+          success: {
+            hover: 'hover:before:bg-light-on-success-container/[0.08]',
+            active: 'active:before:bg-light-on-success-container/[0.12]'
+          },
+          warning: {
+            hover: 'hover:before:bg-light-on-warning-container/[0.08]',
+            active: 'active:before:bg-light-on-warning-container/[0.12]'
+          },
+          error: {
+            hover: 'hover:before:bg-light-on-error-container/[0.08]',
+            active: 'active:before:bg-light-on-error-container/[0.12]'
+          }
+        }
       }
     },
     divider: {
@@ -135,16 +140,6 @@ const accordionHeaderConfig: AccordionHeaderConfig = {
         light: {
           border: 'border-t-transparent border-b-light-divider-variant'
         }
-      }
-    },
-    icon: {
-      base: {
-        margin: 'ml-auto',
-        transition: 'transition',
-        transitionDuration: 'duration-500'
-      },
-      open: {
-        transform: 'rotate-180'
       }
     }
   }
