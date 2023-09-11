809161a98aa73151204d335d79467069fec64a46
Richard Wagner <richard.wagner@gmail.com>
2023-09-11 18:46:01 +0200

    fix

--- src/lib/simpleComponents/contexts/accordion.ts
+++ src/lib/simpleComponents/contexts/accordion.ts
@@ -1,11 +1,8 @@
 import { type ReactNode, createContext } from 'react';
-import { type AccordionVariants } from '../configs/accordionConfig';
 
 export interface AccordionContext {
   onToggle: () => void;
-  variant: AccordionVariants;
   size: Sizes;
-  color: Colors;
   open: boolean;
   icon?: ReactNode;
   id?: string;
@@ -14,9 +11,7 @@ export interface AccordionContext {
 
 export const initialState: AccordionContext = {
   onToggle: () => {},
-  variant: 'text',
   size: 'md',
-  color: 'defualt',
   open: false,
   disabled: false
 };
