70052ea107e656cecb56c75d9329cfe664d02827
Richard Wagner <richard.wagner@gmail.com>
2023-09-13 10:10:26 +0200

    fix

--- src/lib/simpleComponents/configs/cardBodyConfig.ts
+++ src/lib/simpleComponents/configs/cardBodyConfig.ts
@@ -1,37 +1,17 @@
 export interface CardBodyConfig {
-  defaultProps: {
-    columns: boolean;
-    fullwidht: boolean;
-  };
   styles: {
     base: Record<string, string>;
-    columns: Record<string, string>;
-    fullwidth: Record<string, string>;
   }
 }
 
 const cardBodyConfig: CardBodyConfig = {
-  defaultProps: {
-    columns: false,
-    fullwidht: false
-  },
   styles: {
     base: {
       display: 'flex',
       flexDirection: 'flex-col',
-      gap: 'gap-4',
       height: 'h-full',
       width: 'w-full',
       padding: 'p-4'
-    },
-    columns: {
-      display: 'grid',
-      gridTemplateColumns: 'grid-cols-4',
-      gap: 'gap-y-4 gap-x-[16px]',
-      md: 'md:grid-cols-8 md:gap-x-[24px]'
-    },
-    fullwidth: {
-      padding: 'px-0'
     }
   }
 };
