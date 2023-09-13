5ede94318796118216fd216b566d16df44b9e938
Richard Wagner <richard.wagner@gmail.com>
2023-09-13 14:27:41 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,17 +15,34 @@ module.exports = {
     },
     extend: {
       colors: {
-        default: {
-          background: '#e6e6e6',
-          divider: '#e6e6e6',
-          default: '#b3b3b3',
-          light: '#ffffff',
-          dark: '#595959',
+        light: {
+          surface: '#e7f0fe',
+          'surface-light': '#ffffff',
+          'surface-dark': '#d9d9d9',
+          'on-surface': '#262626',
+          'on-surface-variant': '#cccccc',
+          divider: '#cccccc',
+          'divider-variant': '#a6a6a6',
           primary: '#2c7cf5',
+          'on-primary': '#ffffff',
+          'primary-light': '#9ec3fa',
+          'primary-dark': '#0953c3',
           secondary: '#fea716',
+          'on-secondary': '#ffffff',
+          'secondary-light': '#fed99a',
+          'secondary-dark': '#cb8101',
           success: '#00cc44',
+          'on-success': '#ffffff',
+          'success-light': '#99ffbb',
+          'success-dark': '#009933',
           warning: '#ffcc00',
-          error: '#eb474a'
+          'on-warning': '#ffffff',
+          'warning-light': '#ffeb99',
+          'warning-dark': '#cca300',
+          error: '#e83034',
+          'on-error': '#ffffff',
+          'error-light': '#f5a3a5',
+          'error-dark': '#b81417'
         }
       },
       gridTemplateRows: {
