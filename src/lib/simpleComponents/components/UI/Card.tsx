ff54d8b0605bb60b3bddd29b0d467b0f66605d7d
Richard Wagner <richard.wagner@gmail.com>
2023-09-13 11:14:48 +0200

    fix

--- src/lib/simpleComponents/components/UI/Card.tsx
+++ src/lib/simpleComponents/components/UI/Card.tsx
@@ -8,6 +8,8 @@ export type CardVariants = 'plain' | 'filled' | 'outlined';
 export interface CardProps extends BaseHTMLAttributes<HTMLDivElement> {
   variant?: CardVariants;
   elevated?: boolean;
+  clickable?: boolean;
+  grabed?: boolean;
 }
 
 const Card = forwardRef<HTMLDivElement, CardProps>((props, ref) => {
@@ -15,16 +17,27 @@ const Card = forwardRef<HTMLDivElement, CardProps>((props, ref) => {
   const theme = useContext(themeContext).theme;
 
   /* --- Set default props --- */
-  const styles = cardConfig.styles;
-  const { variant, elevated, className, ...restProps } = { ...cardConfig.defaultProps, ...props };
+  const { container: containerStyles, layer: layerStyles } = cardConfig.styles;
+  const { variant, elevated, clickable, grabed, className, ...restProps } = { ...cardConfig.defaultProps, ...props };
 
   /* --- Set props --- */
-  const mergedClassName = mergeClasses(styles.base, styles.variants[variant][theme], elevated && styles.elevated[theme], className);
+  const clickableProps = clickable ? { tabIndex: 0, role: 'button' } : {};
+
+  const mergedClassName = mergeClasses(
+    containerStyles.base,
+    containerStyles.variants[variant][theme],
+    elevated && containerStyles.elevated,
+    layerStyles.base,
+    clickable && layerStyles.colors[theme],
+    grabed && layerStyles.grabed[theme],
+    className
+  );
 
   return (
     <div
       className={mergedClassName}
       ref={ref}
+      {...clickableProps}
       {...restProps}
     />
   );
--- src/lib/simpleComponents/configs/cardConfig.ts
+++ src/lib/simpleComponents/configs/cardConfig.ts
@@ -4,43 +4,79 @@ export interface CardConfig {
   defaultProps: {
     variant: CardVariants;
     elevated: boolean;
+    clickable: boolean;
+    grabed: boolean;
   };
   styles: {
-    base: Record<string, string>;
-    elevated: Record<string, string>;
-    variants: Record<CardVariants, Record<Themes, Record<string, string>>>;
+    container: {
+      base: Record<string, string>;
+      elevated: Record<string, string>;
+      variants: Record<CardVariants, Record<Themes, Record<string, string>>>;
+    },
+    layer: {
+      base: Record<string, string>;
+      colors: Record<Themes, Record<string, string>>;
+      grabed: Record<Themes, Record<string, string>>;
+    }
   }
 }
 
 const cardConfig: CardConfig = {
   defaultProps: {
     variant: 'plain',
-    elevated: false
+    elevated: false,
+    clickable: false,
+    grabed: false
   },
   styles: {
-    base: {
-      display: 'flex',
-      flexDirection: 'flex-col',
-      borderRadius: 'rounded-xl',
-      overflow: 'overflow-hidden'
+    container: {
+      base: {
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'flex-col',
+        borderRadius: 'rounded-xl',
+        overflow: 'overflow-hidden'
+      },
+      elevated: {
+        shadow: 'shadow-md shadow-black/50'
+      },
+      variants: {
+        plain: {
+          light: {
+            background: 'bg-light-surface-light'
+          }
+        },
+        filled: {
+          light: {
+            background: 'bg-light-surface'
+          }
+        },
+        outlined: {
+          light: {
+            border: 'border border-light-divider'
+          }
+        }
+      }
     },
-    elevated: {
-      shadow: 'shadow-md shadow-black/50'
-    },
-    variants: {
-      plain: {
+    layer: {
+      base: {
+        position: 'before:absolute',
+        top: 'before:top-0',
+        left: 'before:left-0',
+        display: 'before:block',
+        height: 'before:h-full',
+        width: 'before:w-full',
+        transition: 'before:transition'
+      },
+      colors: {
         light: {
-          background: 'bg-light-surface-light'
+          hover: 'hover:before:bg-light-on-surface/10',
+          active: 'active:before:bg-light-on-surface/[0.15]'
         }
       },
-      filled: {
+      grabed: {
         light: {
-          background: 'bg-light-surface'
-        }
-      },
-      outlined: {
-        light: {
-          border: 'border border-light-divider'
+          background: 'before:bg-light-on-surface/20'
         }
       }
     }
