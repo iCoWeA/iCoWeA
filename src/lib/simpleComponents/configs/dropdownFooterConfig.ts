cc501a08958af1b617f14faa884f8788c3a5ff85
Richard Wagner <richard.wagner@gmail.com>
2023-09-15 22:04:19 +0200

    fix

--- src/lib/simpleComponents/configs/dropdownFooterConfig.ts
+++ src/lib/simpleComponents/configs/dropdownFooterConfig.ts
@@ -1,36 +1,17 @@
 export interface DropdownFooterConfig {
-  defaultProps: {
-    columns: boolean;
-    fullwidht: boolean;
-  };
   styles: {
     base: Record<string, string>;
-    columns: Record<string, string>;
-    fullwidth: Record<string, string>;
   }
 }
 
 const dropdownFooterConfig: DropdownFooterConfig = {
-  defaultProps: {
-    columns: false,
-    fullwidht: false
-  },
   styles: {
     base: {
       display: 'flex',
-      flexDirection: 'flex-col',
       gap: 'gap-2',
+      alignItems: 'items-center',
       width: 'w-full',
-      padding: 'pb-2 px-2'
-    },
-    columns: {
-      display: 'grid',
-      gridTemplateColumns: 'grid-cols-4',
-      gap: 'gap-y-4 gap-x-[16px]',
-      md: 'md:grid-cols-8 md:gap-x-[24px]'
-    },
-    fullwidth: {
-      padding: 'px-0'
+      padding: 'p-3'
     }
   }
 };
