37c98110f194d7723f40096549bb2f7cfc5d1c14
Richard Wagner <richard.wagner@gmail.com>
2023-09-15 23:22:39 +0200

    fix

---
+++ src/lib/simpleComponents/components/UI/Slide.tsx
@@ -0,0 +1,73 @@
+import React, { type BaseHTMLAttributes, forwardRef, useRef, useImperativeHandle, useEffect } from 'react';
+import slideConfig from '../../configs/slideConfig';
+import useAnimation, { AnimationStates } from '../../hooks/useAnimation';
+import { mergeClasses } from '../../utils/propsHelper';
+
+export interface SlideProps extends BaseHTMLAttributes<HTMLDivElement> {
+  onEnter?: () => void;
+  onExit?: () => void;
+  direction?: Directions;
+  open?: boolean;
+  keepMounted?: boolean;
+}
+
+const Slide = forwardRef<HTMLDivElement, SlideProps>((props, ref) => {
+  /* --- Set default props --- */
+  const styles = slideConfig.styles;
+  const { onEnter, onExit, direction, open, keepMounted, className, ...restProps } = {
+    ...slideConfig.defaultProps,
+    ...props
+  };
+
+  /* --- Set refs --- */
+  const slideRef = useRef<HTMLDivElement>(null);
+
+  /* --- Set states --- */
+  const {
+    state: animationState,
+    enter,
+    exit,
+    transitionEndHandler,
+    animationEndHandler
+  } = useAnimation<HTMLDivElement>(slideRef.current, open, onEnter, onExit);
+
+  /* --- Set imperative anchorElement --- */
+  useImperativeHandle<HTMLDivElement | null, HTMLDivElement | null>(ref, () => slideRef.current, [
+    !keepMounted && !open && animationState.current === AnimationStates.EXITED
+  ]);
+
+  /*
+   * Set open state
+   */
+  useEffect(() => {
+    if (open && animationState.exit) {
+      enter();
+    }
+
+    if (!open && animationState.enter) {
+      exit();
+    }
+  }, [open, animationState.enter, animationState.exit]);
+
+  /* --- Unmount --- */
+  if (!keepMounted && !open && animationState.current === AnimationStates.EXITED) {
+    return <></>;
+  }
+
+  /* --- Set props --- */
+  const mergedClassName = mergeClasses(styles.base, styles.directions[direction], animationState.enter && styles.open[direction], className);
+
+  return (
+    <div
+      onTransitionEnd={transitionEndHandler}
+      onAnimationEnd={animationEndHandler}
+      className={mergedClassName}
+      ref={slideRef}
+      {...restProps}
+    />
+  );
+});
+
+Slide.displayName = 'Slide';
+
+export default Slide;
---
+++ src/lib/simpleComponents/configs/slideConfig.ts
@@ -0,0 +1,68 @@
+export interface SlideConfig {
+  defaultProps: {
+    direction: Directions;
+    open: boolean;
+    keepMounted: boolean;
+  };
+  styles: {
+    base: Record<string, string>;
+    directions: Record<Directions, Record<string, string>>;
+    open: Record<Directions, Record<string, string>>;
+  }
+}
+
+const slideConfig: SlideConfig = {
+  defaultProps: {
+    direction: 'bottom',
+    open: false,
+    keepMounted: false
+  },
+  styles: {
+    base: {
+      position: 'absolute',
+      display: 'block',
+      height: 'h-fit',
+      width: 'w-fit',
+      transition: 'transition',
+      transitionDuration: 'duration-[5000ms]'
+    },
+    directions: {
+      top: {
+        top: 'top-0',
+        left: 'left-2/4',
+        translate: '-translate-y-full -translate-x-2/4'
+      },
+      bottom: {
+        bottom: 'bottom-0',
+        left: 'left-2/4',
+        translate: 'translate-y-full -translate-x-2/4'
+      },
+      left: {
+        top: 'top-2/4',
+        left: 'left-0',
+        translate: '-translate-y-2/4 -translate-x-full'
+      },
+      right: {
+        top: 'top-2/4',
+        right: 'right-0',
+        translate: '-translate-y-2/4 translate-x-full'
+      }
+    },
+    open: {
+      top: {
+        translate: 'translate-y-0'
+      },
+      bottom: {
+        translate: 'translate-y-0'
+      },
+      left: {
+        translate: '-translate-x-0'
+      },
+      right: {
+        translate: 'translate-x-0'
+      }
+    }
+  }
+};
+
+export default slideConfig;
