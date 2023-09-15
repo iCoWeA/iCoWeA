506ea92036b548022fb96cb05b56f6d9e3e9d190
Richard Wagner <richard.wagner@gmail.com>
2023-09-15 22:09:58 +0200

    fix

--- src/lib/simpleComponents/configs/dropdownHeaderConfig.ts
+++ src/lib/simpleComponents/configs/dropdownHeaderConfig.ts
@@ -1,36 +1,17 @@
 export interface DropdownHeaderConfig {
-  defaultProps: {
-    columns: boolean;
-    fullwidht: boolean;
-  }
   styles: {
     base: Record<string, string>;
-    columns: Record<string, string>;
-    fullwidth: Record<string, string>;
   }
 }
 
 const dropdownHeaderConfig: DropdownHeaderConfig = {
-  defaultProps: {
-    columns: false,
-    fullwidht: false
-  },
   styles: {
     base: {
       display: 'flex',
-      flexDirection: 'flex-col',
-      gap: 'gap-2',
+      gap: 'gap-3',
+      alignItems: 'items-center',
       width: 'w-full',
-      padding: 'pt-2 px-2'
-    },
-    columns: {
-      display: 'grid',
-      gridTemplateColumns: 'grid-cols-4',
-      gap: 'gap-y-4 gap-x-[16px]',
-      md: 'md:grid-cols-8 md:gap-x-[24px]'
-    },
-    fullwidth: {
-      padding: 'px-0'
+      padding: 'p-3'
     }
   }
 };
