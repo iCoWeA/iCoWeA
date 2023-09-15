6e33e4faeef4e05118797f9cc7ef81177ef8b4ac
Richard Wagner <richard.wagner@gmail.com>
2023-09-15 11:53:37 +0200

    fix

---
+++ src/lib/simpleComponents/components/UI/IconButton.tsx
@@ -0,0 +1,59 @@
+import React, { type ButtonHTMLAttributes, type BaseHTMLAttributes, forwardRef, useContext } from 'react';
+import iconButtonConfig from '../../configs/iconButtonConfig';
+import themeContext from '../../contexts/theme';
+import { mergeClasses } from '../../utils/propsHelper';
+import StateLayer from './StateLayer';
+
+export type IconButtonVariants = 'plain' | 'text' | 'outlined' | 'filled';
+
+export interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: IconButtonVariants;
+  borderShape?: BorderShapes;
+  size?: Sizes;
+  color?: Colors;
+  elevated?: boolean;
+  stateLayerProps?: BaseHTMLAttributes<HTMLSpanElement>;
+}
+
+const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>((props, ref) => {
+  /* --- Set context props --- */
+  const theme = useContext(themeContext).theme;
+
+  /* --- Set default props --- */
+  const styles = iconButtonConfig.styles;
+  const { variant, borderShape, size, color, elevated, stateLayerProps, className, children, ...restProps } = {
+    ...iconButtonConfig.defaultProps,
+    ...props
+  };
+
+  /* --- Set props --- */
+  const mergedClassName = mergeClasses(
+    styles.base,
+    styles.variants[variant][theme][color],
+    styles.borderShapes[borderShape],
+    styles.sizes[size],
+    variant === 'outlined' && styles.outlineSizes[size],
+    elevated && styles.elevated,
+    className
+  );
+
+  return (
+    <button
+      className={mergedClassName}
+      type="button"
+      ref={ref}
+      {...restProps}
+    >
+      {children}
+      <StateLayer
+        state={`${variant}-click`}
+        color={color}
+        {...stateLayerProps}
+      />
+    </button>
+  );
+});
+
+IconButton.displayName = 'IconButton';
+
+export default IconButton;
--- src/lib/simpleComponents/configs/iconButtonConfig.ts
+++ src/lib/simpleComponents/configs/iconButtonConfig.ts
@@ -1,238 +1,251 @@
+import { type IconButtonVariants } from '../components/UI/IconButton';
+
 export interface IconButtonConfig {
   defaultProps: {
-    variant: ButtonVariants;
-    size: ButtonSizes;
+    variant: IconButtonVariants;
+    borderShape: BorderShapes;
+    size: Sizes;
     color: Colors;
+    elevated: boolean;
   };
   styles: {
     base: Record<string, string>;
-    sizes: Record<ButtonSizes, Record<ButtonVariants, Record<string, string>>>;
-    variants: Record<ButtonVariants, Record<Themes, Record<Colors, Record<string, string>>>>;
+    elevated: Record<string, string>;
+    borderShapes: Record<BorderShapes, Record<string, string>>;
+    sizes: Record<Sizes, Record<string, string>>;
+    outlineSizes: Record<Sizes, Record<string, string>>;
+    variants: Record<IconButtonVariants, Record<Themes, Record<Colors, Record<string, string>>>>;
   }
 }
 
-const iconButtonConfig: IconButtonConfig = {
+const iconIconButtonConfig: IconButtonConfig = {
   defaultProps: {
     variant: 'filled',
+    borderShape: 'circular',
     size: 'md',
-    color: 'primary'
+    color: 'primary',
+    elevated: false
   },
   styles: {
     base: {
+      position: 'relative',
       display: 'flex',
+      alignItems: 'items-center',
+      justifyContent: 'justify-center',
       aspectRatio: 'aspect-square',
-      borderRadius: 'rounded-full',
+      overflow: 'overflow-hidden',
       transition: 'transition',
-      hover: 'hover:bg-gradient-to-r',
-      active: 'hover:bg-gradient-to-r',
-      focus: 'focus:outline-0 focus:bg-gradient-to-r',
-      disabled: 'disabled:pointer-events-none disabled:select-none'
+      userSelect: 'select-none',
+      focus: 'focus:outline-0',
+      disabled: 'disabled:pointer-events-none',
+      group: 'group'
+    },
+    elevated: {
+      shadow: 'shadow-md shadow-black/50',
+      hover: 'hover:shadow-lg hover:shadow-black/50',
+      active: 'active:shadow-md active:shadow-black/50'
+    },
+    borderShapes: {
+      rounded: {
+        borderRadius: 'rounded-xl'
+      },
+      circular: {
+        borderRadius: 'rounded-full'
+      },
+      square: {
+        borderRadius: 'rounded-none'
+      }
     },
     sizes: {
+      xs: {
+        height: 'h-6',
+        padding: 'p-0.5'
+      },
       sm: {
-        filled: {
-          height: 'h-8',
-          padding: 'p-1.5'
-        },
-        tonal: {
-          height: 'h-8',
-          padding: 'p-1.5'
-        },
-        outlined: {
-          height: 'h-8',
-          padding: 'p-[0.3125rem]'
-        },
-        text: {
-          height: 'h-8',
-          padding: 'p-1.5'
-        }
+        height: 'h-8',
+        padding: 'p-1.5'
       },
       md: {
-        filled: {
-          height: 'h-10',
-          padding: 'p-2.5'
-        },
-        tonal: {
-          height: 'h-10',
-          padding: 'p-2.5'
-        },
-        outlined: {
-          height: 'h-10',
-          padding: 'p-[0.5625rem]'
-        },
-        text: {
-          height: 'h-10',
-          padding: 'p-2.5'
-        }
+        height: 'h-10',
+        padding: 'p-2.5'
       },
       lg: {
-        filled: {
-          height: 'h-12',
-          padding: 'p-3.5'
-        },
-        tonal: {
-          height: 'h-12',
-          padding: 'p-3.5'
-        },
-        outlined: {
-          height: 'h-12',
-          padding: 'p-[0.8125rem]'
-        },
-        text: {
-          height: 'h-12',
-          padding: 'p-3.5'
-        }
+        height: 'h-12',
+        padding: 'p-3.5'
+      }
+    },
+    outlineSizes: {
+      xs: {
+        padding: 'p-px'
+      },
+      sm: {
+        padding: 'p-[0.3125rem]'
+      },
+      md: {
+        padding: 'p-[0.5625rem]'
+      },
+      lg: {
+        padding: 'p-[0.8125rem]'
       }
     },
     variants: {
-      filled: {
+      plain: {
         light: {
+          default: {
+            fill: 'fill-light-on-surface-variant',
+            color: 'text-light-on-surface-variant',
+            disabled: 'disabled:fill-light-on-surface-variant/40 disabled:text-light-on-surface-variant/40'
+          },
           primary: {
             fill: 'fill-light-on-primary',
-            background: 'bg-light-primary',
-            hover: 'hover:shadow-md hover:shadow-black/70 hover:from-light-on-primary/10 hover:to-light-on-primary/10',
-            active: 'active:shadow-none active:from-light-on-primary/20 active:to-light-on-primary/20',
-            disabled: 'disabled:fill-light-on-surface/40 disabled:bg-light-on-surface/10'
+            color: 'text-light-on-primary',
+            disabled: 'disabled:fill-light-on-primary/40 disabled:text-light-on-primary/40'
           },
           secondary: {
             fill: 'fill-light-on-secondary',
-            background: 'bg-light-secondary',
-            hover: 'hover:shadow-md hover:shadow-black/70 hover:from-light-on-secondary/10 hover:to-light-on-secondary/10',
-            active: 'active:shadow-none active:from-light-on-secondary/20 active:to-light-on-secondary/20',
-            disabled: 'disabled:fill-light-on-surface/40 disabled:bg-light-on-surface/10'
+            color: 'text-light-on-secondary',
+            disabled: 'disabled:fill-light-on-secondary/40 disabled:text-light-on-secondary/40'
           },
           success: {
             fill: 'fill-light-on-success',
-            background: 'bg-light-success',
-            hover: 'hover:shadow-md hover:shadow-black/70 hover:from-light-on-success/10 hover:to-light-on-success/10',
-            active: 'active:shadow-none active:from-light-on-success/20 active:to-light-on-success/20',
-            disabled: 'disabled:fill-light-on-surface/40 disabled:bg-light-on-surface/10'
+            color: 'text-light-on-success',
+            disabled: 'disabled:fill-light-on-success/40 disabled:text-light-on-success/40'
           },
           warning: {
             fill: 'fill-light-on-warning',
-            background: 'bg-light-warning',
-            hover: 'hover:shadow-md hover:shadow-black/70 hover:from-light-on-warning/10 hover:to-light-on-warning/10',
-            active: 'active:shadow-none active:from-light-on-warning/20 active:to-light-on-warning/20',
-            disabled: 'disabled:fill-light-on-surface/40 disabled:bg-light-on-surface/10'
+            color: 'text-light-on-warning',
+            disabled: 'disabled:fill-light-on-warning/40 disabled:text-light-on-warning/40'
           },
           error: {
             fill: 'fill-light-on-error',
-            background: 'bg-light-error',
-            hover: 'hover:shadow-md hover:shadow-black/70 hover:from-light-on-error/10 hover:to-light-on-error/10',
-            active: 'active:shadow-none active:from-light-on-error/20 active:to-light-on-error/20',
-            disabled: 'disabled:fill-light-on-surface/40 disabled:bg-light-on-surface/10'
-          }
-        }
-      },
-      tonal: {
-        light: {
-          primary: {
-            fill: 'fill-light-on-primary-container',
-            background: 'bg-light-primary-container',
-            hover: 'hover:shadow-md hover:shadow-black/70 hover:from-light-on-primary-container/10 hover:to-light-on-primary-container/10',
-            active: 'active:shadow-none active:from-light-on-primary-container/20 active:to-light-on-primary-container/20',
-            disabled: 'disabled:fill-light-on-surface/40 disabled:bg-light-on-surface/10'
-          },
-          secondary: {
-            fill: 'fill-light-on-secondary-container',
-            background: 'bg-light-secondary-container',
-            hover: 'hover:shadow-md hover:shadow-black/70 hover:from-light-on-secondary-container/10 hover:to-light-on-secondary-container/10',
-            active: 'active:shadow-none active:from-light-on-secondary-container/20 active:to-light-on-secondary-container/20',
-            disabled: 'disabled:fill-light-on-surface/40 disabled:bg-light-on-surface/10'
-          },
-          success: {
-            fill: 'fill-light-on-success-container',
-            background: 'bg-light-success-container',
-            hover: 'hover:shadow-md hover:shadow-black/70 hover:from-light-on-success-container/10 hover:to-light-on-success-container/10',
-            active: 'active:shadow-none active:from-light-on-success-container/20 active:to-light-on-success-container/20',
-            disabled: 'disabled:fill-light-on-surface/40 disabled:bg-light-on-surface/10'
-          },
-          warning: {
-            fill: 'fill-light-on-warning-container',
-            background: 'bg-light-warning-container',
-            hover: 'hover:shadow-md hover:shadow-black/70 hover:from-light-on-warning-container/10 hover:to-light-on-warning-container/10',
-            active: 'active:shadow-none active:from-light-on-warning-container/20 active:to-light-on-warning-container/20',
-            disabled: 'disabled:fill-light-on-surface/40 disabled:bg-light-on-surface/10'
-          },
-          error: {
-            fill: 'fill-light-on-error-container',
-            background: 'bg-light-error-container',
-            hover: 'hover:shadow-md hover:shadow-black/70 hover:from-light-on-error-container/10 hover:to-light-on-error-container/10',
-            active: 'active:shadow-none active:from-light-on-error-container/20 active:to-light-on-error-container/20',
-            disabled: 'disabled:fill-light-on-surface/40 disabled:bg-light-on-surface/10'
-          }
-        }
-      },
-      outlined: {
-        light: {
-          primary: {
-            border: 'border border-light-primary',
-            fill: 'fill-light-primary',
-            hover: 'hover:from-light-primary/10 hover:to-light-primary/10',
-            active: 'active:from-light-primary/20 active:to-light-primary/20',
-            disabled: 'disabled:border-light-on-surface/10 disabled:fill-light-on-surface/40'
-          },
-          secondary: {
-            border: 'border border-light-secondary',
-            fill: 'fill-light-secondary',
-            hover: 'hover:from-light-secondary/10 hover:to-light-secondary/10',
-            active: 'active:from-light-secondary/20 active:to-light-secondary/20',
-            disabled: 'disabled:border-light-on-surface/10 disabled:fill-light-on-surface/40'
-          },
-          success: {
-            border: 'border border-light-success',
-            fill: 'fill-light-success',
-            hover: 'hover:from-light-success/10 hover:to-light-success/10',
-            active: 'active:from-light-success/20 active:to-light-success/20',
-            disabled: 'disabled:border-light-on-surface/10 disabled:fill-light-on-surface/40'
-          },
-          warning: {
-            border: 'border border-light-warning',
-            fill: 'fill-light-warning',
-            hover: 'hover:from-light-warning/10 hover:to-light-warning/10',
-            active: 'active:from-light-warning/20 active:to-light-warning/20',
-            disabled: 'disabled:border-light-on-surface/10 disabled:fill-light-on-surface/40'
-          },
-          error: {
-            border: 'border border-light-error',
-            fill: 'fill-light-error',
-            hover: 'hover:from-light-error/10 hover:to-light-error/10',
-            active: 'active:from-light-error/20 active:to-light-error/20',
-            disabled: 'disabled:border-light-on-surface/10 disabled:fill-light-on-surface/40'
+            color: 'text-light-on-error',
+            disabled: 'disabled:fill-light-on-error/40 disabled:text-light-on-error/40'
           }
         }
       },
       text: {
         light: {
+          default: {
+            fill: 'fill-light-on-surface',
+            color: 'text-light-on-surface',
+            disabled: 'disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40'
+          },
           primary: {
             fill: 'fill-light-primary',
-            hover: 'hover:from-light-primary/10 hover:to-light-primary/10',
-            active: 'active:from-light-primary/20 active:to-light-primary/20',
-            disabled: 'disabled:fill-light-on-surface/40'
+            color: 'text-light-primary',
+            disabled: 'disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40'
           },
           secondary: {
             fill: 'fill-light-secondary',
-            hover: 'hover:from-light-secondary/10 hover:to-light-secondary/10',
-            active: 'active:from-light-secondary/20 active:to-light-secondary/20',
-            disabled: 'disabled:fill-light-on-surface/40'
+            color: 'text-light-secondary',
+            disabled: 'disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40'
           },
           success: {
             fill: 'fill-light-success',
-            hover: 'hover:from-light-success/10 hover:to-light-success/10',
-            active: 'active:from-light-success/20 active:to-light-success/20',
-            disabled: 'disabled:fill-light-on-surface/40'
+            color: 'text-light-success',
+            disabled: 'disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40'
           },
           warning: {
             fill: 'fill-light-warning',
-            hover: 'hover:from-light-warning/10 hover:to-light-warning/10',
-            active: 'active:from-light-warning/20 active:to-light-warning/20',
-            disabled: 'disabled:fill-light-on-surface/40'
+            color: 'text-light-warning',
+            disabled: 'disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40'
           },
           error: {
             fill: 'fill-light-error',
-            hover: 'hover:from-light-error/10 hover:to-light-error/10',
-            active: 'active:from-light-error/20 active:to-light-error/20',
-            disabled: 'disabled:fill-light-on-surface/40'
+            color: 'text-light-error',
+            disabled: 'disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40'
+          }
+        }
+      },
+      outlined: {
+        light: {
+          default: {
+            border: 'border border-light-on-surface',
+            fill: 'fill-light-on-surface',
+            color: 'text-light-on-surface',
+            disabled: 'disabled:border-light-on-surface/40 disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40'
+          },
+          primary: {
+            border: 'border border-light-primary',
+            fill: 'fill-light-primary',
+            color: 'text-light-primary',
+            disabled: 'disabled:border-light-on-surface/40 disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40'
+          },
+          secondary: {
+            border: 'border border-light-secondary',
+            fill: 'fill-light-secondary',
+            color: 'text-light-secondary',
+            disabled: 'disabled:border-light-on-surface/40 disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40'
+          },
+          success: {
+            border: 'border border-light-success',
+            fill: 'fill-light-success',
+            color: 'text-light-success',
+            disabled: 'disabled:border-light-on-surface/40 disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40'
+          },
+          warning: {
+            border: 'border border-light-warning',
+            fill: 'fill-light-warning',
+            color: 'text-light-warning',
+            disabled: 'disabled:border-light-on-surface/40 disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40'
+          },
+          error: {
+            border: 'border border-light-error',
+            fill: 'fill-light-error',
+            color: 'text-light-error',
+            disabled: 'disabled:border-light-on-surface/40 disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40'
+          }
+        }
+      },
+      filled: {
+        light: {
+          default: {
+            fill: 'fill-light-on-surface',
+            color: 'text-light-on-surface',
+            background: 'bg-light-surface-dark',
+            hover: 'hover:shadow-md hover:shadow-black/50',
+            active: 'active:shadow-none',
+            disabled: 'disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40 disabled:bg-light-on-surface/20'
+          },
+          primary: {
+            fill: 'fill-light-on-primary',
+            color: 'text-light-on-primary',
+            background: 'bg-light-primary',
+            hover: 'hover:shadow-md hover:shadow-black/50',
+            active: 'active:shadow-none',
+            disabled: 'disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40 disabled:bg-light-on-surface/20'
+          },
+          secondary: {
+            fill: 'fill-light-on-secondary',
+            color: 'text-light-on-secondary',
+            background: 'bg-light-secondary',
+            hover: 'hover:shadow-md hover:shadow-black/50',
+            active: 'active:shadow-none',
+            disabled: 'disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40 disabled:bg-light-on-surface/20'
+          },
+          success: {
+            fill: 'fill-light-on-success',
+            color: 'text-light-on-success',
+            background: 'bg-light-success',
+            hover: 'hover:shadow-md hover:shadow-black/50',
+            active: 'active:shadow-none',
+            disabled: 'disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40 disabled:bg-light-on-surface/20'
+          },
+          warning: {
+            fill: 'fill-light-on-warning',
+            color: 'text-light-on-warning',
+            background: 'bg-light-warning',
+            hover: 'hover:shadow-md hover:shadow-black/50',
+            active: 'active:shadow-none',
+            disabled: 'disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40 disabled:bg-light-on-surface/20'
+          },
+          error: {
+            fill: 'fill-light-on-error',
+            color: 'text-light-on-error',
+            background: 'bg-light-error',
+            hover: 'hover:shadow-md hover:shadow-black/50',
+            active: 'active:shadow-none',
+            disabled: 'disabled:fill-light-on-surface/40 disabled:text-light-on-surface/40 disabled:bg-light-on-surface/20'
           }
         }
       }
@@ -240,4 +253,4 @@ const iconButtonConfig: IconButtonConfig = {
   }
 };
 
-export default iconButtonConfig;
+export default iconIconButtonConfig;
