9d8d83b42925e535e0c7e97bb618045dc201730f
Richard Wagner <richard.wagner@gmail.com>
2023-09-18 18:47:02 +0200

    fix

---
+++ src/lib/simpleComponents/components/layouts/Footer.tsx
@@ -0,0 +1,34 @@
+import React, { type BaseHTMLAttributes, forwardRef, useContext } from 'react';
+import footerConfig from '../../configs/footerConfig';
+import themeContext from '../../contexts/theme';
+import { mergeClasses } from '../../utils/propsHelper';
+
+export type FooterVariants = 'plain' | 'filled';
+
+export interface FooterProps extends BaseHTMLAttributes<HTMLElement> {
+  variant?: FooterVariants;
+}
+
+const Footer = forwardRef<HTMLElement, FooterProps>((props, ref) => {
+  /* --- Set context props --- */
+  const theme = useContext(themeContext).theme;
+
+  /* --- Set default props --- */
+  const styles = footerConfig.styles;
+  const { variant, className, ...restProps } = { ...footerConfig.defaultProps, ...props };
+
+  /* --- Set props --- */
+  const mergedClassName = mergeClasses(styles.base, styles.variants[variant][theme], className);
+
+  return (
+    <footer
+      className={mergedClassName}
+      ref={ref}
+      {...restProps}
+    />
+  );
+});
+
+Footer.displayName = 'Footer';
+
+export default Footer;
---
+++ src/lib/simpleComponents/configs/footerConfig.ts
@@ -0,0 +1,40 @@
+import { type FooterVariants } from '../components/layouts/Footer';
+
+export interface FooterConfig {
+  defaultProps: {
+    variant: FooterVariants;
+  };
+  styles: {
+    base: Record<string, string>;
+    variants: Record<FooterVariants, Record<Themes, Record<string, string>>>;
+  };
+}
+
+const footerConfig: FooterConfig = {
+  defaultProps: {
+    variant: 'plain'
+  },
+  styles: {
+    base: {
+      display: 'flex',
+      gap: 'gap-4',
+      alignItems: 'items-center',
+      width: 'w-full',
+      padding: 'py-2 px-4'
+    },
+    variants: {
+      plain: {
+        light: {
+          background: 'bg-light-surface-low'
+        }
+      },
+      filled: {
+        light: {
+          background: 'bg-light-surface'
+        }
+      }
+    }
+  }
+};
+
+export default footerConfig;
