984226f5e749edb843c10cf911e4518d3f26f649
Richard Wagner <richard.wagner@gmail.com>
2023-09-18 13:58:17 +0200

    fix

--- src/lib/simpleComponents/hooks/useResize.ts
+++ src/lib/simpleComponents/hooks/useResize.ts
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 
-const useResize = (onResize: (event?: UIEvent) => void, enabled: boolean = true): void => {
+const useResize = (onResize?: (event?: UIEvent) => void, enabled: boolean = true): void => {
   useEffect(() => {
-    if (enabled) {
+    if (enabled && onResize !== undefined) {
       window.addEventListener('resize', onResize);
     }
 
     return () => {
-      if (enabled) {
+      if (enabled && onResize !== undefined) {
         window.removeEventListener('resize', onResize);
       }
     };
--- src/lib/simpleComponents/hooks/useScroll.ts
+++ src/lib/simpleComponents/hooks/useScroll.ts
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 
-const useScroll = (onScroll: (event?: Event) => void, enabled: boolean = true): void => {
+const useScroll = (onScroll?: (event?: Event) => void, enabled: boolean = true): void => {
   useEffect(() => {
-    if (enabled) {
+    if (enabled && onScroll !== undefined) {
       document.addEventListener('scroll', onScroll);
     }
 
     return () => {
-      if (enabled) {
+      if (enabled && onScroll !== undefined) {
         document.removeEventListener('scroll', onScroll);
       }
     };
