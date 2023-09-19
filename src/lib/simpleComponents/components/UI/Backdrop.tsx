4125220bf0ef62fa4f66715ba958fd80e022a3c6
Richard Wagner <richard.wagner@gmail.com>
2023-09-20 00:29:07 +0200

    fix

--- src/lib/simpleComponents/components/UI/Backdrop.tsx
+++ src/lib/simpleComponents/components/UI/Backdrop.tsx
@@ -1,4 +1,4 @@
-import React, { type MouseEvent, forwardRef, useContext } from 'react';
+import React, { forwardRef, useContext } from 'react';
 import { createPortal } from 'react-dom';
 import backdropConfig from '../../configs/backdropConfig';
 import themeContext from '../../contexts/theme';
@@ -6,7 +6,6 @@ import { mergeClasses } from '../../utils/propsHelper';
 import Fade, { type FadeProps } from './Fade';
 
 export interface BackdropProps extends FadeProps {
-  onClose?: () => void;
   open?: boolean;
   invisible?: boolean;
   keepMounted?: boolean;
@@ -19,23 +18,13 @@ const Backdrop = forwardRef<HTMLDivElement, BackdropProps>((props, ref) => {
 
   /* --- Set default props --- */
   const styles = backdropConfig.styles;
-  const { onClose, onClick, open, invisible, keepMounted, overlayRef, className, ...restProps } = { ...backdropConfig.defaultProps, ...props };
+  const { open, invisible, keepMounted, overlayRef, className, ...restProps } = { ...backdropConfig.defaultProps, ...props };
 
   /* --- Set props --- */
-  const clickHandler = (event: MouseEvent<HTMLDivElement>): void => {
-    if (onClose !== undefined) {
-      onClose();
-    }
-
-    if (onClick !== undefined) {
-      onClick(event);
-    }
-  };
   const mergedClassName = mergeClasses(styles.base, styles.colors[theme], invisible && styles.invisible, className);
 
   const node = (
     <Fade
-      onClick={clickHandler}
       open={open}
       keepMounted={keepMounted}
       className={mergedClassName}
