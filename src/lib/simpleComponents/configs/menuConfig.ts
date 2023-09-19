84c19d4ceba6a947e5f6719bba5a7446d37dbf94
Richard Wagner <richard.wagner@gmail.com>
2023-09-19 19:10:37 +0200

    fix

--- src/lib/simpleComponents/configs/menuConfig.ts
+++ src/lib/simpleComponents/configs/menuConfig.ts
@@ -1,24 +1,58 @@
-import { type PopoverProps } from '../components/UI/Popover/Popover';
+import { type MenuVariants } from '../components/UI/Menu';
 
 export interface MenuConfig {
   defaultProps: {
-    color: Colors;
-    elevated: boolean;
-    position: Positions;
-    lockScroll: boolean;
-    overlayRef: Element | null;
-    popoverProps: PopoverProps;
+    variant?: MenuVariants;
+    position?: OuterPositions;
+    responsive?: boolean;
+    offset?: number;
+    lockScroll?: boolean;
+    closeOnAwayClick?: boolean;
+    keepMounted?: boolean;
+    backdrop?: boolean;
   };
+  styles: {
+    base: Record<string, string>;
+    variants: Record<MenuVariants, Record<Themes, Record<string, string>>>;
+  }
 }
 
 const menuConfig: MenuConfig = {
   defaultProps: {
-    color: 'light',
-    elevated: true,
+    variant: 'filled',
     position: 'bottom',
+    responsive: true,
+    offset: 0,
     lockScroll: false,
-    overlayRef: null,
-    popoverProps: {}
+    closeOnAwayClick: true,
+    keepMounted: false,
+    backdrop: false
+  },
+  styles: {
+    base: {
+      display: 'flex',
+      flexDirection: 'flex-col',
+      borderRadius: 'rounded-xl',
+      overflow: 'overflow-hidden'
+    },
+    variants: {
+      plain: {
+        light: {
+          background: 'bg-light-surface-low'
+        }
+      },
+      filled: {
+        light: {
+          background: 'bg-light-surface'
+        }
+      },
+      outlined: {
+        light: {
+          border: 'border border-light-divider',
+          background: 'bg-light-surface-low'
+        }
+      }
+    }
   }
 };
 
