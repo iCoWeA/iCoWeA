d5c4b761ad31577f8dfb9f4ca240300ccb68c870
Richard Wagner <richard.wagner@gmail.com>
2023-09-20 01:07:25 +0200

    fix

--- src/lib/simpleComponents/components/UI/Snackbar.tsx
+++ src/lib/simpleComponents/components/UI/Snackbar.tsx
@@ -1,7 +1,6 @@
-import React, { forwardRef, useRef, useImperativeHandle, type ReactNode } from 'react';
+import React, { forwardRef } from 'react';
 import snackbarConfig from '../../configs/snackbarConfig';
 import { mergeClasses } from '../../utils/propsHelper';
-import Backdrop, { type BackdropProps } from './Backdrop';
 import Popper, { type PopperProps } from './Popper';
 
 /* ARIA
@@ -20,58 +19,34 @@ export interface SnackbarProps extends PopperProps {
   closeDuration?: number;
   keepMounted?: boolean;
   backdrop?: boolean;
-  backdropProps?: BackdropProps;
   overlayRef?: Element | null;
 }
 
 const Snackbar = forwardRef<HTMLDivElement, SnackbarProps>((props, ref) => {
   /* --- Set default props --- */
   const styles = snackbarConfig.styles;
-  const { onClose, open, position, lockScroll, closeOnAwayClick, closeDuration, keepMounted, backdrop, backdropProps, overlayRef, className, ...restProps } = {
+  const { onClose, open, position, lockScroll, closeOnAwayClick, closeDuration, keepMounted, backdrop, overlayRef, className, ...restProps } = {
     ...snackbarConfig.defaultProps,
     ...props
   };
-
-  /* --- Set refs --- */
-  const popperRef = useRef<HTMLDivElement | null>(null);
-
-  /* --- Set imperative anchorElement --- */
-  useImperativeHandle<HTMLDivElement | null, HTMLDivElement | null>(ref, () => popperRef.current, []);
-
-  /* --- Set backdrop --- */
-  let backdropNode: ReactNode;
-
-  if (backdrop) {
-    backdropNode = (
-      <Backdrop
-        onClose={onClose}
-        open={open}
-        keepMounted={keepMounted}
-        invisible
-        {...backdropProps}
-      />
-    );
-  }
 
   /* --- Set props --- */
   const mergedClassName = mergeClasses(styles.base, styles.positions[position], className);
 
   return (
-    <>
-      {backdropNode}
-      <Popper
-        onClose={onClose}
-        open={open}
-        lockScroll={lockScroll}
-        closeOnAwayClick={backdrop ? false : closeOnAwayClick}
-        closeDuration={closeDuration}
-        keepMounted={keepMounted}
-        overlayRef={overlayRef}
-        className={mergedClassName}
-        ref={popperRef}
-        {...restProps}
-      />
-    </>
+    <Popper
+      onClose={onClose}
+      open={open}
+      lockScroll={lockScroll}
+      closeOnAwayClick={closeOnAwayClick}
+      closeDuration={closeDuration}
+      keepMounted={keepMounted}
+      backdrop={backdrop}
+      overlayRef={overlayRef}
+      className={mergedClassName}
+      ref={ref}
+      {...restProps}
+    />
   );
 });
 
