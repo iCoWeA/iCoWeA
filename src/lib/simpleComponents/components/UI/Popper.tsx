b0b10f0c665a22e9fa90f19f7cd602fd426f53e5
Richard Wagner <richard.wagner@gmail.com>
2023-09-20 12:38:49 +0200

    fix

--- src/lib/simpleComponents/components/UI/Popper.tsx
+++ src/lib/simpleComponents/components/UI/Popper.tsx
@@ -1,8 +1,8 @@
-import React, { type BaseHTMLAttributes, forwardRef, useRef, useImperativeHandle, useEffect, useCallback, type ReactNode, type TransitionEvent } from 'react';
+import React, { type BaseHTMLAttributes, forwardRef, useContext, useRef, useImperativeHandle, useEffect, type ReactNode, type TransitionEvent } from 'react';
 import { createPortal } from 'react-dom';
 import popperConfig from '../../configs/popperConfig';
+import themeContext from '../../contexts/theme';
 import useAnimation, { AnimationStates } from '../../hooks/useAnimation';
-import useOutsideClick from '../../hooks/useOutsideClick';
 import { mergeClasses } from '../../utils/propsHelper';
 import Backdrop, { type BackdropProps } from './Backdrop';
 
@@ -13,6 +13,8 @@ import Backdrop, { type BackdropProps } from './Backdrop';
  *
  */
 
+export type PopperVariants = 'plain' | 'filled' | 'outlined';
+
 export interface PopperProps extends BaseHTMLAttributes<HTMLDivElement> {
   onClose?: () => void;
   onEnter?: () => void;
@@ -20,6 +22,7 @@ export interface PopperProps extends BaseHTMLAttributes<HTMLDivElement> {
   onEntering?: () => void;
   onExiting?: () => void;
   onResize?: () => void;
+  variant?: PopperVariants;
   open?: boolean;
   lockScroll?: boolean;
   closeOnAwayClick?: boolean;
@@ -32,6 +35,9 @@ export interface PopperProps extends BaseHTMLAttributes<HTMLDivElement> {
 }
 
 const Popper = forwardRef<HTMLDivElement, PopperProps>((props, ref) => {
+  /* --- Set context props --- */
+  const theme = useContext(themeContext).theme;
+
   /* --- Set default props --- */
   const styles = popperConfig.styles;
   const {
@@ -42,6 +48,7 @@ const Popper = forwardRef<HTMLDivElement, PopperProps>((props, ref) => {
     onExiting,
     onResize,
     onTransitionEnd,
+    variant,
     open,
     lockScroll,
     closeOnAwayClick,
@@ -82,19 +89,26 @@ const Popper = forwardRef<HTMLDivElement, PopperProps>((props, ref) => {
   }, [open, animationState.enter, animationState.exit]);
 
   /* --- Set outside click action --- */
-  const outsideClickHandler = useCallback(
-    (event: MouseEvent) => {
+  useEffect(() => {
+    const outsideClickHandler = (event: MouseEvent): void => {
       const isPopperClicked = popperRef.current?.contains(event.target as Node) ?? false;
       const isAnchorClicked = anchorElement?.contains(event.target as Node) ?? false;
 
       if (!isPopperClicked && !isAnchorClicked && onClose !== undefined) {
         onClose();
       }
-    },
-    [anchorElement, onClose]
-  );
+    };
 
-  useOutsideClick(outsideClickHandler, closeOnAwayClick && animationState.enter && onClose !== undefined && !backdrop);
+    if (closeOnAwayClick && animationState.enter && onClose !== undefined && !backdrop) {
+      document.addEventListener('click', outsideClickHandler);
+    }
+
+    return () => {
+      if (closeOnAwayClick && animationState.enter && onClose !== undefined && !backdrop) {
+        document.removeEventListener('click', outsideClickHandler);
+      }
+    };
+  }, [onClose, closeOnAwayClick, animationState.enter, backdrop]);
 
   /* --- Set timer action --- */
   useEffect(() => {
@@ -186,6 +200,7 @@ const Popper = forwardRef<HTMLDivElement, PopperProps>((props, ref) => {
 
   const mergedClassName = mergeClasses(
     styles.base,
+    styles.variants[variant][theme],
     animationState.enter && styles.open,
     animationState.current === AnimationStates.EXITED && !open && styles.hide,
     className
