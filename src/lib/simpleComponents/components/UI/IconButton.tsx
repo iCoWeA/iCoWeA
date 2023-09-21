596e06d4ae6626562797e3af16e63481589cb5a2
Richard Wagner <richard.wagner@gmail.com>
2023-09-21 13:47:59 +0200

    fix

--- src/lib/simpleComponents/components/UI/IconButton.tsx
+++ src/lib/simpleComponents/components/UI/IconButton.tsx
@@ -1,26 +1,16 @@
-import React, { type ButtonHTMLAttributes, type BaseHTMLAttributes, forwardRef, useContext } from 'react';
+import React, { forwardRef } from 'react';
 import iconButtonConfig from '../../configs/iconButtonConfig';
-import themeContext from '../../contexts/theme';
 import { mergeClasses } from '../../utils/propsHelper';
+import Button, { type ButtonProps } from './Button';
 
-export type IconButtonVariants = 'plain' | 'text' | 'outlined' | 'filled';
-
-export interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: IconButtonVariants;
+export interface IconButtonProps extends ButtonProps {
   borderShape?: Shapes;
-  size?: Sizes;
-  color?: Colors;
-  elevated?: boolean;
-  stateLayerProps?: BaseHTMLAttributes<HTMLSpanElement>;
 }
 
 const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>((props, ref) => {
-  /* --- Set context props --- */
-  const theme = useContext(themeContext).theme;
-
   /* --- Set default props --- */
   const styles = iconButtonConfig.styles;
-  const { variant, borderShape, size, color, elevated, stateLayerProps, disabled, className, children, ...restProps } = {
+  const { variant, size, borderShape, className, ...restProps } = {
     ...iconButtonConfig.defaultProps,
     ...props
   };
@@ -28,24 +18,19 @@ const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>((props, ref) =
   /* --- Set props --- */
   const mergedClassName = mergeClasses(
     styles.base,
-    styles.variants[variant][theme][color],
     styles.borderShapes[borderShape],
     styles.sizes[size],
     variant === 'outlined' && styles.outlineSizes[size],
-    elevated && styles.elevated,
     className
   );
 
   return (
-    <button
-      disabled={disabled}
+    <Button
+      variant={variant}
       className={mergedClassName}
-      type="button"
       ref={ref}
       {...restProps}
-    >
-      {children}
-    </button>
+    />
   );
 });
 
