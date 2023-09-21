71ad2c491dac9e3ff2755184398d1ee4db70c91b
Richard Wagner <richard.wagner@gmail.com>
2023-09-21 23:44:16 +0200

    fix

--- src/lib/simpleComponents/configs/chipButtonConfig.ts
+++ src/lib/simpleComponents/configs/chipButtonConfig.ts
@@ -3,14 +3,12 @@ import { type ChipVariants } from '../components/UI/Chip';
 export interface ChipButtonConfig {
   defaultProps: {
     chipVariant: ChipVariants;
-    color: Colors;
   };
 }
 
 const chipButtonConfig: ChipButtonConfig = {
   defaultProps: {
-    chipVariant: 'filled',
-    color: 'primary'
+    chipVariant: 'filled'
   }
 };
 
