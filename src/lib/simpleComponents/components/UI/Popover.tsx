6b948ce330c1d25a45d9316390da1f79f4672b7b
Richard Wagner <richard.wagner@gmail.com>
2023-09-21 03:38:09 +0200

    fix

--- src/lib/simpleComponents/components/UI/Popover.tsx
+++ src/lib/simpleComponents/components/UI/Popover.tsx
@@ -5,16 +5,13 @@ import React, {
   cloneElement,
   useContext,
   useRef,
-  useState,
   useImperativeHandle,
-  useEffect,
   useCallback,
   type ReactNode,
   type MouseEvent
 } from 'react';
 import popoverConfig from '../../configs/popoverConfig';
 import themeContext from '../../contexts/theme';
-import usePrevious from '../../hooks/usePrevious';
 import { setElementPosition } from '../../utils/positiontHelper';
 import { mergeClasses } from '../../utils/propsHelper';
 import Popper, { type PopperProps } from './Popper';
@@ -89,45 +86,13 @@ const Popover = forwardRef<HTMLDivElement, PopoverProps>((props, ref) => {
     ...popoverConfig.defaultProps,
     ...props
   };
-  const isControlled = open !== undefined;
 
   /* --- Set refs --- */
   const popperRef = useRef<HTMLDivElement | null>(null);
   const handlerRef = useRef<HTMLElement | null>(null);
 
-  /* --- Set states --- */
-  const [isOpen, setIsOpen] = useState(false);
-
   /* --- Set imperative anchorElement --- */
   useImperativeHandle<HTMLDivElement | null, HTMLDivElement | null>(ref, () => popperRef.current, []);
-
-  /* --- Set previous values  --- */
-  const prevOpen = usePrevious(open);
-
-  useEffect(() => {
-    if (prevOpen !== undefined && open === undefined) {
-      setIsOpen(prevOpen);
-    }
-  }, [open]);
-
-  useEffect(() => {
-    const keyDownHandler = (event: KeyboardEvent): void => {
-      if (event.key === 'Escape' && isOpen) {
-        handlerRef.current?.focus();
-        setIsOpen(false);
-      }
-    };
-
-    if (!isControlled) {
-      document.addEventListener('keydown', keyDownHandler);
-    }
-
-    return () => {
-      if (!isControlled) {
-        document.removeEventListener('keydown', keyDownHandler);
-      }
-    };
-  }, [isOpen, isControlled]);
 
   /* --- Set position action --- */
   const resizeHandler = useCallback(() => {
@@ -162,12 +127,8 @@ const Popover = forwardRef<HTMLDivElement, PopoverProps>((props, ref) => {
     };
 
     const clickHandler = (event: MouseEvent<HTMLElement>): void => {
-      if (isControlled && onClose !== undefined) {
+      if (onClose !== undefined) {
         onClose();
-      }
-
-      if (!isControlled) {
-        setIsOpen(false);
       }
 
       if (handler.props.onClick !== undefined) {
@@ -178,7 +139,7 @@ const Popover = forwardRef<HTMLDivElement, PopoverProps>((props, ref) => {
     handlerNode = (
       <Handler
         onClick={clickHandler}
-        open={open ?? isOpen}
+        open={open}
         ref={setHandlerRef}
       >
         {handler}
@@ -187,27 +148,18 @@ const Popover = forwardRef<HTMLDivElement, PopoverProps>((props, ref) => {
   }
 
   /* --- Set props --- */
-  let closeHandler = onClose;
-
-  if (!isControlled) {
-    closeHandler = () => {
-      setIsOpen(false);
-    };
-  }
-
   const mergedClassName = mergeClasses(styles.base, styles.variants[variant][theme], className);
 
   return (
     <>
       {handlerNode}
       <Popper
-        onClose={closeHandler}
+        onClose={onClose}
         onResize={resizeHandler}
-        open={open ?? isOpen}
+        open={open}
         lockScroll={lockScroll}
         closeOnAwayClick={closeOnAwayClick}
         keepMounted={keepMounted}
-        anchorElement={handlerRef.current}
         overlayRef={overlayRef}
         className={mergedClassName}
         ref={popperRef}
