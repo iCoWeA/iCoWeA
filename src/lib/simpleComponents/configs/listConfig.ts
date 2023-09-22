4231be528bb59a13e8f5b0c00b13b2598ff5a1c1
Richard Wagner <richard.wagner@gmail.com>
2023-09-22 19:19:59 +0200

    fix

--- src/lib/simpleComponents/configs/listConfig.ts
+++ src/lib/simpleComponents/configs/listConfig.ts
@@ -1,15 +1,25 @@
 export interface ListConfig {
+  defaultProps: {
+    group: boolean;
+  },
   styles: {
     base: Record<string, string>;
+    group: Record<string, string>;
   }
 }
 
 const listConfig: ListConfig = {
+  defaultProps: {
+    group: false
+  },
   styles: {
     base: {
       display: 'flex',
       flexDirection: 'flex-col',
       padding: 'py-2'
+    },
+    group: {
+      padding: 'p-0'
     }
   }
 };
