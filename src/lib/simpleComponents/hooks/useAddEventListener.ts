4a0cfa1cb608098046e904e085c6de4148aca8e8
Richard Wagner <richard.wagner@gmail.com>
2023-09-22 04:28:37 +0200

    fix

---
+++ src/lib/simpleComponents/hooks/useAddEventListener.ts
@@ -0,0 +1,13 @@
+import { type MutableRefObject, useEffect } from 'react';
+
+const useAddEventListener = (elementRef: MutableRefObject<HTMLElement | null>, event: string, handler: () => void): void => {
+  useEffect(() => {
+    elementRef.current?.addEventListener(event, handler);
+
+    return () => {
+      elementRef.current?.removeEventListener(event, handler);
+    };
+  }, [event, handler]);
+};
+
+export default useAddEventListener;
