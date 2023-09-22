849c14e68c3140ea18ca1dc81901a22c2c6cf89f
Richard Wagner <richard.wagner@gmail.com>
2023-09-22 16:58:29 +0200

    fix

--- src/lib/simpleComponents/configs/dividerConfig.ts
+++ src/lib/simpleComponents/configs/dividerConfig.ts
@@ -1,21 +1,30 @@
+import { type DividerVariants } from '../components/UI/Divider';
+
 export interface DividerConfig {
   defaultProps: {
     orientation: Orientations;
+    variant: DividerVariants;
+    disabled: boolean;
   }
   styles: {
     base: Record<string, string>;
     orientations: Record<Orientations, Record<string, string>>;
+    disabled: Record<Themes, Record<string, string>>;
     color: Record<Themes, Record<string, string>>;
+    variants: Record<DividerVariants, Record<Themes, Record<Colors, Record<string, string>>>>;
   }
 }
 
 const dividerConfig: DividerConfig = {
   defaultProps: {
-    orientation: 'horizontal'
+    orientation: 'horizontal',
+    variant: 'plain',
+    disabled: false
   },
   styles: {
     base: {
-      display: 'block'
+      display: 'block',
+      border: 'border-0'
     },
     orientations: {
       horizontal: {
@@ -30,6 +39,101 @@ const dividerConfig: DividerConfig = {
     color: {
       light: {
         background: 'bg-light-divider'
+      }
+    },
+    disabled: {
+      light: {
+        background: 'bg-light-on-surface/40'
+      }
+    },
+    variants: {
+      plain: {
+        light: {
+          default: {
+            background: 'bg-light-on-surface'
+          },
+          primary: {
+            background: 'bg-light-on-primary'
+          },
+          secondary: {
+            background: 'bg-light-on-secondary'
+          },
+          success: {
+            background: 'bg-light-on-success'
+          },
+          warning: {
+            background: 'bg-light-on-warning'
+          },
+          error: {
+            background: 'bg-light-on-error'
+          }
+        }
+      },
+      text: {
+        light: {
+          default: {
+            background: 'bg-light-on-surface'
+          },
+          primary: {
+            background: 'bg-light-primary'
+          },
+          secondary: {
+            background: 'bg-light-secondary'
+          },
+          success: {
+            background: 'bg-light-success'
+          },
+          warning: {
+            background: 'bg-light-warning'
+          },
+          error: {
+            background: 'bg-light-error'
+          }
+        }
+      },
+      outlined: {
+        light: {
+          default: {
+            background: 'bg-light-on-surface'
+          },
+          primary: {
+            background: 'bg-light-primary'
+          },
+          secondary: {
+            background: 'bg-light-secondary'
+          },
+          success: {
+            background: 'bg-light-success'
+          },
+          warning: {
+            background: 'bg-light-warning'
+          },
+          error: {
+            background: 'bg-light-error'
+          }
+        }
+      },
+      filled: {
+        light: {
+          default: {
+            background: 'bg-light-on-surface'
+          },
+          primary: {
+            background: 'bg-light-on-primary'
+          },
+          secondary: {
+            background: 'bg-light-on-secondary'
+          },
+          success: {
+            background: 'bg-light-on-success'
+          },
+          warning: {
+            background: 'bg-light-on-warning'
+          },
+          error: {
+            background: 'bg-light-on-error'
+          }
+        }
       }
     }
   }
