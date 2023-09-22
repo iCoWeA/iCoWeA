00b249afb77df21e66a3e51579b320e44539345b
Richard Wagner <richard.wagner@gmail.com>
2023-09-22 05:04:09 +0200

    fix

--- src/lib/simpleComponents/hooks/useClickAway.ts
+++ src/lib/simpleComponents/hooks/useClickAway.ts
@@ -1,25 +1,21 @@
-import { useEffect } from 'react';
+import { type MutableRefObject, useEffect } from 'react';
 
-const useClickAway = (onClose: (() => void) | null, ...elements: Array<HTMLElement | null>): void => {
+const useClickAway = (onClose: (() => void) | null, ...elements: Array<MutableRefObject<HTMLElement | null>>): void => {
   useEffect(() => {
-    const clickHandler = (event: MouseEvent): void => {
-      if (onClose === null) {
-        return;
-      }
+    if (onClose === null) {
+      return;
+    }
 
-      if (elements.every((element) => !(element?.contains(event.target as Node) ?? false))) {
+    const clickHandler = (event: MouseEvent): void => {
+      if (elements.every((element) => !(element.current?.contains(event.target as Node) ?? false))) {
         onClose();
       }
     };
 
-    if (onClose !== null) {
-      document.addEventListener('click', clickHandler);
-    }
+    document.addEventListener('click', clickHandler);
 
     return () => {
-      if (onClose !== null) {
-        document.addEventListener('click', clickHandler);
-      }
+      document.addEventListener('click', clickHandler);
     };
   }, [onClose, ...elements]);
 };
