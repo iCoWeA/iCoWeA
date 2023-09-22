ff12f7cb532abd9179f1df572cdd5a95368cc366
Richard Wagner <richard.wagner@gmail.com>
2023-09-22 19:22:40 +0200

    fix

--- src/lib/simpleComponents/components/UI/List.tsx
+++ src/lib/simpleComponents/components/UI/List.tsx
@@ -2,18 +2,27 @@ import React, { type BaseHTMLAttributes, forwardRef } from 'react';
 import listConfig from '../../configs/listConfig';
 import { mergeClasses } from '../../utils/propsHelper';
 
-export interface ListProps extends BaseHTMLAttributes<HTMLUListElement> {}
+export interface ListProps extends BaseHTMLAttributes<HTMLUListElement> {
+  group?: boolean;
+}
 
 const List = forwardRef<HTMLUListElement, ListProps>((props, ref) => {
   /* --- Set default props --- */
   const styles = listConfig.styles;
-  const { className, ...restProps } = { ...props };
+  const { group, className, ...restProps } = { ...listConfig.defaultProps, ...props };
 
   /* --- Set props --- */
-  const mergedClassName = mergeClasses(styles.base, className);
+  const groupProps: BaseHTMLAttributes<HTMLUListElement> = {};
+
+  if (group) {
+    groupProps.role = 'group';
+  }
+
+  const mergedClassName = mergeClasses(styles.base, group && styles.group, className);
 
   return (
     <ul
+      {...groupProps}
       className={mergedClassName}
       ref={ref}
       {...restProps}
