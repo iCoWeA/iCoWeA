04569eb8520061f4bf74a1f086557a470ae67d5e
Richard Wagner <richard.wagner@gmail.com>
2023-09-24 02:56:57 +0200

    fix

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,3 @@
 export {};
 
-declare global {
-  export interface Message {
-    name: string;
-    email: string;
-    message: string;
-    date: string;
-    isRead: boolean;
-  }
-};
+declare global {};
