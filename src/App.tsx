3035ec19783eb9cfbfa790c4f48580138bc3489c
Richard Wagner <richard.wagner@gmail.com>
2023-09-24 05:20:15 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -12,27 +12,11 @@ const router = createBrowserRouter([
     children: [
       {
         path: '',
-        lazy: async () => await import('./pages/DefaultLayout'),
+        lazy: async () => await import('./pages/StandardLayout'),
         children: [
           {
-            path: 'login',
-            lazy: async () => await import('./pages/Login')
-          }
-        ]
-      },
-      {
-        path: '',
-        lazy: async () => await import('./pages/DashboardLayout'),
-        children: [
-          {
-            path: 'admin',
-            lazy: async () => await import('./pages/Admin'),
-            children: [
-              {
-                path: 'messages',
-                lazy: async () => await import('./pages/AdminMessages')
-              }
-            ]
+            path: 'container',
+            lazy: async () => await import('./pages/Container')
           }
         ]
       }
@@ -40,74 +24,12 @@ const router = createBrowserRouter([
   }
 ]);
 
-const App: FC = () => {
-  return (
-    <ThemeProvider>
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
-    </ThemeProvider>
-  );
-};
+const App: FC = () => (
+  <ThemeProvider>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </ThemeProvider>
+);
 
 export default App;
-
-/* <RouterProvider router={router} /> */
-
-/* <RouterProvider router={router} />
-
-import React, { type FC } from 'react';
-import { Provider } from 'react-redux';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import ThemeProvider from './lib/simpleComponents/components/providers/ThemeProvider';
-import store from './store';
-import Root from './pages/Root';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    children: [
-      {
-        path: '',
-        lazy: async () => await import('./pages/DefaultLayout'),
-        children: [
-          {
-            path: 'login',
-            lazy: async () => await import('./pages/Login')
-          }
-        ]
-      },
-      {
-        path: '',
-        lazy: async () => await import('./pages/DashboardLayout'),
-        children: [
-          {
-            path: 'admin',
-            lazy: async () => await import('./pages/Admin'),
-            children: [
-              {
-                path: 'messages',
-                lazy: async () => await import('./pages/AdminMessages')
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-]);
-
-const App: FC = () => {
-  return (
-    <ThemeProvider>
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
-    </ThemeProvider>
-  );
-};
-
-export default App;
-
-/* <RouterProvider router={router} /> */
