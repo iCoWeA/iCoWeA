f40c740b3915c76b1a2c62a44d298c64d14d20f3
Richard Wagner <richard.wagner@gmail.com>
2023-09-24 16:46:02 +0200

    fix

--- src/pages/StandardLayout/index.tsx
+++ src/pages/StandardLayout/index.tsx
@@ -3,13 +3,11 @@ import { Outlet } from 'react-router-dom';
 import MainFooter from '../../layouts/MainFooter';
 import MainHeader from '../../layouts/MainHeader';
 import Box from '../../lib/simpleComponents/components/UI/Box';
-import Divider from '../../lib/simpleComponents/components/UI/Divider';
 
 export const Component: FC = () => (
   <Box variant="layout">
     <MainHeader />
     <Outlet />
-    <Divider />
     <MainFooter />
   </Box>
 );
