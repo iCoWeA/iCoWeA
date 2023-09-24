601124ea39c2b4dfeb08e92e955b02a1c991434b
Richard Wagner <richard.wagner@gmail.com>
2023-09-24 05:19:24 +0200

    fix

--- src/pages/StandardLayout/index.tsx
+++ src/pages/StandardLayout/index.tsx
@@ -1,12 +1,13 @@
 import React, { type FC } from 'react';
 import { Outlet } from 'react-router-dom';
 import MainHeader from '../../layouts/MainHeader';
+import Container from '../../lib/simpleComponents/components/UI/Container';
 
 export const Component: FC = () => (
-  <>
+  <Container variant="page">
     <MainHeader />
     <Outlet />
-  </>
+  </Container>
 );
 
 Component.displayName = 'StandardLayoutRoute';
