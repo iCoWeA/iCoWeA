9fe424b00fe8dccd6f635154df08c2dfc0e7c51a
Richard Wagner <richard.wagner@gmail.com>
2023-09-25 22:20:28 +0200

    fix

---
+++ src/pages/Button/ShapesExample.tsx
@@ -0,0 +1,16 @@
+import React, { type FC } from 'react';
+import ExampleSection from '../../components/ExampleSection';
+import Button from '../../lib/simpleComponents/components/UI/Button';
+
+const ShapesExample: FC = () => (
+  <ExampleSection
+    title="Shapes"
+    titleId="shapes"
+  >
+    <Button shape="circular">Button</Button>
+    <Button shape="rounded">Button</Button>
+    <Button shape="square">Button</Button>
+  </ExampleSection>
+);
+
+export default ShapesExample;
