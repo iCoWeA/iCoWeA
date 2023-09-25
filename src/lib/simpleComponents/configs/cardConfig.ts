bf454e07e23efd0c0cf5b767240c294a7ec78c70
Richard Wagner <richard.wagner@gmail.com>
2023-09-25 17:11:10 +0200

    fix

--- src/lib/simpleComponents/configs/cardConfig.ts
+++ src/lib/simpleComponents/configs/cardConfig.ts
@@ -1,8 +1,7 @@
-import { type CardVariants } from '../components/UI/Card';
-
 export interface CardConfig {
   defaultProps: {
-    variant: CardVariants;
+    variant: Variants;
+    color: Colors;
     elevated: boolean;
     clickable: boolean;
     grabed: boolean;
@@ -10,17 +9,21 @@ export interface CardConfig {
   };
   styles: {
     base: Record<string, string>;
+    stateLayer: Record<string, string>;
     elevated: Record<string, string>;
-    disabled: Record<Themes, Record<string, string>>;
-    clickable: Record<Themes, Record<string, string>>;
-    grabed: Record<Themes, Record<string, string>>;
-    variants: Record<CardVariants, Record<Themes, Record<string, string>>>;
+    clickable: Record<string, string>;
+    grabed: Record<string, string>;
+    sizes: Record<Sizes, Record<string, string>>;
+    stateLayerVariants: Record<TextVariants, Record<Themes, Record<Colors, Record<string, string>>>>;
+    stateLayerGrabed: Record<TextVariants, Record<Themes, Record<Colors, Record<string, string>>>>;
+    disabled: Record<Variants, Record<Themes, Record<string, string>>>;
   }
 }
 
 const cardConfig: CardConfig = {
   defaultProps: {
-    variant: 'plain',
+    variant: 'text',
+    color: 'default',
     elevated: false,
     clickable: false,
     grabed: false,
@@ -28,52 +31,331 @@ const cardConfig: CardConfig = {
   },
   styles: {
     base: {
-      display: 'flex',
-      flexDirection: 'flex-col',
       borderRadius: 'rounded-xl',
       overflow: 'overflow-hidden'
     },
-    elevated: {
-      shadow: 'shadow-md shadow-black/50'
+    stateLayer: {
+      position: 'after:absolute',
+      top: 'after:-top-px',
+      left: 'after:-left-px',
+      display: 'after:block',
+      width: 'after:w-[calc(100%_+_2px)]',
+      height: 'after:h-[calc(100%_+_2px)]',
+      transitions: 'after:transition-colors',
+      pointerEvents: 'after:pointer-events-none'
     },
-    disabled: {
-      light: {
-        userSelect: 'select-none',
-        pointerEvents: 'pointer-event-none',
-        background: 'bg-light-on-surface/20',
-        hover: 'hover:bg-none',
-        active: 'active:bg-none',
-        pointer: 'cursor-default'
-      }
+    elevated: {
+      shadow: 'shadow-md'
     },
     clickable: {
-      light: {
-        hover: 'hover:bg-gradient-to-r hover:from-light-on-surface/10 hover:to-light-on-surface/10',
-        active: 'active:bg-gradient-to-r active:from-light-on-surface/[0.15] active:to-light-on-surface/[0.15]',
-        focus: 'focus:outline-0 focus:bg-gradient-to-r focus:from-light-on-surface/[0.15] focus:to-light-on-surface/[0.15]',
-        focusVisible: 'focus-visible:ring-2 focus-visible:ring-light-on-surface'
-      }
+      position: 'relative'
     },
     grabed: {
-      light: {
-        background: 'bg-gradient-to-r from-light-on-surface/20 to-light-on-surface/20'
+      position: 'relative'
+    },
+    sizes: {
+      xs: {
+        padding: 'px-xs-p'
+      },
+      sm: {
+        padding: 'px-sm-p'
+      },
+      md: {
+        padding: 'px-md-p'
+      },
+      lg: {
+        padding: 'px-lg-p'
       }
     },
-    variants: {
+    stateLayerVariants: {
       plain: {
         light: {
-          background: 'bg-light-surface-low'
+          default: {
+            hover: 'hover:after:bg-light-on-surface/10',
+            active: 'active:after:bg-light-on-surface/20',
+            focus: 'focus:after:bg-light-on-surface/20',
+            focusVisible: 'focus-visible:ring-light-on-surface'
+          },
+          primary: {
+            hover: 'hover:after:bg-light-on-primary/10',
+            active: 'active:after:bg-light-on-primary/20',
+            focus: 'focus:after:bg-light-on-primary/20',
+            focusVisible: 'focus-visible:ring-light-primary'
+          },
+          secondary: {
+            hover: 'hover:after:bg-light-on-secondary/10',
+            active: 'active:after:bg-light-on-secondary/20',
+            focus: 'focus:after:bg-light-on-secondary/20',
+            focusVisible: 'focus-visible:ring-light-secondary'
+          },
+          success: {
+            hover: 'hover:after:bg-light-on-success/10',
+            active: 'active:after:bg-light-on-success/20',
+            focus: 'focus:after:bg-light-on-success/20',
+            focusVisible: 'focus-visible:ring-light-success'
+          },
+          warning: {
+            hover: 'hover:after:bg-light-on-warning/10',
+            active: 'active:after:bg-light-on-warning/20',
+            focus: 'focus:after:bg-light-on-warning/20',
+            focusVisible: 'focus-visible:ring-light-warning'
+          },
+          error: {
+            hover: 'hover:after:bg-light-on-error/10',
+            active: 'active:after:bg-light-on-error/20',
+            focus: 'focus:after:bg-light-on-error/20',
+            focusVisible: 'focus-visible:ring-light-error'
+          }
+        },
+        dark: {
+          default: {
+            hover: 'hover:after:bg-dark-on-surface/10',
+            active: 'active:after:bg-dark-on-surface/20',
+            focus: 'focus:after:bg-dark-on-surface/20',
+            focusVisible: 'focus-visible:ring-dark-on-surface'
+          },
+          primary: {
+            hover: 'hover:after:bg-dark-on-primary/10',
+            active: 'active:after:bg-dark-on-primary/20',
+            focus: 'focus:after:bg-dark-on-primary/20',
+            focusVisible: 'focus-visible:ring-dark-primary'
+          },
+          secondary: {
+            hover: 'hover:after:bg-dark-on-secondary/10',
+            active: 'active:after:bg-dark-on-secondary/20',
+            focus: 'focus:after:bg-dark-on-secondary/20',
+            focusVisible: 'focus-visible:ring-dark-secondary'
+          },
+          success: {
+            hover: 'hover:after:bg-dark-on-success/10',
+            active: 'active:after:bg-dark-on-success/20',
+            focus: 'focus:after:bg-dark-on-success/20',
+            focusVisible: 'focus-visible:ring-dark-success'
+          },
+          warning: {
+            hover: 'hover:after:bg-dark-on-warning/10',
+            active: 'active:after:bg-dark-on-warning/20',
+            focus: 'focus:after:bg-dark-on-warning/20',
+            focusVisible: 'focus-visible:ring-dark-warning'
+          },
+          error: {
+            hover: 'hover:after:bg-dark-on-error/10',
+            active: 'active:after:bg-dark-on-error/20',
+            focus: 'focus:after:bg-dark-on-error/20',
+            focusVisible: 'focus-visible:ring-dark-error'
+          }
         }
       },
-      filled: {
+      solid: {
         light: {
-          background: 'bg-light-surface'
+          default: {
+            hover: 'hover:after:bg-light-on-surface/10',
+            active: 'active:after:bg-light-on-surface/20',
+            focus: 'focus:after:bg-light-on-surface/20',
+            focusVisible: 'focus-visible:ring-light-on-surface'
+          },
+          primary: {
+            hover: 'hover:after:bg-light-primary/10',
+            active: 'active:after:bg-light-primary/20',
+            focus: 'focus:after:bg-light-primary/20',
+            focusVisible: 'focus-visible:ring-light-primary'
+          },
+          secondary: {
+            hover: 'hover:after:bg-light-secondary/10',
+            active: 'active:after:bg-light-secondary/20',
+            focus: 'focus:after:bg-light-secondary/20',
+            focusVisible: 'focus-visible:ring-light-secondary'
+          },
+          success: {
+            hover: 'hover:after:bg-light-success/10',
+            active: 'active:after:bg-light-success/20',
+            focus: 'focus:after:bg-light-success/20',
+            focusVisible: 'focus-visible:ring-light-success'
+          },
+          warning: {
+            hover: 'hover:after:bg-light-warning/10',
+            active: 'active:after:bg-light-warning/20',
+            focus: 'focus:after:bg-light-warning/20',
+            focusVisible: 'focus-visible:ring-light-warning'
+          },
+          error: {
+            hover: 'hover:after:bg-light-error/10',
+            active: 'active:after:bg-light-error/20',
+            focus: 'focus:after:bg-light-error/20',
+            focusVisible: 'focus-visible:ring-light-error'
+          }
+        },
+        dark: {
+          default: {
+            hover: 'hover:after:bg-dark-on-surface/10',
+            active: 'active:after:bg-dark-on-surface/20',
+            focus: 'focus:after:bg-dark-on-surface/20',
+            focusVisible: 'focus-visible:ring-dark-on-surface'
+          },
+          primary: {
+            hover: 'hover:after:bg-dark-primary/10',
+            active: 'active:after:bg-dark-primary/20',
+            focus: 'focus:after:bg-dark-primary/20',
+            focusVisible: 'focus-visible:ring-dark-primary'
+          },
+          secondary: {
+            hover: 'hover:after:bg-dark-secondary/10',
+            active: 'active:after:bg-dark-secondary/20',
+            focus: 'focus:after:bg-dark-secondary/20',
+            focusVisible: 'focus-visible:ring-dark-secondary'
+          },
+          success: {
+            hover: 'hover:after:bg-dark-success/10',
+            active: 'active:after:bg-dark-success/20',
+            focus: 'focus:after:bg-dark-success/20',
+            focusVisible: 'focus-visible:ring-dark-success'
+          },
+          warning: {
+            hover: 'hover:after:bg-dark-warning/10',
+            active: 'active:after:bg-dark-warning/20',
+            focus: 'focus:after:bg-dark-warning/20',
+            focusVisible: 'focus-visible:ring-dark-warning'
+          },
+          error: {
+            hover: 'hover:after:bg-dark-error/10',
+            active: 'active:after:bg-dark-error/20',
+            focus: 'focus:after:bg-dark-error/20',
+            focusVisible: 'focus-visible:ring-dark-error'
+          }
+        }
+      }
+    },
+    stateLayerGrabed: {
+      plain: {
+        light: {
+          default: {
+            background: 'after:bg-light-on-surface/25'
+          },
+          primary: {
+            background: 'after:bg-light-on-primary/25'
+          },
+          secondary: {
+            background: 'after:bg-light-on-secondary/25'
+          },
+          success: {
+            background: 'after:bg-light-on-success/25'
+          },
+          warning: {
+            background: 'after:bg-light-on-warning/25'
+          },
+          error: {
+            background: 'after:bg-light-on-error/25'
+          }
+        },
+        dark: {
+          default: {
+            background: 'after:bg-dark-on-surface/25'
+          },
+          primary: {
+            background: 'after:bg-dark-on-primary/25'
+          },
+          secondary: {
+            background: 'after:bg-dark-on-secondary/25'
+          },
+          success: {
+            background: 'after:bg-dark-on-success/25'
+          },
+          warning: {
+            background: 'after:bg-dark-on-warning/25'
+          },
+          error: {
+            background: 'after:bg-dark-on-error/25'
+          }
+        }
+      },
+      solid: {
+        light: {
+          default: {
+            background: 'after:bg-light-surface/25'
+          },
+          primary: {
+            background: 'after:bg-light-primary/25'
+          },
+          secondary: {
+            background: 'after:bg-light-secondary/25'
+          },
+          success: {
+            background: 'after:bg-light-success/25'
+          },
+          warning: {
+            background: 'after:bg-light-warning/25'
+          },
+          error: {
+            background: 'after:bg-light-error/25'
+          }
+        },
+        dark: {
+          default: {
+            background: 'after:bg-dark-surface/25'
+          },
+          primary: {
+            background: 'after:bg-dark-primary/25'
+          },
+          secondary: {
+            background: 'after:bg-dark-secondary/25'
+          },
+          success: {
+            background: 'after:bg-dark-success/25'
+          },
+          warning: {
+            background: 'after:bg-dark-warning/25'
+          },
+          error: {
+            background: 'after:bg-dark-error/25'
+          }
+        }
+      }
+    },
+    disabled: {
+      plain: {
+        light: {
+          color: 'text-light-on-surface/40'
+        },
+        dark: {
+          color: 'text-dark-on-surface/40'
+        }
+      },
+      text: {
+        light: {
+          color: 'text-light-on-surface/40'
+        },
+        dark: {
+          color: 'text-dark-on-surface/40'
+        }
+      },
+      soft: {
+        light: {
+          color: 'text-light-on-surface/40',
+          background: 'bg-light-on-surface/20'
+        },
+        dark: {
+          color: 'text-dark-on-surface/40',
+          background: 'bg-dark-on-surface/20'
+        }
+      },
+      solid: {
+        light: {
+          color: 'text-light-on-surface/40',
+          background: 'bg-light-on-surface/20'
+        },
+        dark: {
+          color: 'text-dark-on-surface/40',
+          background: 'bg-dark-on-surface/20'
         }
       },
       outlined: {
         light: {
-          border: 'border border-light-divider',
-          background: 'bg-light-surface-low'
+          border: 'border-light-on-surface/40',
+          color: 'text-light-on-surface/40'
+        },
+        dark: {
+          border: 'border-dark-on-surface/40',
+          color: 'text-dark-on-surface/40'
         }
       }
     }
