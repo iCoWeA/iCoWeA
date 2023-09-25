3d65cde1b4b1b51123283e2145f3baa89729551e
Richard Wagner <richard.wagner@gmail.com>
2023-09-25 22:38:47 +0200

    fix

---
+++ src/pages/Button/TextVariantExample.tsx
@@ -0,0 +1,50 @@
+import React, { type FC } from 'react';
+import ExampleSection from '../../components/ExampleSection';
+import Button from '../../lib/simpleComponents/components/UI/Button';
+
+const TextVariantExample: FC = () => (
+  <ExampleSection
+    title="Text variant"
+    titleId="text"
+    row
+  >
+    <Button
+      variant="text"
+      color="default"
+    >
+      Button
+    </Button>
+    <Button
+      variant="text"
+      color="primary"
+    >
+      Button
+    </Button>
+    <Button
+      variant="text"
+      color="secondary"
+    >
+      Button
+    </Button>
+    <Button
+      variant="text"
+      color="success"
+    >
+      Button
+    </Button>
+    <Button
+      variant="text"
+      color="warning"
+    >
+      Button
+    </Button>
+    <Button
+      variant="text"
+      color="error"
+    >
+      Button
+    </Button>
+  </ExampleSection>
+);
+
+export default TextVariantExample;
