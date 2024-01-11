f04212763c4f3b509ffd5b433444388773798ec2
Richard Wagner <richard.wagner@gmail.com>
2024-01-11 12:51:30 +0100

    fix

--- src/pages/DefaultLayout/index.tsx
+++ src/pages/DefaultLayout/index.tsx
@@ -5,13 +5,16 @@ import Layout from '../../lib/iCoWeaUI/components/layouts/Layout/Layout';
 import Main from '../../lib/iCoWeaUI/components/layouts/Main/Main';
 
 export const Component: FC = () => (
-  <Layout layout="default">
-    <Main
-      variant="solid"
-      color="primary"
-    >
-      <Outlet />
-    </Main>
+  <Layout
+    layout="default"
+    variant="solid"
+    color="primary"
+  >
+    <Layout layout="fullbleed">
+      <Main>
+        <Outlet />
+      </Main>
+    </Layout>
   </Layout>
 );
 
