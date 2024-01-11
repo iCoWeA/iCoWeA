70b2bb5c5160c88846ae7fef9b053de562ec7ac2
Richard Wagner <richard.wagner@gmail.com>
2024-01-11 10:27:17 +0100

    fix: defaultLayout

--- src/pages/DefaultLayout/index.tsx
+++ src/pages/DefaultLayout/index.tsx
@@ -2,10 +2,13 @@ import React, { type FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Layout from '../../lib/iCoWeaUI/components/layouts/Layout/Layout';
+import Main from '../../lib/iCoWeaUI/components/layouts/Main/Main';
 
 const DefaultLayout: FC = () => (
-  <Layout layout='default'>
-    <Outlet />
+  <Layout layout="default">
+    <Main>
+      <Outlet />
+    </Main>
   </Layout>
 );
 
