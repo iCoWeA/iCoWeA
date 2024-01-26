89c52c53298acab31f70717c8b2e52acb3fe61d9
Richard Wagner <richard.wagner@gmail.com>
2024-01-26 07:58:04 +0100

    fix

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,14 @@
 import React, { type FC } from 'react';
 import { Form, redirect } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase';
 
 import PasswordInput from '../../components/PasswordInput/PasswordInput';
 import { EMAIL_PATTERN } from '../../data/constants';
-import { auth } from '../../firebase';
 import Button from '../../lib/iCoWeaUI/components/inputs/Button/Button';
 import FormControl from '../../lib/iCoWeaUI/components/inputs/FormControl/FormControl';
 import Input from '../../lib/iCoWeaUI/components/inputs/Input/Input';
 import Section from '../../lib/iCoWeaUI/components/layouts/Section/Section';
-import Stack from '../../lib/iCoWeaUI/components/layouts/Stack/Stack';
 import Card from '../../lib/iCoWeaUI/components/surfaces/Card/Card';
 import useForm from '../../lib/iCoWeaUI/hooks/useForm';
 
@@ -22,61 +21,58 @@ export const Component: FC = () => {
   } = useForm({ email: '', password: '' });
 
   return (
-    <Section align="center">
-      <Card
-        simple
-        block
-        className="lg:max-w-2/4"
+    <Section className="max-w-120">
+      <Form
+        method="post"
+        onSubmit={resetForm}
       >
-        <Form
-          method="post"
-          onSubmit={resetForm}
+        <Card
+          spacing="md"
+          gap="md"
         >
-          <Stack gap="md">
-            <FormControl color="error">
-              <Input
-                block
-                label="Email"
-                invalid={inputs.email.error}
-                onChange={(event) => {
-                  change(event, 1000);
-                }}
-                onBlur={blur}
-                id="email"
-                name="email"
-                value={inputs.email.value}
-                type="email"
-                pattern={EMAIL_PATTERN}
-                required
-              />
-              {inputs.email.error && 'Invalid email'}
-            </FormControl>
-            <FormControl color="error">
-              <PasswordInput
-                block
-                label="Password"
-                invalid={inputs.password.error}
-                onChange={(event) => {
-                  change(event, 1000);
-                }}
-                onBlur={blur}
-                id="password"
-                name="password"
-                value={inputs.password.value}
-                required
-              />
-              {inputs.password.error && 'Invalid password'}
-            </FormControl>
-            <Button
+          <FormControl color="error">
+            <Input
               block
-              disabled={!isFormValid}
-              type="submit"
-            >
-              LOGIN
-            </Button>
-          </Stack>
-        </Form>
-      </Card>
+              label="Email"
+              invalid={inputs.email.error}
+              onChange={(event) => {
+                change(event, 1000);
+              }}
+              onBlur={blur}
+              id="email"
+              name="email"
+              value={inputs.email.value}
+              type="email"
+              pattern={EMAIL_PATTERN}
+              required
+            />
+            {inputs.email.error && 'Invalid email'}
+          </FormControl>
+          <FormControl color="error">
+            <PasswordInput
+              block
+              label="Password"
+              invalid={inputs.password.error}
+              onChange={(event) => {
+                change(event, 1000);
+              }}
+              onBlur={blur}
+              id="password"
+              name="password"
+              value={inputs.password.value}
+              required
+            />
+            {inputs.password.error && 'Invalid password'}
+          </FormControl>
+          <Button
+            block
+            disabled={!isFormValid}
+            type="submit"
+          >
+            LOGIN
+          </Button>
+        </Card>
+      </Form>
     </Section>
   );
 };
