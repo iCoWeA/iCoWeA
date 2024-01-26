41d2715e97a22bc421d36bea684889c459bd8cab
Richard Wagner <richard.wagner@gmail.com>
2024-01-26 16:29:18 +0100

    f

--- src/pages/DefaultLayout/index.tsx
+++ src/pages/DefaultLayout/index.tsx
@@ -1,5 +1,6 @@
 import React, { type FC } from 'react';
 import { Outlet } from 'react-router-dom';
+
 import Layout from '../../lib/iCoWeaUI/components/layouts/Layout/Layout';
 import Main from '../../lib/iCoWeaUI/components/layouts/Main/Main';
 
