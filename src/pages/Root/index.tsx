c83a1f5500d820467c7ea9ff3d50016dedfaef77
Richard Wagner <richard.wagner@gmail.com>
2024-01-26 16:27:18 +0100

    fix

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,14 +1,10 @@
 import React, { type FC } from 'react';
-
 import { ScrollRestoration, Outlet } from 'react-router-dom';
-import Layout from '../../lib/iCoWeaUI/components/layouts/Layout/Layout';
 
 const Component: FC = () => (
   <>
     <ScrollRestoration />
-    <Layout layout="root">
-      <Outlet />
-    </Layout>
+    <Outlet />
   </>
 );
 
