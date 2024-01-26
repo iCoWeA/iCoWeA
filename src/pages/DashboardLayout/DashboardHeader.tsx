f2aa3d019263b2d2f56c3e6f10bce34e511845b5
Richard Wagner <richard.wagner@gmail.com>
2024-01-26 18:59:49 +0100

    fix

--- src/pages/DashboardLayout/DashboardHeader.tsx
+++ src/pages/DashboardLayout/DashboardHeader.tsx
@@ -4,33 +4,26 @@ import Avatar from '../../lib/iCoWeaUI/components/data-display/Avatar/Avatar';
 import Text from '../../lib/iCoWeaUI/components/data-display/Text/Text';
 import Flex from '../../lib/iCoWeaUI/components/layouts/Flex/Flex';
 import Header from '../../lib/iCoWeaUI/components/layouts/Header/Header';
-import Layout from '../../lib/iCoWeaUI/components/layouts/Layout/Layout';
 import Stack from '../../lib/iCoWeaUI/components/layouts/Stack/Stack';
 
 const DashboardHeader: FC = () => (
-  <Header>
-    <Layout
-      layout="dashboard"
-      variant="plain"
-      justify="end"
-    >
-      <Flex gap="base">
-        <Stack>
-          <Text>Richard Wagner</Text>
-          <Text
-            size="sm"
-            align="right"
-          >
-            Admin
-          </Text>
-        </Stack>
-        <Avatar
-          color="neutral"
-          src={require('../../assets/images/photo.png')}
-          alt="RW"
-        />
-      </Flex>
-    </Layout>
+  <Header justify="end">
+    <Flex gap="base">
+      <Stack>
+        <Text>Richard Wagner</Text>
+        <Text
+          size="sm"
+          align="right"
+        >
+          Admin
+        </Text>
+      </Stack>
+      <Avatar
+        color="neutral"
+        src={require('../../assets/images/photo.png')}
+        alt="RW"
+      />
+    </Flex>
   </Header>
 );
 
