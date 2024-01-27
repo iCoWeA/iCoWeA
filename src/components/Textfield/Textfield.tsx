7110d3076d19df7fd7b1845f4a6f5c99b189c608
Richard Wagner <richard.wagner@gmail.com>
2024-01-27 16:22:56 +0100

    fix

--- src/components/Textfield/Textfield.tsx
+++ src/components/Textfield/Textfield.tsx
@@ -4,16 +4,21 @@ import FormControl, {
   type FormControlProps
 } from '../../lib/iCoWeaUI/components/inputs/FormControl/FormControl';
 import Input, { type InputProps } from '../../lib/iCoWeaUI/components/inputs/Input/Input';
+import Label, { type LabelProps } from '../../lib/iCoWeaUI/components/inputs/Label/Label';
 
 type TextfieldProps = InputProps & {
   indexId?: string;
+  labelVariant?: 'floating' | 'text' | 'none';
   containerProps?: FormControlProps;
+  labelProps?: LabelProps;
 };
 
 const Textfield: FC<TextfieldProps> = ({
   indexId,
+  labelVariant = 'floating',
   invalid,
   containerProps,
+  labelProps,
   children,
   ...restProps
 }) => (
@@ -22,15 +27,27 @@ const Textfield: FC<TextfieldProps> = ({
     {...containerProps}
   >
     {children ?? (
-      <Input
-        block
-        label={indexId && `${indexId[0].toUpperCase() + indexId.slice(1)}`}
-        id={indexId}
-        name={indexId}
-        {...restProps}
-      />
+      <>
+        {labelVariant === 'text' && indexId && (
+          <Label
+            htmlFor={indexId}
+            {...labelProps}
+          >{`${indexId[0].toUpperCase() + indexId.slice(1)}`}</Label>
+        )}
+        <Input
+          block
+          label={
+            labelVariant === 'floating' &&
+            indexId &&
+            `${indexId[0].toUpperCase() + indexId.slice(1)}`
+          }
+          id={indexId}
+          name={indexId}
+          {...restProps}
+        />
+        {invalid && indexId && `Invalid ${indexId}`}
+      </>
     )}
-    {invalid && indexId && `Invalid ${indexId}`}
   </FormControl>
 );
 
