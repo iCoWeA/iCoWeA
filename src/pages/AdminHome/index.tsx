e3813709c0b69786422954c9a5abfd2b8d773f2c
Richard Wagner <richard.wagner@gmail.com>
2024-01-27 16:44:46 +0100

    fix

--- src/pages/AdminHome/index.tsx
+++ src/pages/AdminHome/index.tsx
@@ -1,19 +1,22 @@
 import React, { type FC } from 'react';
 
 import Grid from '../../lib/iCoWeaUI/components/layouts/Grid/Grid';
+import Section from '../../lib/iCoWeaUI/components/layouts/Section/Section';
 import LikesCard from './LikesCard';
 import MessagesCard from './MessagesCard';
 import UserCard from './UserCard';
 
 export const Component: FC = () => (
-  <Grid
-    gap="lg"
-    className="grid-cols-2 max-md:grid-cols-1"
-  >
-    <UserCard />
-    <LikesCard />
-    <MessagesCard />
-  </Grid>
+  <Section>
+    <Grid
+      gap="lg"
+      className="grid-cols-2 max-md:grid-cols-1"
+    >
+      <UserCard />
+      <LikesCard />
+      <MessagesCard />
+    </Grid>
+  </Section>
 );
 
 Component.displayName = 'AdminHomeRoute';
