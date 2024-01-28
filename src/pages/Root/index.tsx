a5eda2da5c3f2803a77a04307ea98f3e2c6cc0db
Richard Wagner <richard.wagner@gmail.com>
2024-01-28 15:13:06 +0100

    fix

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,12 +1,46 @@
-import React, { type FC } from 'react';
+import React, { type FC, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { ScrollRestoration, Outlet } from 'react-router-dom';
+import { ref, onValue } from 'firebase/database';
+import { database } from '../../firebase';
 
-const Component: FC = () => (
-  <>
-    <ScrollRestoration />
-    <Outlet />
-  </>
-);
+import ErrorScreen from '../../components/ErrorScreen/ErrorScreen';
+import LoadingScreen from '../../components/LoadingScreen/LoadingScreen';
+import { selectUser, userActions } from '../../store/slices/user';
+
+const Component: FC = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  /* --- Set event handlers --- */
+  useEffect(() => {
+    const userRef = ref(database, 'users/0');
+
+    return onValue(userRef, (snap) => {
+      if (snap.exists()) {
+        dispatch(userActions.setUser(snap.val()));
+
+        return;
+      }
+
+      dispatch(userActions.setError());
+    });
+  }, []);
+
+  return (
+    <>
+      <ScrollRestoration />
+      {!user && <LoadingScreen />}
+      {user && Object.keys(user).length === 0
+        ? (
+        <ErrorScreen>{'User not found'}</ErrorScreen>
+          )
+        : (
+        <Outlet />
+          )}
+    </>
+  );
+};
 
 export default Component;
 
