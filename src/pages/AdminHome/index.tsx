c5172e942996f022cd197f97e08e8ee4e2196d37
Richard Wagner <richard.wagner@gmail.com>
2024-01-28 09:46:46 +0100

    fix

--- src/pages/AdminHome/index.tsx
+++ src/pages/AdminHome/index.tsx
@@ -1,21 +1,16 @@
 import React, { type FC } from 'react';
 
-import Grid from '../../lib/iCoWeaUI/components/layouts/Grid/Grid';
 import Section from '../../lib/iCoWeaUI/components/layouts/Section/Section';
-import LikesCard from './LikesCard';
+import Stack from '../../lib/iCoWeaUI/components/layouts/Stack/Stack';
 import MessagesCard from './MessagesCard';
 import UserCard from './UserCard';
 
 export const Component: FC = () => (
   <Section>
-    <Grid
-      gap="lg"
-      className="grid-cols-2 max-md:grid-cols-1"
-    >
+    <Stack gap="md">
       <UserCard />
-      <LikesCard />
       <MessagesCard />
-    </Grid>
+    </Stack>
   </Section>
 );
 
