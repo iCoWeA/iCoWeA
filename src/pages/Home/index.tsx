7ca1ed36347b34dd0c437c767176369ee44c6e91
Richard Wagner <richard.wagner@gmail.com>
2024-01-28 15:28:28 +0100

    fix

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React, { type FC } from 'react';
+
+import Section from '../../lib/iCoWeaUI/components/layouts/Section/Section';
+
+export const Component: FC = () => (
+  <Section>
+    Hello
+  </Section>
+);
+
+Component.displayName = 'HomeRoute';
