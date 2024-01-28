b0433d97135176f398fb3d148fecb541ea5cf095
Richard Wagner <richard.wagner@gmail.com>
2024-01-28 10:46:46 +0100

    fix

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,6 +2,6 @@ export const NAME_PATTERN = '^[A-Za-z]{1,32}$';
 
 export const EMAIL_PATTERN = '^[\\w\\.]+@[\\w\\.]+$';
 
-export const MOBILE_PATTERN = '^\\d+$';
+export const NUMBER_PATTERN = '^\\d+$';
 
 export const EXPIRATION_DURATION = 30;
