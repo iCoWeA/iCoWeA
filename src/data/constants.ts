b6822da04b83d851bd203910c485371520b6e4a2
Richard Wagner <richard.wagner@gmail.com>
2024-01-29 16:05:05 +0100

    fix

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,7 +1,3 @@
 export const NAME_PATTERN = '^[A-Za-z]{1,32}$';
 
-export const EMAIL_PATTERN = '^[\\w\\.]+@[\\w\\.]+$';
-
-export const NUMBER_PATTERN = '^\\d+$';
-
 export const EXPIRATION_DURATION = 30;
---
+++ src/lib/iCoWeaUI/data/constants.ts
@@ -0,0 +1,3 @@
+export const EMAIL_PATTERN = '^[\\w\\.]+@[\\w\\.]+$';
+
+export const NUMBER_PATTERN = '^\\d+$';
