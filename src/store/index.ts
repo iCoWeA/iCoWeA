f3dd652e2b430cfbecd7f258f378aa185f018abd
Richard Wagner <richard.wagner@gmail.com>
2024-01-29 15:49:59 +0100

    fixfix

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import breakpoint from './slices/breakpoint';
 import navbar from './slices/navbar';
 import user from './slices/user';
 
 const store = configureStore({
   reducer: {
+    breakpoint: breakpoint.reducer,
     user: user.reducer,
     navbar: navbar.reducer
   }
