d29bc9984bc28d7392164158d6a05e3c2a1580cb
Richard Wagner <richard.wagner@gmail.com>
2024-01-29 16:10:48 +0100

    fix

--- src/store/Slices/user.ts
+++ src/store/Slices/user.ts
@@ -1,32 +1,12 @@
-import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import { type PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 import { type StoreState } from '..';
 
-type User = {
-  id: string,
-  firstname: string,
-  lastname: string,
-  imageURL: string,
-  email: string,
-  phone: string,
-  sex: 'man' | 'woman',
-  dob: string,
-  about: string,
-  street: string,
-  postalCode: string,
-  city: string,
-  country: string,
-  github?: string,
-  linkedin?: string,
-  instagram?: string,
-  facebook?: string
-};
-
-type State = User | Record<string, string> | null;
+type State = User | Obj | null;
 
 type Reducers = {
-  setError: () => Record<string, string>,
-  setUser: (prevState: State, action: PayloadAction<User>) => User
+  setError: () => Obj,
+  setUser: (_: State, action: PayloadAction<User>) => User
 };
 
 const user = createSlice<State, Reducers>({
@@ -34,50 +14,12 @@ const user = createSlice<State, Reducers>({
   initialState: null,
   reducers: {
     setError: () => ({}),
-    setUser: (prevState, {
-      payload: {
-        id,
-        firstname,
-        lastname,
-        imageURL,
-        email,
-        phone,
-        sex,
-        dob,
-        about,
-        street,
-        postalCode,
-        city,
-        country,
-        github,
-        linkedin,
-        instagram,
-        facebook
-      }
-    }) => ({
-      id,
-      firstname,
-      lastname,
-      imageURL,
-      email,
-      phone,
-      sex,
-      dob,
-      about,
-      street,
-      postalCode,
-      city,
-      country,
-      github,
-      linkedin,
-      instagram,
-      facebook
-    })
+    setUser: (_, action) => action.payload
   }
 });
-
-export default user;
 
 export const selectUser = ({ user }: StoreState): State => user;
 
 export const userActions = user.actions;
+
+export default user;
