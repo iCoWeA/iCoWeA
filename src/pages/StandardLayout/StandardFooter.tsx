f35332726afcb35f050178a1aca92ba948dcd365
Richard Wagner <richard.wagner@gmail.com>
2024-01-30 19:45:22 +0100

    fix

--- src/pages/StandardLayout/StandardFooter.tsx
+++ src/pages/StandardLayout/StandardFooter.tsx
@@ -2,11 +2,14 @@ import React, { type FC } from 'react';
 
 import Text from '../../lib/iCoWeABaseUI/components/data-display/Text/Text';
 import Footer from '../../lib/iCoWeABaseUI/components/layouts/Footer/Footer';
+import Link from '../../lib/iCoWeARouterUI/components/Link/Link';
 
 const StandardFooter: FC = () => (
   <Footer containerProps={{ className: 'max-lg:flex-col' }}>
     <Text align="center">Copyright © 2024 Richard Wagner. All Rights Reserved.</Text>
-    <Text align="center">Designed by Harnish Design</Text>
+    <Text align="center">
+      Designed by <Link to="/">Richard Wagner</Link>
+    </Text>
   </Footer>
 );
 
