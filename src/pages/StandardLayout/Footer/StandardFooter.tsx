e4933f03fd0e763476e5ddad5db65e562eb02919
Richard Wagner <richard.wagner@gmail.com>
2024-02-12 10:46:21 +0100

    fix: layout

--- src/pages/StandardLayout/Footer/StandardFooter.tsx
+++ src/pages/StandardLayout/Footer/StandardFooter.tsx
@@ -6,8 +6,8 @@ import Link from '../../../lib/iCoWeARouterUI/components/Link/Link';
 
 const StandardFooter: FC = () => (
   <Footer containerProps={{ className: 'max-lg:flex-col' }}>
-    <Text align="center">Copyright © 2024 Richard Wagner. All Rights Reserved.</Text>
-    <Text align="center">
+    <Text>Copyright © 2024 Richard Wagner. All Rights Reserved.</Text>
+    <Text>
       Designed by <Link to="/">Richard Wagner</Link>
     </Text>
   </Footer>
