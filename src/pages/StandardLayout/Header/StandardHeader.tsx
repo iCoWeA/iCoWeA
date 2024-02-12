8be109e6da7b339ad2ee90abfdecb5717e25e160
Richard Wagner <richard.wagner@gmail.com>
2024-02-12 10:55:06 +0100

    fix: header

--- src/pages/StandardLayout/Header/StandardHeader.tsx
+++ src/pages/StandardLayout/Header/StandardHeader.tsx
@@ -1,7 +1,7 @@
 import React, { type FC } from 'react';
 import { useSelector } from 'react-redux';
-
 import Logo from '../../../components/Icons/Logo';
+
 import ListItem from '../../../lib/iCoWeABaseUI/components/data-display/ListItem/ListItem';
 import Header from '../../../lib/iCoWeABaseUI/components/layouts/Header/Header';
 import Navigation from '../../../lib/iCoWeABaseUI/components/layouts/Navigation/Navigation';
@@ -18,37 +18,33 @@ const StandardHeader: FC = () => {
       {(breakpoint === Breakpoints.SM || breakpoint === Breakpoints.MD) && <MobileNavigation />}
       {breakpoint !== Breakpoints.SM && breakpoint !== Breakpoints.MD && (
         <Navigation>
-          <ListItem spacing="none">
+          <ListItem>
             <Navlink
               to="/"
-              size="lg"
               activeVariant="solid"
             >
               Home
             </Navlink>
           </ListItem>
-          <ListItem spacing="none">
+          <ListItem>
             <Navlink
               to="projects"
-              size="lg"
               activeVariant="solid"
             >
               Projects
             </Navlink>
           </ListItem>
-          <ListItem spacing="none">
+          <ListItem>
             <Navlink
               to="about"
-              size="lg"
               activeVariant="solid"
             >
               About
             </Navlink>
           </ListItem>
-          <ListItem spacing="none">
+          <ListItem>
             <Navlink
               to="contact"
-              size="lg"
               activeVariant="solid"
             >
               Contact
