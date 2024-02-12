cffd8164944d3ee658aee77664c0937d0b9c3587
Richard Wagner <richard.wagner@gmail.com>
2024-02-12 13:04:31 +0100

    fix: textfield

--- src/components/Textfield/Textfield.tsx
+++ src/components/Textfield/Textfield.tsx
@@ -1,54 +1,36 @@
-import React, { type FC } from 'react';
+import React, { type ReactNode, type FC } from 'react';
 
 import FormControl, {
   type FormControlProps
 } from '../../lib/iCoWeABaseUI/components/inputs/FormControl/FormControl';
-import Input, { type InputProps } from '../../lib/iCoWeABaseUI/components/inputs/Input/Input';
 import Label, { type LabelProps } from '../../lib/iCoWeABaseUI/components/inputs/Label/Label';
 import { upperCaseFirstLetter } from '../../lib/iCoWeAUtilsUI/utils/utils';
 
-type TextfieldProps = InputProps & {
-  labelVariant?: 'floating' | 'text' | 'none';
+type TextfieldProps = FormControlProps & {
   indexId?: string;
-  containerProps?: FormControlProps;
+  errorText?: ReactNode;
   labelProps?: LabelProps;
 };
 
 const Textfield: FC<TextfieldProps> = ({
-  labelVariant = 'floating',
-  invalid,
   indexId,
-  containerProps,
+  errorText,
   labelProps,
   children,
   ...restProps
 }) => (
   <FormControl
     color="error"
-    {...containerProps}
+    {...restProps}
   >
-    {children ?? (
-      <>
-        {labelVariant === 'text' && indexId && (
-          <Label
-            htmlFor={indexId}
-            {...labelProps}
-          >{`${upperCaseFirstLetter(indexId).replace('-', ' ')}`}</Label>
-        )}
-        <Input
-          block
-          label={
-            labelVariant === 'floating' &&
-            indexId &&
-            `${upperCaseFirstLetter(indexId).replace('-', ' ')}`
-          }
-          id={indexId}
-          name={indexId}
-          {...restProps}
-        />
-        {invalid && indexId && `Invalid ${indexId.replace('-', ' ')}`}
-      </>
+    {indexId && (
+      <Label
+        htmlFor={indexId}
+        {...labelProps}
+      >{`${upperCaseFirstLetter(indexId).replace('-', ' ')}`}</Label>
     )}
+    {children}
+    {errorText}
   </FormControl>
 );
 
