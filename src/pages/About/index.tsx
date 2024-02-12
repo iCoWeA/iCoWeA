159532472e9cc96d15ff41e1749fe872fbec1423
Richard Wagner <richard.wagner@gmail.com>
2024-02-12 22:43:00 +0100

    fix: layout

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -5,6 +5,8 @@ import MovableAvatar from '../../components/MovableAvatar/MovableAvatar';
 import Text from '../../lib/iCoWeABaseUI/components/data-display/Text/Text';
 import Title from '../../lib/iCoWeABaseUI/components/data-display/Title/Title';
 import Flex from '../../lib/iCoWeABaseUI/components/layouts/Flex/Flex';
+import Layout from '../../lib/iCoWeABaseUI/components/layouts/Layout/Layout';
+import Main from '../../lib/iCoWeABaseUI/components/layouts/Main/Main';
 import Section from '../../lib/iCoWeABaseUI/components/layouts/Section/Section';
 import Stack from '../../lib/iCoWeABaseUI/components/layouts/Stack/Stack';
 import LinkButton from '../../lib/iCoWeARouterUI/components/LinkButton/LinkButton';
@@ -14,61 +16,64 @@ export const Component: FC = () => {
   const user = useSelector(selectUser);
 
   return (
-    <Section className="animate-slide">
-      <Flex
-        wrap="nowrap"
-        justify="center"
-        gap="lg"
-        className="max-lg:flex-col"
-      >
-        <MovableAvatar
-          distance={10}
-          border
-          className="w-full max-w-[25rem] border-8"
-          src={require('../../assets/images/photo.png')}
-        />
-        <Stack gap="sm">
-          <Title
-            size="5"
-            color="inherit"
+    <Layout layout="default">
+      <Main>
+        <Section className="animate-slide">
+          <Flex
+            wrap="nowrap"
+            justify="center"
+            className="gap-16 max-lg:flex-col"
           >
-            Software developer
-          </Title>
-          <Title
-            size="1"
-            gutter
-            color="secondary"
-          >
-            {(user?.firstname ?? '') + ' ' + (user?.lastname ?? '')}
-          </Title>
-          <Text
-            size="lg"
-            gutter
-            color="inherit"
-          >
-            {user?.about}
-          </Text>
-          <Flex gap="sm">
-            <LinkButton
-              to="projects"
-              size="lg"
-              variant="solid"
-              color="secondary"
-            >
-              View my works
-            </LinkButton>
-            <LinkButton
-              to="contact"
-              size="lg"
-              variant="default"
-              color="secondary"
-            >
-              Get in touch
-            </LinkButton>
+            <MovableAvatar
+              distance={10}
+              border
+              className="w-full max-w-[25rem] border-8"
+              src={require('../../assets/images/photo.png')}
+            />
+            <Stack gap="md">
+              <Title
+                size="5"
+                color="inherit"
+              >
+                Software developer
+              </Title>
+              <Title
+                size="1"
+                gutter
+                color="secondary"
+              >
+                {(user?.firstname ?? '') + ' ' + (user?.lastname ?? '')}
+              </Title>
+              <Text
+                size="lg"
+                gutter
+                color="inherit"
+              >
+                {user?.about}
+              </Text>
+              <Flex gap="sm">
+                <LinkButton
+                  to="projects"
+                  size="lg"
+                  variant="solid"
+                  color="secondary"
+                >
+                  View my works
+                </LinkButton>
+                <LinkButton
+                  to="contact"
+                  size="lg"
+                  variant="default"
+                  color="secondary"
+                >
+                  Get in touch
+                </LinkButton>
+              </Flex>
+            </Stack>
           </Flex>
-        </Stack>
-      </Flex>
-    </Section>
+        </Section>
+      </Main>
+    </Layout>
   );
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,8 @@ import { useSelector } from 'react-redux';
 import MovableAvatar from '../../components/MovableAvatar/MovableAvatar';
 import Title from '../../lib/iCoWeABaseUI/components/data-display/Title/Title';
 import Flex from '../../lib/iCoWeABaseUI/components/layouts/Flex/Flex';
+import Layout from '../../lib/iCoWeABaseUI/components/layouts/Layout/Layout';
+import Main from '../../lib/iCoWeABaseUI/components/layouts/Main/Main';
 import Section from '../../lib/iCoWeABaseUI/components/layouts/Section/Section';
 import Stack from '../../lib/iCoWeABaseUI/components/layouts/Stack/Stack';
 import LinkButton from '../../lib/iCoWeARouterUI/components/LinkButton/LinkButton';
@@ -13,66 +15,69 @@ export const Component: FC = () => {
   const user = useSelector(selectUser);
 
   return (
-    <Section className='animate-slide'>
-      <Flex
-        wrap="nowrap"
-        justify="center"
-        gap="lg"
-        className="max-md:flex-col"
-      >
-        <Stack gap="sm">
-          <Title
-            size="3"
-            color="inherit"
+    <Layout layout="default">
+      <Main>
+        <Section className="animate-slide">
+          <Flex
+            wrap="nowrap"
+            justify="center"
+            className="gap-16 max-md:flex-col"
           >
-            Hi, I&apos;m
-          </Title>
-          <Title
-            size="1"
-            color="secondary"
-          >
-            {(user?.firstname ?? '') + ' ' + (user?.lastname ?? '')}
-          </Title>
-          <Title
-            size="4"
-            color="inherit"
-          >
-            SOFTWARE DEVELOPER
-          </Title>
-          <Title
-            size="5"
-            color="inherit"
-            gutter
-          >
-            based in Košice, Slovakia.
-          </Title>
-          <Flex gap="sm">
-            <LinkButton
-              to="projects"
-              size="lg"
-              variant="solid"
-              color="secondary"
-            >
-              View my works
-            </LinkButton>
-            <LinkButton
-              to="contact"
-              size="lg"
-              variant="default"
-              color="secondary"
-            >
-              Get in touch
-            </LinkButton>
+            <Stack gap="md">
+              <Title
+                size="3"
+                color="inherit"
+              >
+                Hi, I&apos;m
+              </Title>
+              <Title
+                size="1"
+                color="secondary"
+              >
+                {(user?.firstname ?? '') + ' ' + (user?.lastname ?? '')}
+              </Title>
+              <Title
+                size="4"
+                color="inherit"
+              >
+                SOFTWARE DEVELOPER
+              </Title>
+              <Title
+                size="5"
+                color="inherit"
+                gutter
+              >
+                based in Košice, Slovakia.
+              </Title>
+              <Flex gap="sm">
+                <LinkButton
+                  to="projects"
+                  size="lg"
+                  variant="solid"
+                  color="secondary"
+                >
+                  View my works
+                </LinkButton>
+                <LinkButton
+                  to="contact"
+                  size="lg"
+                  variant="default"
+                  color="secondary"
+                >
+                  Get in touch
+                </LinkButton>
+              </Flex>
+            </Stack>
+            <MovableAvatar
+              distance={10}
+              border
+              className="w-full max-w-[25rem] border-8"
+              src={require('../../assets/images/photo.png')}
+            />
           </Flex>
-        </Stack>
-        <MovableAvatar
-          distance={10}
-          border
-          className="w-full max-w-[25rem] border-8"
-          src={require('../../assets/images/photo.png')}
-        />
-      </Flex>
-    </Section>
+        </Section>
+      </Main>
+    </Layout>
   );
 };
 
