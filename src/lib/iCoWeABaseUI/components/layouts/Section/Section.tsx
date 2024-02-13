203db45b6c404884266e84d7fb3ccb9b864b60ca
Richard Wagner <richard.wagner@gmail.com>
2024-02-13 20:05:36 +0100

    fix: sidebar & section

--- src/lib/iCoWeABaseUI/components/layouts/Section/Section.tsx
+++ src/lib/iCoWeABaseUI/components/layouts/Section/Section.tsx
@@ -4,46 +4,25 @@
 
 import React, { type BaseHTMLAttributes, forwardRef, useMemo } from 'react';
 
-import useTheme from '../../../../iCoWeAUI/hooks/useTheme';
 import { mergeClasses } from '../../../../iCoWeAUI/utils/utils';
 import useConfig from '../../../hooks/useConfig';
-import { getBorderType, getBorderVariant } from '../../../utils/utils';
 import sectionConfig from './sectionConfig';
 
-export type SectionDefaultProps = {
-  variant?: Variants;
-  color?: Colors;
-  border?: Borders;
-};
-
-export type SectionProps = BaseHTMLAttributes<HTMLElement> & SectionDefaultProps;
+export type SectionProps = BaseHTMLAttributes<HTMLElement>;
 
 const Section = forwardRef<HTMLElement, SectionProps>((props, ref) => {
-  const { variant, color, border, defaultClassName, className, ...restProps } = useConfig(
+  const { defaultClassName, className, ...restProps } = useConfig(
     'section',
     sectionConfig.defaultProps,
     props
   );
 
-  const theme = useTheme();
-
   /* --- Set classes --- */
   const mergedClassName = useMemo(() => {
     const styles = sectionConfig.styles;
-    const borderType = getBorderType(border);
-    const borderVariant = getBorderVariant(variant);
 
-    return mergeClasses(
-      styles.base,
-      borderType !== 'none' && styles.borders[borderType],
-      color !== 'inherit' && styles.variants[variant][theme][color],
-      color !== 'inherit' &&
-        borderType !== 'none' &&
-        styles.borderVariants[borderVariant][theme][color],
-      defaultClassName,
-      className
-    );
-  }, [border, variant, theme, color, defaultClassName, className]);
+    return mergeClasses(styles.base, defaultClassName, className);
+  }, [defaultClassName, className]);
 
   return (
     <section
--- src/lib/iCoWeABaseUI/components/layouts/Sidebar/Sidebar.tsx
+++ src/lib/iCoWeABaseUI/components/layouts/Sidebar/Sidebar.tsx
@@ -4,45 +4,25 @@
 
 import React, { type BaseHTMLAttributes, forwardRef, useMemo } from 'react';
 
-import useTheme from '../../../../iCoWeAUI/hooks/useTheme';
 import { mergeClasses } from '../../../../iCoWeAUI/utils/utils';
 import useConfig from '../../../hooks/useConfig';
-import { getBorderType, getBorderVariant } from '../../../utils/utils';
 import sidebarConfig from './sidebarConfig';
 
-export type SidebarDefaultProps = {
-  placement?: SidePlacements;
-  variant?: Variants;
-  color?: Colors;
-  border?: Borders;
-};
-
-export type SidebarProps = BaseHTMLAttributes<HTMLElement> & SidebarDefaultProps;
+export type SidebarProps = BaseHTMLAttributes<HTMLElement>;
 
 const Sidebar = forwardRef<HTMLElement, SidebarProps>((props, ref) => {
-  const { placement, variant, color, border, defaultClassName, className, ...restProps } =
-    useConfig('sidebar', sidebarConfig.defaultProps, props);
-
-  const theme = useTheme();
+  const { defaultClassName, className, ...restProps } = useConfig(
+    'sidebar',
+    sidebarConfig.defaultProps,
+    props
+  );
 
   /* --- Set classes --- */
   const mergedClassName = useMemo(() => {
     const styles = sidebarConfig.styles;
-    const borderType = getBorderType(border);
-    const borderVariant = getBorderVariant(variant);
 
-    return mergeClasses(
-      styles.base,
-      styles.placements[placement],
-      borderType !== 'none' && styles.borders[borderType],
-      color !== 'inherit' && styles.variants[variant][theme][color],
-      color !== 'inherit' &&
-        borderType !== 'none' &&
-        styles.borderVariants[borderVariant][theme][color],
-      defaultClassName,
-      className
-    );
-  }, [border, placement, variant, theme, color, defaultClassName, className]);
+    return mergeClasses(styles.base, defaultClassName, className);
+  }, [defaultClassName, className]);
 
   return (
     <aside
