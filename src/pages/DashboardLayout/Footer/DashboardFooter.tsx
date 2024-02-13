e709b32a4e5d2a65a7f17e814dc371a093a93a17
Richard Wagner <richard.wagner@gmail.com>
2024-02-13 21:33:25 +0100

    fix: dashboard footer

---
+++ src/pages/DashboardLayout/Footer/DashboardFooter.tsx
@@ -0,0 +1,19 @@
+import React, { type FC } from 'react';
+
+import Text from '../../../lib/iCoWeABaseUI/components/data-display/Text/Text';
+import Footer from '../../../lib/iCoWeABaseUI/components/layouts/Footer/Footer';
+import Link from '../../../lib/iCoWeARouterUI/components/Link/Link';
+
+const DashboardFooter: FC = () => (
+  <Footer
+    block
+    containerProps={{ className: 'max-lg:flex-col' }}
+  >
+    <Text align="center">Copyright © 2024 Richard Wagner. All Rights Reserved.</Text>
+    <Text align="center">
+      Designed by <Link to="/">Richard Wagner</Link>
+    </Text>
+  </Footer>
+);
+
+export default DashboardFooter;
