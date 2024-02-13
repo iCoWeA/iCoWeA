03eddf67de3d3944f80ed5543ec2f994ed98eaf5
Richard Wagner <richard.wagner@gmail.com>
2024-02-13 20:07:46 +0100

    fix: main

--- src/lib/iCoWeABaseUI/components/layouts/Main/Main.tsx
+++ src/lib/iCoWeABaseUI/components/layouts/Main/Main.tsx
@@ -4,48 +4,34 @@
 
 import React, { type BaseHTMLAttributes, forwardRef, useMemo } from 'react';
 
-import useTheme from '../../../../iCoWeAUI/hooks/useTheme';
 import { mergeClasses } from '../../../../iCoWeAUI/utils/utils';
 import useConfig from '../../../hooks/useConfig';
-import { getBorderType, getBorderVariant } from '../../../utils/utils';
 import mainConfig from './mainConfig';
 
 export type MainDefaultProps = {
-  block?: boolean;
-  variant?: Variants;
-  color?: Colors;
-  border?: Borders;
+  placement?: LayoutPlacements;
 };
 
 export type MainProps = BaseHTMLAttributes<HTMLElement> & MainDefaultProps;
 
 const Main = forwardRef<HTMLElement, MainProps>((props, ref) => {
-  const { block, variant, color, border, defaultClassName, className, ...restProps } = useConfig(
+  const { placement, defaultClassName, className, ...restProps } = useConfig(
     'main',
     mainConfig.defaultProps,
     props
   );
 
-  const theme = useTheme();
-
   /* --- Set classes --- */
   const mergedClassName = useMemo(() => {
     const styles = mainConfig.styles;
-    const borderType = getBorderType(border);
-    const borderVariant = getBorderVariant(variant);
 
     return mergeClasses(
       styles.base,
-      !block && styles.block,
-      borderType !== 'none' && styles.borders[borderType],
-      color !== 'inherit' && styles.variants[variant][theme][color],
-      color !== 'inherit' &&
-        borderType !== 'none' &&
-        styles.borderVariants[borderVariant][theme][color],
+      placement !== 'full' && styles.placements[placement],
       defaultClassName,
       className
     );
-  }, [border, block, variant, theme, color, defaultClassName, className]);
+  }, [placement, defaultClassName, className]);
 
   return (
     <main
