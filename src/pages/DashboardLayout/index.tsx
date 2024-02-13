f81a0d0d20c20f00b03b499841adc65e88eea9b3
Richard Wagner <richard.wagner@gmail.com>
2024-02-13 23:26:45 +0100

    fix: dashboardLayout

--- src/pages/DashboardLayout/index.tsx
+++ src/pages/DashboardLayout/index.tsx
@@ -1,9 +1,11 @@
 import React, { type FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import Flex from '../../lib/iCoWeABaseUI/components/layouts/Flex/Flex';
 import Layout from '../../lib/iCoWeABaseUI/components/layouts/Layout/Layout';
 import DashboardFooter from './Footer/DashboardFooter';
 import DashboardHeader from './Header/DashboardHeader';
+import DashboardSidebar from './Sidebar/DashboardSidebar';
 
 export const Component: FC = () => (
   <Layout
@@ -12,7 +14,14 @@ export const Component: FC = () => (
     color="neutral"
   >
     <DashboardHeader />
-    <Outlet />
+    <Flex
+      wrap="nowrap"
+      align="stretch"
+      className="grow"
+    >
+      <DashboardSidebar />
+      <Outlet />
+    </Flex>
     <DashboardFooter />
   </Layout>
 );
