4b821ccfbe3bb0ecef87ac8ab0f133085ee328f0
Richard Wagner <richard.wagner@gmail.com>
2024-02-13 17:11:01 +0100

    fix: default

--- src/pages/DefaultLayout/index.tsx
+++ src/pages/DefaultLayout/index.tsx
@@ -2,7 +2,6 @@ import React, { type FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Layout from '../../lib/iCoWeABaseUI/components/layouts/Layout/Layout';
-import Main from '../../lib/iCoWeABaseUI/components/layouts/Main/Main';
 
 export const Component: FC = () => (
   <Layout
@@ -10,11 +9,7 @@ export const Component: FC = () => (
     variant="solid"
     color="primary"
   >
-    <Layout layout="default">
-      <Main>
-        <Outlet />
-      </Main>
-    </Layout>
+    <Outlet />
   </Layout>
 );
 
