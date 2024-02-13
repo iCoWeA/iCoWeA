b3fa2713115843b1a9943f3569d37112b78351e0
Richard Wagner <richard.wagner@gmail.com>
2024-02-13 22:14:01 +0100

    fix: header

---
+++ src/pages/DashboardLayout/Header/DashboardHeader.tsx
@@ -0,0 +1,48 @@
+import React, { type FC } from 'react';
+import { useSelector } from 'react-redux';
+
+import Avatar from '../../../lib/iCoWeABaseUI/components/data-display/Avatar/Avatar';
+import Text from '../../../lib/iCoWeABaseUI/components/data-display/Text/Text';
+import Title from '../../../lib/iCoWeABaseUI/components/data-display/Title/Title';
+import Flex from '../../../lib/iCoWeABaseUI/components/layouts/Flex/Flex';
+import Header from '../../../lib/iCoWeABaseUI/components/layouts/Header/Header';
+import Stack from '../../../lib/iCoWeABaseUI/components/layouts/Stack/Stack';
+import { upperCaseFirstLetter } from '../../../lib/iCoWeAUtilsUI/utils/utils';
+import { selectBreakpoint, Breakpoints } from '../../../store/slices/breakpoint';
+import { selectUser } from '../../../store/slices/user';
+import MobileNavigation from './MobileNavigation';
+
+const DashboardHeader: FC = () => {
+  const breakpoint = useSelector(selectBreakpoint);
+  const user = useSelector(selectUser);
+
+  return (
+    <Header
+      block
+      shadow
+      justify={breakpoint === Breakpoints.SM ? 'between' : 'end'}
+    >
+      {breakpoint === Breakpoints.SM && <MobileNavigation />}
+      <Flex gap="base">
+        <Stack>
+          <Title>{`${upperCaseFirstLetter(user.firstname)} ${upperCaseFirstLetter(
+            user.lastname
+          )}`}</Title>
+          <Text
+            size="sm"
+            align="right"
+          >
+            Admin
+          </Text>
+        </Stack>
+        <Avatar
+          color="neutral"
+          alt={user.firstname[0].toLocaleUpperCase() + user.lastname[0].toLocaleUpperCase()}
+          src={require('../../../assets/images/photo.png')}
+        />
+      </Flex>
+    </Header>
+  );
+};
+
+export default DashboardHeader;
