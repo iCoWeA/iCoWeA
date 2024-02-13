c794625e754be67ad8c008f7fe298fba97498e47
Richard Wagner <richard.wagner@gmail.com>
2024-02-14 00:48:53 +0100

    fix: card

--- src/pages/AdminHome/UserCard.tsx
+++ src/pages/AdminHome/UserCard.tsx
@@ -1,34 +1,40 @@
 import React, { type FC } from 'react';
+import { useSelector } from 'react-redux';
 
-import Avatar from '../../lib/iCoWeaUI/components/data-display/Avatar/Avatar';
-import Text from '../../lib/iCoWeaUI/components/data-display/Text/Text';
-import Title from '../../lib/iCoWeaUI/components/data-display/Title/Title';
-import Flex from '../../lib/iCoWeaUI/components/layouts/Flex/Flex';
-import Stack from '../../lib/iCoWeaUI/components/layouts/Stack/Stack';
-import Card from '../../lib/iCoWeaUI/components/surfaces/Card/Card';
+import Avatar from '../../lib/iCoWeABaseUI/components/data-display/Avatar/Avatar';
+import Text from '../../lib/iCoWeABaseUI/components/data-display/Text/Text';
+import Title from '../../lib/iCoWeABaseUI/components/data-display/Title/Title';
+import Flex from '../../lib/iCoWeABaseUI/components/layouts/Flex/Flex';
+import Stack from '../../lib/iCoWeABaseUI/components/layouts/Stack/Stack';
+import Card from '../../lib/iCoWeABaseUI/components/surfaces/Card/Card';
+import { upperCaseFirstLetter } from '../../lib/iCoWeAUtilsUI/utils/utils';
+import { selectUser } from '../../store/slices/user';
 
-const UserCard: FC = () => (
-  <Card
-    spacing="md"
-    gap="md"
-    className="col-span-2 max-md:col-span-1"
-  >
-    <Flex
-      gap="md"
-      align="stretch"
+const UserCard: FC = () => {
+  const user = useSelector(selectUser);
+
+  return (
+    <Card
+      spacing="lg"
+      gap="lg"
     >
-      <Avatar
-        src={require('../../assets/images/photo.png')}
-        alt="RW"
-        className="w-24 h-24"
-      />
-      <Stack justify="between">
-        <Title variant="4">Welcome</Title>
-        <Title variant="2">Richard Wagner</Title>
-      </Stack>
-    </Flex>
-    <Text>Welcome back to our comunity.</Text>
-  </Card>
-);
+      <Flex
+        gap="md"
+        align="stretch"
+      >
+        <Avatar
+          alt={`${upperCaseFirstLetter(user.firstname)} ${upperCaseFirstLetter(user.lastname)}`}
+          className="w-24"
+          src={require('../../assets/images/photo.png')}
+        />
+        <Stack justify="between">
+          <Title size="2">{`${user.firstname} ${user.lastname}`}</Title>
+          <Title size="4">Admin</Title>
+        </Stack>
+      </Flex>
+      <Text>Welcome back to our comunity.</Text>
+    </Card>
+  );
+};
 
 export default UserCard;
