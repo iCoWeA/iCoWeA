4f3f442b4959098ecd50b518035c38a67cd9828d
Richard Wagner <richard.wagner@gmail.com>
2024-02-14 22:06:01 +0100

    fix: listButton

--- src/pages/AdminProjects/Projects/ProjectListButton.tsx
+++ src/pages/AdminProjects/Projects/ProjectListButton.tsx
@@ -1,52 +1,22 @@
-import React, { type Dispatch, type SetStateAction, type FC, useRef, useCallback } from 'react';
+import React, { type FC, useState } from 'react';
 
 import Icon from '../../../lib/iCoWeABaseUI/components/data-display/Icon/Icon';
 import ListButton from '../../../lib/iCoWeABaseUI/components/data-display/ListButton/ListButton';
 import ListItem from '../../../lib/iCoWeABaseUI/components/data-display/ListItem/ListItem';
 import Button from '../../../lib/iCoWeABaseUI/components/inputs/Button/Button';
-import useAddEventListener from '../../../lib/iCoWeABaseUI/hooks/useAddEventListener';
 import EditProjectForm from './EditProjectForm';
 
 export type ProjectListButtonProps = {
-  setIsDraged: Dispatch<SetStateAction<number>>;
-  setIsEditing: Dispatch<SetStateAction<number>>;
-  isEditing: number;
   id: string;
   name: string;
   url: string;
   imageURL: string;
-  position: number;
-  draggable: boolean;
 };
 
-const ProjectListButton: FC<ProjectListButtonProps> = ({
-  setIsDraged,
-  setIsEditing,
-  isEditing,
-  id,
-  name,
-  url,
-  imageURL,
-  position,
-  draggable
-}) => {
-  const ref = useRef<HTMLLIElement>(null);
+const ProjectListButton: FC<ProjectListButtonProps> = ({ id, name, url, imageURL }) => {
+  const [isEditing, setIsEditing] = useState(false);
 
-  /* --- Set event handlers --- */
-  const dragStartHandler = useCallback((event: DragEvent): void => {
-    event.dataTransfer?.setData('listId', id);
-    setIsDraged(position);
-  }, []);
-
-  const dragEndHandler = useCallback((event: DragEvent): void => {
-    setIsDraged(-1);
-  }, []);
-
-  useAddEventListener(ref, 'dragstart', dragStartHandler);
-
-  useAddEventListener(ref, 'dragend', dragEndHandler);
-
-  if (isEditing === position) {
+  if (isEditing) {
     return (
       <ListItem
         spacing="md"
@@ -72,13 +42,11 @@ const ProjectListButton: FC<ProjectListButtonProps> = ({
       color="primary"
       radius="rounded"
       justify="between"
-      draggable={draggable}
-      ref={ref}
     >
       {name}
       <Button
         onClick={() => {
-          setIsEditing(position);
+          setIsEditing(false);
         }}
         size="sm"
         variant="plain"
