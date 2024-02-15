e851c1e932dd45545a3bc3ecf76fd8c4c2099eb0
Richard Wagner <richard.wagner@gmail.com>
2024-02-15 21:28:47 +0100

    fix

--- src/components/LogoutButton/logoutButton.tsx
+++ src/components/LogoutButton/logoutButton.tsx
@@ -1,29 +1,23 @@
 import React, { type FC } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 
 import { appAuth } from '../../firebase';
 import Button from '../../lib/iCoWeABaseUI/components/inputs/Button/Button';
 import LogoutIcon from '../Icons/LogoutIcon';
 
-const LogoutButton: FC = () => {
-  const navigate = useNavigate();
-
-  return (
-    <Button
-      onClick={() => {
-        void signOut(appAuth);
-        navigate('/logout');
-      }}
-      block
-      variant="soft"
-      color="error"
-      className="mt-auto"
-      leftDecorator={<LogoutIcon />}
-    >
-      Logout
-    </Button>
-  );
-};
+const LogoutButton: FC = () => (
+  <Button
+    onClick={() => {
+      void signOut(appAuth);
+    }}
+    block
+    variant="soft"
+    color="error"
+    className="mt-auto"
+    leftDecorator={<LogoutIcon />}
+  >
+    Logout
+  </Button>
+);
 
 export default LogoutButton;
