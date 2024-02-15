dc67061b1839e1b4c7273a0bc98a7120184ffa0a
Richard Wagner <richard.wagner@gmail.com>
2024-02-15 14:08:04 +0100

    fix: settings

---
+++ src/pages/AdminSettings/SettingsForm.tsx
@@ -0,0 +1,363 @@
+import React, { type FC } from 'react';
+import { useSelector } from 'react-redux';
+import { Form } from 'react-router-dom';
+
+import Textfield from '../../components/Textfield/Textfield';
+import Title from '../../lib/iCoWeABaseUI/components/data-display/Title/Title';
+import Button from '../../lib/iCoWeABaseUI/components/inputs/Button/Button';
+import Input from '../../lib/iCoWeABaseUI/components/inputs/Input/Input';
+import Textarea from '../../lib/iCoWeABaseUI/components/inputs/Textarea/Textarea';
+import Grid from '../../lib/iCoWeABaseUI/components/layouts/Grid/Grid';
+import Card from '../../lib/iCoWeABaseUI/components/surfaces/Card/Card';
+import useForm from '../../lib/iCoWeAHooks/hooks/useForm';
+import {
+  NAME_PATTERN,
+  EMAIL_PATTERN,
+  NUMBER_PATTERN
+} from '../../lib/iCoWeAUtilsUI/data/constants';
+import { selectUser } from '../../store/slices/user';
+import { isFormChanged } from '../../utils/utils';
+
+const SettingsForm: FC = () => {
+  const user = useSelector(selectUser);
+
+  const {
+    state: { inputs, isFormValid },
+    change,
+    blur,
+    revalidForm
+  } = useForm({
+    firstname: user.firstname,
+    lastname: user.lastname,
+    phone: user.phone,
+    email: user.email,
+    'image-url': user.imageURL,
+    dob: user.dob,
+    about: user.about,
+    street: user.street,
+    'street-number': user.streetNumber,
+    'postal-code': user.postalCode,
+    city: user.city,
+    country: user.country,
+    'github-url': user.githubURL,
+    'linkedin-url': user.linkedinURL,
+    'instagram-url': user.instagramURL,
+    'facebook-url': user.facebookURL
+  });
+
+  return (
+    <Form
+      method="post"
+      onFocus={(event) => revalidForm(event.target)}
+    >
+      <Card
+        spacing="lg"
+        gap="xxl"
+      >
+        <Grid
+          block
+          colGap="xxl"
+          rowGap="lg"
+          className="grid-cols-2 max-md:grid-cols-1"
+        >
+          <Title
+            size="3"
+            className="col-span-2 max-md:col-span-1"
+          >
+            Basic info
+          </Title>
+          <Textfield errorText={inputs.firstname.error && 'Invalid firstname'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs.firstname.error}
+              label="Firstname"
+              id="firstname"
+              maxLength={32}
+              name="firstname"
+              pattern={NAME_PATTERN}
+              required
+              value={inputs.firstname.value}
+            />
+          </Textfield>
+          <Textfield errorText={inputs.lastname.error && 'Invalid lastname'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs.lastname.error}
+              label="Lastname"
+              id="lastname"
+              maxLength={32}
+              name="lastname"
+              pattern={NAME_PATTERN}
+              required
+              value={inputs.lastname.value}
+            />
+          </Textfield>
+          <Textfield errorText={inputs.email.error && 'Invalid email'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs.email.error}
+              label="Email"
+              id="email"
+              name="email"
+              pattern={EMAIL_PATTERN}
+              required
+              value={inputs.email.value}
+            />
+          </Textfield>
+          <Textfield errorText={inputs.phone.error && 'Invalid phone'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs.phone.error}
+              label="Phone"
+              id="phone"
+              maxLength={10}
+              name="phone"
+              pattern={NUMBER_PATTERN}
+              required
+              value={inputs.phone.value}
+            />
+          </Textfield>
+          <Textfield errorText={inputs['image-url'].error && 'Invalid image URL'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs['image-url'].error}
+              label="Image URL"
+              id="image-url"
+              name="image-url"
+              required
+              value={inputs['image-url'].value}
+            />
+          </Textfield>
+          <Textfield errorText={inputs.dob.error && 'Invalid date of birth'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs.dob.error}
+              label="Date of birth"
+              id="dob"
+              name="dob"
+              required
+              type="date"
+              value={inputs.dob.value}
+            />
+          </Textfield>
+          <Textfield
+            errorText={inputs.about.error && 'Invalid about me'}
+            className="col-span-2 max-md:col-span-1"
+          >
+            <Textarea
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs.about.error}
+              label="About me"
+              id="about"
+              maxLength={500}
+              name="about"
+              required
+              value={inputs.about.value}
+            />
+          </Textfield>
+        </Grid>
+        <Grid
+          block
+          colGap="xxl"
+          rowGap="lg"
+          className="grid-cols-2 max-md:grid-cols-1"
+        >
+          <Title
+            size="3"
+            className="col-span-2 max-md:col-span-1"
+          >
+            Address
+          </Title>
+          <Textfield errorText={inputs.street.error && 'Invalid street'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs.street.error}
+              label="Street"
+              id="street"
+              name="street"
+              pattern={NAME_PATTERN}
+              required
+              value={inputs.street.value}
+            />
+          </Textfield>
+          <Textfield errorText={inputs['street-number'].error && 'Invalid street number'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs['street-number'].error}
+              label="Street number"
+              id="street-number"
+              maxLength={5}
+              name="street-number"
+              pattern={NUMBER_PATTERN}
+              required
+              value={inputs['street-number'].value}
+            />
+          </Textfield>
+          <Textfield errorText={inputs.city.error && 'Invalid city'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs.city.error}
+              label="City"
+              id="city"
+              name="city"
+              pattern={NAME_PATTERN}
+              required
+              value={inputs.city.value}
+            />
+          </Textfield>
+          <Textfield errorText={inputs.country.error && 'Invalid country'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs.country.error}
+              label="Country"
+              id="country"
+              name="country"
+              pattern={NAME_PATTERN}
+              required
+              value={inputs.country.value}
+            />
+          </Textfield>
+          <Textfield errorText={inputs['postal-code'].error && 'Invalid postal-code'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs['postal-code'].error}
+              label="Postal code"
+              id="postal-code"
+              maxLength={10}
+              name="postal-code"
+              required
+              value={inputs['postal-code'].value}
+            />
+          </Textfield>
+        </Grid>
+        <Grid
+          block
+          colGap="xxl"
+          rowGap="lg"
+          className="grid-cols-2 max-md:grid-cols-1"
+        >
+          <Title
+            size="3"
+            className="col-span-2 max-md:col-span-1"
+          >
+            Social networks
+          </Title>
+          <Textfield errorText={inputs['github-url'].error && 'Invalid github'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs['github-url'].error}
+              label="Github URL"
+              id="github-url"
+              name="github-url"
+              required
+              value={inputs['github-url'].value}
+            />
+          </Textfield>
+          <Textfield errorText={inputs['linkedin-url'].error && 'Invalid linkedIn'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs['linkedin-url'].error}
+              label="LinkedIn URL"
+              id="linkedin-url"
+              name="linkedin-url"
+              required
+              value={inputs['linkedin-url'].value}
+            />
+          </Textfield>
+          <Textfield errorText={inputs['facebook-url'].error && 'Invalid facebook'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs['facebook-url'].error}
+              label="Facebook URL"
+              id="facebook-url"
+              name="facebook-url"
+              required
+              value={inputs['facebook-url'].value}
+            />
+          </Textfield>
+          <Textfield errorText={inputs['instagram-url'].error && 'Invalid instagram'}>
+            <Input
+              onChange={(event) => change(event, 1000)}
+              onBlur={blur}
+              block
+              variant="default"
+              invalid={inputs['instagram-url'].error}
+              label="Instagram URL"
+              id="instagram-url"
+              name="instagram-url"
+              required
+              value={inputs['instagram-url'].value}
+            />
+          </Textfield>
+        </Grid>
+        <Button
+          size="lg"
+          block
+          disabled={
+            !isFormValid ||
+            !isFormChanged(user, {
+              ...inputs,
+              streetNumber: inputs['street-number'],
+              postalCode: inputs['postal-code'],
+              imageURL: inputs['image-url'],
+              githubURL: inputs['github-url'],
+              linkedinURL: inputs['linkedin-url'],
+              facebookURL: inputs['facebook-url'],
+              instagramURL: inputs['instagram-url']
+            })
+          }
+          type="submit"
+        >
+          Save
+        </Button>
+      </Card>
+    </Form>
+  );
+};
+
+export default SettingsForm;
