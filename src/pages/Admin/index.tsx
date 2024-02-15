a202f7076c05e7d32ad9b0cf51f4686fb827e105
Richard Wagner <richard.wagner@gmail.com>
2024-02-15 16:40:27 +0100

    fix: admin

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,6 +1,41 @@
-import React, { type FC } from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { type FC, useRef, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, Outlet } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { ref, onValue } from 'firebase/database';
 
-export const Component: FC = () => <Outlet />;
+import LoadingScreen from '../../components/LoadingScreen/LoadingScreen';
+import { database, appAuth } from '../../firebase';
+import { selectMessages, messagesActions } from '../../store/slices/messages';
 
-Component.displayName = 'AdminRoute';
+export const Component: FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useSelector(selectMessages);
+
+  const isEmpty = useRef(true);
+
+  /* --- Set event handlers --- */
+  useEffect(() => {
+    const userRef = ref(database, 'messages');
+
+    return onValue(userRef, (snap) => {
+      isEmpty.current = false;
+
+      if (snap.exists()) {
+        dispatch(messagesActions.setMessages(snap.val()));
+
+        return;
+      }
+
+      dispatch(messagesActions.setMessages({}));
+    });
+  }, []);
+
+  useEffect(() => {
+    return onAuthStateChanged(appAuth, (user) => !user && navigate('/login'));
+  }, []);
+
+  return isEmpty.current ? <LoadingScreen /> : <Outlet />;
+};
