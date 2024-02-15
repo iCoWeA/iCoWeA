d95d7434da164cfc221afbef66eccbb7942f0fa1
Richard Wagner <richard.wagner@gmail.com>
2024-02-15 10:09:11 +0100

    fix: card

--- src/pages/AdminHome/MessagesCard.tsx
+++ src/pages/AdminHome/MessagesCard.tsx
@@ -1,9 +1,30 @@
-import React, { type FC } from 'react';
+import React, { type FC, useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 import Title from '../../lib/iCoWeABaseUI/components/data-display/Title/Title';
 import Card from '../../lib/iCoWeABaseUI/components/surfaces/Card/Card';
+import { selectMessages } from '../../store/slices/messages';
 
 const MessagesCard: FC = () => {
+  const messages = useSelector(selectMessages);
+
+  const [messageCounter, setMessagesCounter] = useState(0);
+
+  const count = Object.keys(messages).length;
+
+  useEffect(() => setMessagesCounter(0), [count]);
+
+  /* --- Set event handlers --- */
+  useEffect(() => {
+    if (messageCounter === count) {
+      return;
+    }
+
+    const id = window.setTimeout(() => setMessagesCounter((count) => count + 1), 500 / count);
+
+    return () => clearTimeout(id);
+  }, [count, messageCounter]);
+
   return (
     <Card
       spacing="lg"
@@ -15,7 +36,7 @@ const MessagesCard: FC = () => {
         size="2"
         color="inherit"
       >
-        0
+        {messageCounter}
       </Title>
       <Title
         size="4"
