5c17a7235201adeb69f789f9c322ce824cad99f2
Richard Wagner <richard.wagner@gmail.com>
2024-02-15 21:50:58 +0100

    fix

--- src/components/Icons/FullTrash.tsx
+++ src/components/Icons/FullTrash.tsx
@@ -8,7 +8,7 @@ const FullTrashIcon: FC<IconProps> = (props) => (
       focusable="false"
       viewBox="0 0 24 24"
     >
-      <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z"></path>
+      <path d="Ms6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z"></path>
     </svg>
   </Icon>
 );
