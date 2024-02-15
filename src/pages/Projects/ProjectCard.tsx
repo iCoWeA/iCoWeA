297685aa8c61f0ea7d68e7bc46c915d817709219
Richard Wagner <richard.wagner@gmail.com>
2024-02-15 20:37:28 +0100

    fix: icon

--- src/pages/Projects/ProjectCard.tsx
+++ src/pages/Projects/ProjectCard.tsx
@@ -1,6 +1,6 @@
 import React, { type FC, useState } from 'react';
 
-import Icon from '../../lib/iCoWeABaseUI/components/data-display/Icon/Icon';
+import LinkIcon from '../../components/Icons/LinkIcon';
 import ListItem from '../../lib/iCoWeABaseUI/components/data-display/ListItem/ListItem';
 import Overlay from '../../lib/iCoWeABaseUI/components/feedback/Overlay/Overlay';
 import Card from '../../lib/iCoWeABaseUI/components/surfaces/Card/Card';
@@ -44,16 +44,7 @@ const ProjectCard: FC<ProjectCardProps> = ({ name, url, imageURL }) => {
           variant="solid"
           color="secondary"
           className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 text-base whitespace-pre"
-          rightDecorator={
-            <Icon size="sm">
-              <svg
-                focusable="false"
-                viewBox="0 0 24 24"
-              >
-                <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3z"></path>
-              </svg>
-            </Icon>
-          }
+          rightDecorator={<LinkIcon size="sm" />}
         >
           {`Visit ${name}`}
         </LinkButton>
