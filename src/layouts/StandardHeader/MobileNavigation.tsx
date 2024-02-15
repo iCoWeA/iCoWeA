00ad816ea219ad16c9f0b65f4060b8dfeb3b385f
Richard Wagner <richard.wagner@gmail.com>
2024-02-15 21:17:37 +0100

    fix: mobile

--- src/layouts/StandardHeader/MobileNavigation.tsx
+++ src/layouts/StandardHeader/MobileNavigation.tsx
@@ -1,8 +1,16 @@
 import React, { type FC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import AboutIcon from '../../components/Icons/AboutIcon';
+import ContactIcon from '../../components/Icons/ContactIcon';
+import FacebookIcon from '../../components/Icons/FacebookIcon';
+import GithubIcon from '../../components/Icons/GithubIcon';
+import HamburgerIcon from '../../components/Icons/HamburgerIcon';
+import HomeIcon from '../../components/Icons/HomeIcon';
+import InstagramIcon from '../../components/Icons/InstagramIcon';
+import LinkedInIcon from '../../components/Icons/LinkedInIcon';
 import Logo from '../../components/Icons/Logo';
-import Icon from '../../lib/iCoWeABaseUI/components/data-display/Icon/Icon';
+import ProjectIcon from '../../components/Icons/ProjectIcon';
 import ListItem from '../../lib/iCoWeABaseUI/components/data-display/ListItem/ListItem';
 import Button from '../../lib/iCoWeABaseUI/components/inputs/Button/Button';
 import Flex from '../../lib/iCoWeABaseUI/components/layouts/Flex/Flex';
@@ -25,14 +33,7 @@ const MobileNavigation: FC = () => {
         icon
         color="secondary"
       >
-        <Icon>
-          <svg
-            focusable="false"
-            viewBox="0 0 24 24"
-          >
-            <path d="M3 18h18v-2H3zm0-5h18v-2H3zm0-7v2h18V6z"></path>
-          </svg>
-        </Icon>
+        <HamburgerIcon />
       </Button>
       <Drawer
         onClose={() => dispatch(navbarActions.close())}
@@ -51,16 +52,7 @@ const MobileNavigation: FC = () => {
             <ListNavlink
               to="/"
               activeVariant="solid"
-              leftDecorator={
-                <Icon>
-                  <svg
-                    focusable="false"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path>
-                  </svg>
-                </Icon>
-              }
+              leftDecorator={<HomeIcon />}
             >
               Home
             </ListNavlink>
@@ -69,16 +61,7 @@ const MobileNavigation: FC = () => {
             <ListNavlink
               to="projects"
               activeVariant="solid"
-              leftDecorator={
-                <Icon>
-                  <svg
-                    focusable="false"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M19 2h-4.18C14.4.84 13.3 0 12 0c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2m-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1m7 18H5V4h2v3h10V4h2z"></path>
-                  </svg>
-                </Icon>
-              }
+              leftDecorator={<ProjectIcon />}
             >
               Projects
             </ListNavlink>
@@ -87,16 +70,7 @@ const MobileNavigation: FC = () => {
             <ListNavlink
               to="about"
               activeVariant="solid"
-              leftDecorator={
-                <Icon>
-                  <svg
-                    focusable="false"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M17.5 4.5c-1.95 0-4.05.4-5.5 1.5-1.45-1.1-3.55-1.5-5.5-1.5S2.45 4.9 1 6v14.65c0 .65.73.45.75.45C3.1 20.45 5.05 20 6.5 20c1.95 0 4.05.4 5.5 1.5 1.35-.85 3.8-1.5 5.5-1.5 1.65 0 3.35.3 4.75 1.05.41.21.75-.19.75-.45V6c-1.49-1.12-3.63-1.5-5.5-1.5m3.5 14c-1.1-.35-2.3-.5-3.5-.5-1.7 0-4.15.65-5.5 1.5V8c1.35-.85 3.8-1.5 5.5-1.5 1.2 0 2.4.15 3.5.5z"></path>
-                  </svg>
-                </Icon>
-              }
+              leftDecorator={<AboutIcon />}
             >
               About
             </ListNavlink>
@@ -105,16 +79,7 @@ const MobileNavigation: FC = () => {
             <ListNavlink
               to="contact"
               activeVariant="solid"
-              leftDecorator={
-                <Icon>
-                  <svg
-                    focusable="false"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20 0H4v2h16zM4 24h16v-2H4zM20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m-8 2.75c1.24 0 2.25 1.01 2.25 2.25s-1.01 2.25-2.25 2.25S9.75 10.24 9.75 9 10.76 6.75 12 6.75M17 17H7v-1.5c0-1.67 3.33-2.5 5-2.5s5 .83 5 2.5z"></path>
-                  </svg>
-                </Icon>
-              }
+              leftDecorator={<ContactIcon />}
             >
               Contact
             </ListNavlink>
@@ -129,53 +94,25 @@ const MobileNavigation: FC = () => {
             to={user.githubURL}
             target="_blanc"
           >
-            <Icon>
-              <svg
-                focusable="false"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 1.27a11 11 0 00-3.48 21.46c.55.09.73-.28.73-.55v-1.84c-3.03.64-3.67-1.46-3.67-1.46-.55-1.29-1.28-1.65-1.28-1.65-.92-.65.1-.65.1-.65 1.1 0 1.73 1.1 1.73 1.1.92 1.65 2.57 1.2 3.21.92a2 2 0 01.64-1.47c-2.47-.27-5.04-1.19-5.04-5.5 0-1.1.46-2.1 1.2-2.84a3.76 3.76 0 010-2.93s.91-.28 3.11 1.1c1.8-.49 3.7-.49 5.5 0 2.1-1.38 3.02-1.1 3.02-1.1a3.76 3.76 0 010 2.93c.83.74 1.2 1.74 1.2 2.94 0 4.21-2.57 5.13-5.04 5.4.45.37.82.92.82 2.02v3.03c0 .27.1.64.73.55A11 11 0 0012 1.27"></path>
-              </svg>
-            </Icon>
+            <GithubIcon />
           </Link>
           <Link
             to={user.linkedinURL}
             target="_blanc"
           >
-            <Icon>
-              <svg
-                focusable="false"
-                viewBox="0 0 24 24"
-              >
-                <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z"></path>
-              </svg>
-            </Icon>
+            <LinkedInIcon />
           </Link>
           <Link
             to={user.facebookURL}
             target="_blanc"
           >
-            <Icon>
-              <svg
-                focusable="false"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2m13 2h-2.5A3.5 3.5 0 0 0 12 8.5V11h-2v3h2v7h3v-7h3v-3h-3V9a1 1 0 0 1 1-1h2V5z"></path>
-              </svg>
-            </Icon>
+            <FacebookIcon />
           </Link>
           <Link
             to={user.instagramURL}
             target="_blanc"
           >
-            <Icon>
-              <svg
-                focusable="false"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path>
-              </svg>
-            </Icon>
+            <InstagramIcon />
           </Link>
         </Flex>
       </Drawer>
