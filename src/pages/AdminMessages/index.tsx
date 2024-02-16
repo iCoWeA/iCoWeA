a8e67c62b8d2d0a5630c1301fc4ea9c413745dea
Richard Wagner <richard.wagner@gmail.com>
2024-02-16 11:42:06 +0100

    fix: messages

--- src/pages/AdminMessages/index.tsx
+++ src/pages/AdminMessages/index.tsx
@@ -1,5 +1,5 @@
 import React, { type FC } from 'react';
-import { remove, ref } from 'firebase/database';
+import { update, ref, remove } from 'firebase/database';
 
 import { database } from '../../firebase';
 import Main from '../../lib/iCoWeABaseUI/components/layouts/Main/Main';
@@ -14,7 +14,31 @@ export const Component: FC = () => (
 export const action = async ({ request }: { request: Request }): Promise<unknown> => {
   const formData = await request.formData();
 
+  const read = formData.get('read')?.toString();
+  const unread = formData.get('unread')?.toString();
   const del = formData.get('del')?.toString();
+
+  if (read) {
+    const data = {
+      unread: false,
+      lastModificationDate: new Date().toISOString()
+    };
+
+    await update(ref(database, `messages/${read}`), data);
+
+    return data;
+  }
+
+  if (unread) {
+    const data = {
+      unread: true,
+      lastModificationDate: new Date().toISOString()
+    };
+
+    await update(ref(database, `messages/${unread}`), data);
+
+    return data;
+  }
 
   if (!del) {
     throw new Error('No del');
