9164a49155f6a65ccc48bf170bee3cfefb5a6d7d
Richard Wagner <richard.wagner@gmail.com>
2024-02-16 13:03:09 +0100

    fix: messages

--- src/pages/AdminMessages/Messages.tsx
+++ src/pages/AdminMessages/Messages.tsx
@@ -1,8 +1,11 @@
 import React, { type FC, useState } from 'react';
 import { useSelector } from 'react-redux';
 
+import SortIcon from '../../components/Icons/SortIcon';
 import List from '../../lib/iCoWeABaseUI/components/data-display/List/List';
 import Title from '../../lib/iCoWeABaseUI/components/data-display/Title/Title';
+import ToggleButton from '../../lib/iCoWeABaseUI/components/inputs/ToggleButton/ToggleButton';
+import Flex from '../../lib/iCoWeABaseUI/components/layouts/Flex/Flex';
 import Section from '../../lib/iCoWeABaseUI/components/layouts/Section/Section';
 import Card from '../../lib/iCoWeABaseUI/components/surfaces/Card/Card';
 import { selectMessages } from '../../store/slices/messages';
@@ -15,32 +18,41 @@ const Messages: FC = () => {
 
   const [isDraged, setIsDraged] = useState(false);
   const [isReading, setIsReading] = useState('');
+  const [descendingSort, setDescendingSort] = useState(true);
 
-  const nodes = Object.keys(messages).map((key) =>
-    isReading === key
-      ? (
-      <MessageItem
-        key={key}
-        setIsReading={setIsReading}
-        name={messages[key].name}
-        email={messages[key].email}
-        subject={messages[key].subject}
-        message={messages[key].message}
-      />
-        )
-      : (
-      <MessageButton
-        key={key}
-        setIsDraged={setIsDraged}
-        setIsReading={setIsReading}
-        isReading={isReading}
-        id={key}
-        name={messages[key].name}
-        unread={messages[key].unread}
-        draggable={!isDraged && isReading === ''}
-      />
-        )
-  );
+  const nodes = Object.keys(messages)
+    .sort((a, b) =>
+      descendingSort
+        ? new Date(messages[b].creationDate).getMilliseconds() -
+          new Date(messages[a].creationDate).getMilliseconds()
+        : new Date(messages[a].creationDate).getMilliseconds() -
+          new Date(messages[b].creationDate).getMilliseconds()
+    )
+    .map((key) =>
+      isReading === key
+        ? (
+        <MessageItem
+          key={key}
+          setIsReading={setIsReading}
+          name={messages[key].name}
+          email={messages[key].email}
+          subject={messages[key].subject}
+          message={messages[key].message}
+        />
+          )
+        : (
+        <MessageButton
+          key={key}
+          setIsDraged={setIsDraged}
+          setIsReading={setIsReading}
+          isReading={isReading}
+          id={key}
+          name={messages[key].name}
+          unread={messages[key].unread}
+          draggable={!isDraged && isReading === ''}
+        />
+          )
+    );
 
   return (
     <Section>
@@ -48,6 +60,18 @@ const Messages: FC = () => {
         spacing="lg"
         gap="lg"
       >
+        <Flex>
+          <ToggleButton
+            onClick={() => setDescendingSort((sort) => !sort)}
+            checked={descendingSort}
+            size="lg"
+            variant="solid"
+            radius="circular"
+            leftDecorator={<SortIcon className={descendingSort ? '' : 'rotate-180'} />}
+          >
+            {descendingSort ? 'Newest date' : 'Latest date'}
+          </ToggleButton>
+        </Flex>
         {nodes.length === 0 && (
           <Title
             size="3"
