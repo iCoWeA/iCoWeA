bcf75d8054cec0d73dc8fe4c00cbb1233bf011d9
Richard Wagner <richard.wagner@gmail.com>
2024-02-16 02:38:43 +0100

    fix: utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,3 +2,13 @@ import { type InputState } from '../lib/iCoWeAHooks/hooks/useForm';
 
 export const isFormChanged = (defaultValue: Obj, values: Record<string, InputState>): boolean =>
   Object.keys(defaultValue).some((key) => defaultValue[key] !== values[key].value);
+
+export const modifyNumber = (phone: string): string => {
+  let newPhone = phone.substring(0, 4) + ' ';
+
+  for (let i = 4; i <= phone.length; i = i + 3) {
+    newPhone = newPhone + phone.substring(i, i + 3) + ' ';
+  }
+
+  return newPhone;
+};
