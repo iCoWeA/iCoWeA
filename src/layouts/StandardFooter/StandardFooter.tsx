724da910429467ab88482c50eb921e8f9aa16cf3
Richard Wagner <richard.wagner@gmail.com>
2024-02-21 15:51:10 +0100

    fix: footer

--- src/layouts/StandardFooter/StandardFooter.tsx
+++ src/layouts/StandardFooter/StandardFooter.tsx
@@ -1,30 +1,24 @@
 import React, { type FC } from 'react';
-import { useSelector } from 'react-redux';
 
 import GithubIcon from '../../components/Icons/GithubIcon';
 import Text from '../../lib/iCoWeABaseUI/components/data-display/Text/Text';
 import Footer from '../../lib/iCoWeABaseUI/components/layouts/Footer/Footer';
 import Link from '../../lib/iCoWeARouterUI/components/Link/Link';
-import { selectUser } from '../../store/slices/user';
 
-const StandardFooter: FC = () => {
-  const github = useSelector(selectUser).githubURL;
-
-  return (
-    <Footer containerProps={{ className: 'max-lg:flex-col' }}>
-      <Text align="center">Copyright © 2024 Richard Wagner. All Rights Reserved.</Text>
-      <Text align="center">
-        Designed by <Link to="/">Richard Wagner</Link>. Powered by <Link to="/">iCoWeAUI</Link>
-        <Link
-          to={github}
-          target="_blanc"
-          className="ml-4"
-        >
-          <GithubIcon />
-        </Link>
-      </Text>
-    </Footer>
-  );
-};
+const StandardFooter: FC = () => (
+  <Footer containerProps={{ className: 'max-lg:flex-col' }}>
+    <Text align="center">Copyright © 2024 Richard Wagner. All Rights Reserved.</Text>
+    <Text align="center">
+      Designed by <Link to="/">Richard Wagner</Link>. Powered by <Link to="/">iCoWeAUI</Link>
+      <Link
+        to="https://github.com/iCoWeA/iCoWeA"
+        target="_blanc"
+        className="ml-4"
+      >
+        <GithubIcon />
+      </Link>
+    </Text>
+  </Footer>
+);
 
 export default StandardFooter;
