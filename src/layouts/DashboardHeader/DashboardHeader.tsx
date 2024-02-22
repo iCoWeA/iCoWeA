87413ceca4ab2be205fd58baf419e1e1816a2e6f
Richard Wagner <richard.wagner@gmail.com>
2024-02-22 15:31:48 +0100

    fix: dashbor

--- src/layouts/DashboardHeader/DashboardHeader.tsx
+++ src/layouts/DashboardHeader/DashboardHeader.tsx
@@ -5,9 +5,8 @@ import Logo from '../../components/Icons/Logo';
 import Avatar from '../../lib/iCoWeABaseUI/components/data-display/Avatar/Avatar';
 import Text from '../../lib/iCoWeABaseUI/components/data-display/Text/Text';
 import Title from '../../lib/iCoWeABaseUI/components/data-display/Title/Title';
-import Flex from '../../lib/iCoWeABaseUI/components/layouts/Flex/Flex';
+import Grid from '../../lib/iCoWeABaseUI/components/layouts/Grid/Grid';
 import Header from '../../lib/iCoWeABaseUI/components/layouts/Header/Header';
-import Stack from '../../lib/iCoWeABaseUI/components/layouts/Stack/Stack';
 import Link from '../../lib/iCoWeARouterUI/components/Link/Link';
 import { upperCaseFirstLetter } from '../../lib/iCoWeAUtilsUI/utils/utils';
 import { selectBreakpoint, Breakpoints } from '../../store/slices/breakpoint';
@@ -19,33 +18,37 @@ const DashboardHeader: FC = () => {
   const user = useSelector(selectUser);
   const name = `${upperCaseFirstLetter(user.firstname)} ${upperCaseFirstLetter(user.lastname)}`;
 
+  const isSm = breakpoint === Breakpoints.SM || breakpoint === Breakpoints.MD;
+
   return (
     <Header
       block
       shadow
     >
-      {(breakpoint === Breakpoints.SM || breakpoint === Breakpoints.MD) && <MobileNavigation />}
-      {breakpoint !== Breakpoints.SM && breakpoint !== Breakpoints.MD && (
+      {isSm && <MobileNavigation />}
+      {!isSm && (
         <Link to="">
           <Logo />
         </Link>
       )}
-      <Flex gap="base">
-        <Stack>
-          <Title>{name}</Title>
-          <Text
-            size="sm"
-            align="right"
-          >
-            Admin
-          </Text>
-        </Stack>
+      <Grid
+        colGap="base"
+        className="grid-cols-[auto_auto]"
+      >
+        <Title>{name}</Title>
         <Avatar
           color="neutral"
           alt={name}
+          className="row-span-2"
           src={user.imageURL}
         />
-      </Flex>
+        <Text
+          size="sm"
+          align="right"
+        >
+          Admin
+        </Text>
+      </Grid>
     </Header>
   );
 };
