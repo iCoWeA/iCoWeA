5768969f3ececb3cd1526d6ca25e0bdeb0b096fb
Richard Wagner <richard.wagner@gmail.com>
2024-02-22 16:58:20 +0100

    fix: header

--- src/layouts/StandardHeader/StandardHeader.tsx
+++ src/layouts/StandardHeader/StandardHeader.tsx
@@ -2,10 +2,13 @@ import React, { type FC } from 'react';
 import { useSelector } from 'react-redux';
 
 import Logo from '../../components/Icons/Logo';
+import ThemeButton from '../../components/ThemeButton.tsx/ThemeButton';
+import ListItem from '../../lib/iCoWeABaseUI/components/data-display/ListItem/ListItem';
 import Header from '../../lib/iCoWeABaseUI/components/layouts/Header/Header';
+import Navigation from '../../lib/iCoWeABaseUI/components/layouts/Navigation/Navigation';
 import Link from '../../lib/iCoWeARouterUI/components/Link/Link';
+import Navlink from '../../lib/iCoWeARouterUI/components/Navlink/Navlink';
 import { selectBreakpoint, Breakpoints } from '../../store/slices/breakpoint';
-import DefaultNavigation from './DefaultNavigation';
 import MobileNavigation from './MobileNavigation';
 
 const StandardHeader: FC = () => {
@@ -22,7 +25,52 @@ const StandardHeader: FC = () => {
         <Logo />
       </Link>
       {isSm && <MobileNavigation />}
-      {!isSm && <DefaultNavigation />}
+      {!isSm && (
+        <>
+          <Navigation className="ml-auto">
+            <ListItem>
+              <Navlink
+                to="/"
+                activeVariant="solid"
+                activeColor="secondary"
+              >
+                Home
+              </Navlink>
+            </ListItem>
+            <ListItem>
+              <Navlink
+                to="projects"
+                activeVariant="solid"
+                activeColor="secondary"
+              >
+                Projects
+              </Navlink>
+            </ListItem>
+            <ListItem>
+              <Navlink
+                to="about"
+                activeVariant="solid"
+                activeColor="secondary"
+              >
+                About
+              </Navlink>
+            </ListItem>
+            <ListItem>
+              <Navlink
+                to="contact"
+                activeVariant="solid"
+                activeColor="secondary"
+              >
+                Contact
+              </Navlink>
+            </ListItem>
+          </Navigation>
+          <ThemeButton
+            icon
+            radius="rounded"
+          />
+        </>
+      )}
     </Header>
   );
 };
