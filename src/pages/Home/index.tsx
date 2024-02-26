d12957679150d19ab2d25a163a2e066c117a4a0d
Richard Wagner <richard.wagner@gmail.com>
2024-02-27 00:28:37 +0100

    fix: home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,13 @@ import React, { type FC } from 'react';
 
 import Layout from '../../lib/iCoWeABaseUI/components/layouts/Layout/Layout';
 import Main from '../../lib/iCoWeABaseUI/components/layouts/Main/Main';
+import Introduction from './Introduction';
 
 export const Component: FC = () => (
   <Layout layout="default">
-    <Main></Main>
+    <Main>
+      <Introduction />
+    </Main>
   </Layout>
 );
 
