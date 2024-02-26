7f743b659ab65beee1da21abea9ab9826b596028
Richard Wagner <richard.wagner@gmail.com>
2024-02-27 00:37:26 +0100

    fix: project

---
+++ src/pages/Projects/Project.tsx
@@ -0,0 +1,50 @@
+import React, { type FC, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+
+import AnimatedSection from '../../components/AnimatedSection/AnimatedSection';
+import List from '../../lib/iCoWeABaseUI/components/data-display/List/List';
+import Title from '../../lib/iCoWeABaseUI/components/data-display/Title/Title';
+import { selectProjects } from '../../store/slices/projects';
+import ProjectCard from './ProjectCard';
+
+const Project: FC = () => {
+  const projects = useSelector(selectProjects);
+
+  const nodes = useMemo(
+    () =>
+      Object.keys(projects).map((key) => (
+        <ProjectCard
+          key={key}
+          name={projects[key].name}
+          url={projects[key].url}
+          imageURL={projects[key].imageURL}
+        />
+      )),
+    [projects]
+  );
+
+  return (
+    <AnimatedSection>
+      {nodes.length === 0 && (
+        <Title
+          size="2"
+          color="on-primary"
+          align="center"
+        >
+          No projects
+        </Title>
+      )}
+      {nodes.length !== 0 && (
+        <List
+          row
+          justify="center"
+          gap="xxl"
+        >
+          {nodes}
+        </List>
+      )}
+    </AnimatedSection>
+  );
+};
+
+export default Project;
--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,52 +1,15 @@
 import React, { type FC } from 'react';
-import { useSelector } from 'react-redux';
 
-import AnimatedSection from '../../components/AnimatedSection/AnimatedSection';
-import List from '../../lib/iCoWeABaseUI/components/data-display/List/List';
-import Title from '../../lib/iCoWeABaseUI/components/data-display/Title/Title';
 import Layout from '../../lib/iCoWeABaseUI/components/layouts/Layout/Layout';
 import Main from '../../lib/iCoWeABaseUI/components/layouts/Main/Main';
-import { selectProjects } from '../../store/slices/projects';
-import ProjectCard from './ProjectCard';
+import Projects from '../AdminProjects/Projects/Projects';
 
-export const Component: FC = () => {
-  const projects = useSelector(selectProjects);
-
-  const nodes = Object.keys(projects).map((key) => (
-    <ProjectCard
-      key={key}
-      name={projects[key].name}
-      url={projects[key].url}
-      imageURL={projects[key].imageURL}
-    />
-  ));
-
-  return (
-    <Layout layout="default">
-      <Main>
-        <AnimatedSection>
-          {nodes.length === 0 && (
-            <Title
-              size="2"
-              color="on-primary"
-              align="center"
-            >
-              No projects
-            </Title>
-          )}
-          {nodes.length !== 0 && (
-            <List
-              row
-              justify="center"
-              gap="xxl"
-            >
-              {nodes}
-            </List>
-          )}
-        </AnimatedSection>
-      </Main>
-    </Layout>
-  );
-};
+export const Component: FC = () => (
+  <Layout layout="default">
+    <Main>
+      <Projects />
+    </Main>
+  </Layout>
+);
 
 Component.displayName = 'HomeRoute';
