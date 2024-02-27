d944985c67ebac1ebe0670be43116084176b73b9
Richard Wagner <richard.wagner@gmail.com>
2024-02-27 13:04:36 +0100

    fix

--- src/pages/AdminProjects/AddProjectForm.tsx
+++ src/pages/AdminProjects/AddProjectForm.tsx
@@ -1,37 +1,65 @@
-import React, { type Dispatch, type SetStateAction, type FC } from 'react';
+import React, {
+  type Dispatch,
+  type SetStateAction,
+  type FC,
+  useCallback,
+  type ChangeEvent
+} from 'react';
+import { useSelector } from 'react-redux';
 import { Form } from 'react-router-dom';
 
-import InputControl from '../../components/InputControl/InputControl';
 import SubmitButton from '../../components/SubmitButton/SubmitButton';
 import Textfield from '../../components/Textfield/Textfield';
-import Stack from '../../lib/iCoWeABaseUI/components/layouts/Stack/Stack';
+import Card from '../../lib/iCoWeABaseUI/components/surfaces/Card/Card';
+import Collapse from '../../lib/iCoWeABaseUI/components/utils/Collapse/Collapse';
 import useForm from '../../lib/iCoWeAHooks/hooks/useForm';
+import { selectProjects } from '../../store/slices/projects';
 
 export type AddProjectFormProps = {
   setOpen: Dispatch<SetStateAction<boolean>>;
+  open: boolean;
 };
 
-const AddProjectForm: FC<AddProjectFormProps> = ({ setOpen }) => {
+const AddProjectForm: FC<AddProjectFormProps> = ({ setOpen, open }) => {
+  const projects = useSelector(selectProjects);
+
   const {
     state: { inputs, isFormValid },
     change,
     blur,
     resetForm
-  } = useForm({ name: '', url: '', 'image-url': '' });
+  } = useForm({
+    name: '',
+    url: '',
+    'image-url': ''
+  });
+
+  /* -- Set event handlers --- */
+  const submitHandler = useCallback(() => {
+    resetForm();
+    setOpen(false);
+  }, []);
+
+  const changeHandler = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => change(event, 1000),
+    []
+  );
 
   return (
-    <Form
-      onSubmit={() => {
-        resetForm();
-        setOpen(false);
-      }}
-      method="post"
-      className="mb-8"
+    <Collapse
+      open={open}
+      className={`w-full${open ? ' mt-8' : ''}`}
     >
-      <Stack gap="lg">
-        <InputControl>
+      <Form
+        onSubmit={submitHandler}
+        method="post"
+      >
+        <Card
+          spacing="md"
+          gap="lg"
+        >
           <Textfield
-            onChange={(event) => change(event, 1000)}
+            onChange={changeHandler}
             onBlur={blur}
             variant="default"
             invalid={inputs.name.error}
@@ -42,42 +70,40 @@ const AddProjectForm: FC<AddProjectFormProps> = ({ setOpen }) => {
             required
             value={inputs.name.value}
           />
-        </InputControl>
-        <InputControl>
           <Textfield
-            onChange={(event) => change(event, 1000)}
+            onChange={changeHandler}
             onBlur={blur}
             variant="default"
             invalid={inputs.url.error}
-            label="Url"
+            label="URL"
             id="url"
             name="url"
             required
             value={inputs.url.value}
           />
-        </InputControl>
-        <InputControl>
           <Textfield
-            onChange={(event) => change(event, 1000)}
+            onChange={changeHandler}
             onBlur={blur}
             variant="default"
             invalid={inputs['image-url'].error}
             label="Image URL"
             id="image-url"
+            maxLength={32}
             name="image-url"
             required
             value={inputs['image-url'].value}
           />
-        </InputControl>
-        <SubmitButton
-          size="lg"
-          disabled={!isFormValid}
-          name="add"
-        >
-          Save
-        </SubmitButton>
-      </Stack>
-    </Form>
+          <SubmitButton
+            size="sm"
+            disabled={!isFormValid}
+            name="add"
+            value={Object.keys(projects).length}
+          >
+            Save
+          </SubmitButton>
+        </Card>
+      </Form>
+    </Collapse>
   );
 };
 
